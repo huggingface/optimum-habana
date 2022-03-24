163d02e72e3c64507fa64d312e6c457a810a708a
regisss <15324346+regisss@users.noreply.github.com>
2022-03-24 16:56:46 +0000

    Add first version of the trainer
    - add GaudiTrainer in optimum/habana/trainer.py
    - add GaudiTrainingArguments in optimum/habana/training_args.py
    - add GaudiConfig in optimum/habana/gaudi_configuration.py

---
+++ optimum/habana/gaudi_configuration.py
@@ -0,0 +1,77 @@
+#  Copyright 2022 The HuggingFace Team. All rights reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from pathlib import Path
+
+from optimum.configuration_utils import BaseConfig
+from optimum.utils import logging
+
+
+logger = logging.get_logger(__name__)
+
+# Default bf16 and fp32 ops (BERT)
+DEFAULT_BF16_OPS = [
+    "add",
+    "addmm",
+    "bmm",
+    "div",
+    "dropout",
+    "gelu",
+    "iadd",
+    "linear",
+    "layer_norm",
+    "matmul",
+    "mm",
+    "rsub",
+    "softmax",
+    "truediv",
+]
+DEFAULT_FP32_OPS = [
+    "embedding",
+    "nll_loss",
+    "log_softmax",
+]
+
+
+class GaudiConfig(BaseConfig):
+    CONFIG_NAME = "gaudi_config.json"
+    FULL_CONFIGURATION_FILE = "gaudi_config.json"
+
+    def __init__(self, **kwargs):
+        # Habana Mixed Precision (MHP) configuration
+        self.use_habana_mixed_precision = kwargs.pop("use_habana_mixed_precision", False)
+        self.hmp_opt_level = kwargs.pop("hmp_opt_level", "O1")
+        self.hmp_bf16_ops = kwargs.pop("hmp_bf16_ops", DEFAULT_BF16_OPS)
+        self.hmp_fp32_ops = kwargs.pop("hmp_fp32_ops", DEFAULT_FP32_OPS)
+        self.hmp_is_verbose = kwargs.pop("hmp_is_verbose", False)
+
+        # Use Habana's custom AdamW implementation
+        self.use_fused_adam = kwargs.pop("use_fused_adam", False)
+        # Use Habana's custom fused clip norm implementation
+        self.use_fused_clip_norm = kwargs.pop("use_fused_clip_norm", False)
+
+        # Log live memory allocations on device at the given point
+        self.log_device_mem_alloc = kwargs.pop("log_device_mem_alloc", False)
+
+    def write_bf16_fp32_ops_to_text_files(
+        self,
+        path_to_bf16_file: Path,
+        path_to_fp32_file: Path,
+    ):
+        for path, ops in zip(
+            [Path(path_to_bf16_file), Path(path_to_fp32_file)], [self.hmp_bf16_ops, self.hmp_fp32_ops]
+        ):
+            with path.open("w") as text_file:
+                # writelines does not add new lines after each element so "\n" is inserted
+                text_file.writelines(op + "\n" for op in ops)
---
+++ optimum/habana/training_args.py
@@ -0,0 +1,188 @@
+#  Copyright 2022 The HuggingFace Team. All rights reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os
+from dataclasses import dataclass, field
+from typing import Optional
+
+from transformers.file_utils import (
+    cached_property,
+    is_sagemaker_dp_enabled,
+    is_sagemaker_mp_enabled,
+    is_torch_available,
+    is_torch_tpu_available,
+    torch_required,
+)
+from transformers.training_args import TrainingArguments
+from transformers.utils import logging
+
+
+if is_torch_available():
+    import torch
+
+if is_torch_tpu_available():
+    import torch_xla.core.xla_model as xm
+
+if is_sagemaker_dp_enabled():
+    import smdistributed.dataparallel.torch.distributed as sm_dist
+
+if is_sagemaker_mp_enabled():
+    import smdistributed.modelparallel.torch as smp
+
+    smp.init()
+
+
+logger = logging.get_logger(__name__)
+
+
+@dataclass
+class GaudiTrainingArguments(TrainingArguments):
+    """
+    GaudiTrainingArguments is built on top of the tranformers' TrainingArguments
+    to enable deployment on Habana's Gaudi.
+    """
+
+    use_habana: Optional[bool] = field(
+        default=False, metadata={"help": "Whether to use Habana's HPU for training the model."}
+    )
+
+    gaudi_config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained Gaudi config name or path if not the same as model_name."}
+    )
+
+    use_lazy_mode: bool = field(
+        default=False,
+        metadata={"help": "Whether to use lazy mode for training the model."},
+    )
+
+    @cached_property
+    @torch_required
+    def _setup_devices(self) -> "torch.device":
+        logger.info("PyTorch: setting up devices")
+        if self.no_cuda:
+            device = torch.device("cpu")
+            self._n_gpu = 0
+            if self.local_rank != -1:
+                # Initializes distributed backend for cpu
+                if self.xpu_backend not in ("mpi", "ccl"):
+                    raise ValueError(
+                        "CPU distributed training backend is not properly set. "
+                        "Please set '--xpu_backend' to either 'mpi' or 'ccl'."
+                    )
+                torch.distributed.init_process_group(backend=self.xpu_backend)
+        elif is_torch_tpu_available():
+            device = xm.xla_device()
+            self._n_gpu = 0
+        elif is_sagemaker_mp_enabled():
+            local_rank = smp.local_rank()
+            device = torch.device("cuda", local_rank)
+            self._n_gpu = 1
+        elif is_sagemaker_dp_enabled():
+            sm_dist.init_process_group()
+            self.local_rank = sm_dist.get_local_rank()
+            device = torch.device("cuda", self.local_rank)
+            self._n_gpu = 1
+        elif self.deepspeed:
+            # deepspeed inits torch.distributed internally
+            from transformers.deepspeed import is_deepspeed_available
+
+            if not is_deepspeed_available():
+                raise ImportError("--deepspeed requires deepspeed: `pip install deepspeed`.")
+            import deepspeed
+
+            deepspeed.init_distributed()
+
+            # workaround for setups like notebooks where the launcher can't be used,
+            # but deepspeed requires a dist env.
+            # env LOCAL_RANK could be set manually by the user, or via init_distributed if mpi4py is installed
+            self.local_rank = int(os.environ.get("LOCAL_RANK", "-1"))
+
+            device = torch.device("cuda", self.local_rank)
+            self._n_gpu = 1
+        elif self.use_habana:
+            logger.info("Habana is enabled.")
+            if self.use_lazy_mode:
+                os.environ["PT_HPU_LAZY_MODE"] = "1"
+                logger.info("Enabled lazy mode.")
+            else:
+                logger.info("Enabled eager mode because use_lazy_mode=False.")
+            from habana_frameworks.torch.utils.library_loader import load_habana_module
+
+            load_habana_module()
+            device = torch.device("hpu")
+            self._n_gpu = 1
+            world_size = 1
+            rank = -1
+            if "WORLD_SIZE" in os.environ and "RANK" in os.environ:
+                world_size = int(os.environ["WORLD_SIZE"])
+                rank = int(os.environ["RANK"])
+                if "LOCAL_RANK" in os.environ:
+                    self.local_rank = int(os.environ["LOCAL_RANK"])
+                logger.info("Torch distributed launch used")
+            elif (
+                "OMPI_COMM_WORLD_LOCAL_RANK" in os.environ
+                and "OMPI_COMM_WORLD_SIZE" in os.environ
+                and "OMPI_COMM_WORLD_RANK" in os.environ
+            ):
+                self.local_rank = int(os.environ["OMPI_COMM_WORLD_LOCAL_RANK"])
+                world_size = int(os.environ["OMPI_COMM_WORLD_SIZE"])
+                rank = int(os.environ["OMPI_COMM_WORLD_RANK"])
+                logger.info("MPI environment variables set")
+            else:
+                try:
+                    global mpi_comm
+                    from mpi4py import MPI
+
+                    mpi_comm = MPI.COMM_WORLD
+                    world_size = mpi_comm.Get_size()
+                    if world_size > 1:
+                        rank = mpi_comm.Get_rank()
+                        self.local_rank = rank
+                    else:
+                        raise ("Single MPI process")
+                except Exception as e:
+                    logger.info("Single node run")
+
+            if self.local_rank != -1:
+                try:
+                    import habana_frameworks.torch.core.hccl
+                except ImportError as error:
+                    error.msg = f"Could not import habana_frameworks.torch.core.hccl. {error.msg}."
+                    raise error
+                os.environ["ID"] = str(self.local_rank)
+                torch.distributed.init_process_group(backend="hccl", rank=self.local_rank, world_size=world_size)
+                logger.info("Enable distributed run")
+
+        elif self.local_rank == -1:
+            # if n_gpu is > 1 we'll use nn.DataParallel.
+            # If you only want to use a specific subset of GPUs use `CUDA_VISIBLE_DEVICES=0`
+            # Explicitly set CUDA to the first (index 0) CUDA device, otherwise `set_device` will
+            # trigger an error that a device index is missing. Index 0 takes into account the
+            # GPUs available in the environment, so `CUDA_VISIBLE_DEVICES=1,2` with `cuda:0`
+            # will use the first GPU in that env, i.e. GPU#1
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            # Sometimes the line in the postinit has not been run before we end up here, so just checking we're not at
+            # the default value.
+            self._n_gpu = torch.cuda.device_count()
+        else:
+            # Here, we'll use torch.distributed.
+            # Initializes the distributed backend which will take care of synchronizing nodes/GPUs
+            torch.distributed.init_process_group(backend="nccl")
+            device = torch.device("cuda", self.local_rank)
+            self._n_gpu = 1
+
+        if device.type == "cuda":
+            torch.cuda.set_device(device)
+
+        return device
