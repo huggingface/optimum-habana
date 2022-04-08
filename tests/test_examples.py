99144db7810b5776d4d261b31d8dcb7cf76bef25
regisss <15324346+regisss@users.noreply.github.com>
2022-04-08 07:08:54 +0000

    First draft for testing examples and their performance

---
+++ tests/test_examples.py
@@ -0,0 +1,241 @@
+# coding=utf-8
+# Copyright 2022 HuggingFace Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import os
+import re
+import subprocess
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Callable, Dict, List, Optional, Tuple, Union
+from unittest import TestCase
+
+from transformers import (
+    CONFIG_MAPPING,
+    MODEL_FOR_QUESTION_ANSWERING_MAPPING,
+    MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING,
+)
+from transformers.testing_utils import slow
+
+from .utils import (
+    MODELS_TO_TEST_MAPPING,
+    VALID_MODELS_FOR_QUESTION_ANSWERING,
+    VALID_MODELS_FOR_SEQUENCE_CLASSIFICATION,
+)
+
+
+def _get_supported_models_for_script(
+    models_to_test: Dict[str, List[Tuple[str]]],
+    task_mapping: Dict[str, str],
+    valid_models_for_task: List[str],
+) -> List[Tuple[str]]:
+    """
+    Filters models that can perform the task from models_to_test.
+    Args:
+        models_to_test: mapping between a model type and a tuple (model_name_or_path, ipu_config_name).
+        task_mapping: mapping between a model config and a model class.
+        valid_models_for_task: list of supported models for a specific task.
+    Returns:
+        A list of models that are supported for the task.
+        Each element of the list follows the same format: (model_type, (model_name_or_path, ipu_config_name)).
+    """
+
+    def is_valid_model_type(model_type: str) -> bool:
+        in_task_mapping = CONFIG_MAPPING[model_type] in task_mapping
+        in_valid_models_for_task = model_type in valid_models_for_task
+        if in_task_mapping and in_valid_models_for_task:
+            return True
+        return False
+
+    return [
+        (model_type, names)
+        for (model_type, possible_names) in models_to_test.items()
+        for names in possible_names
+        if is_valid_model_type(model_type)
+    ]
+
+
+_SCRIPT_TO_MODEL_MAPPING = {
+    "run_qa": _get_supported_models_for_script(
+        MODELS_TO_TEST_MAPPING,
+        MODEL_FOR_QUESTION_ANSWERING_MAPPING,
+        VALID_MODELS_FOR_QUESTION_ANSWERING,
+    ),
+    "run_glue": _get_supported_models_for_script(
+        MODELS_TO_TEST_MAPPING,
+        MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING,
+        VALID_MODELS_FOR_SEQUENCE_CLASSIFICATION,
+    ),
+}
+
+
+class ExampleTestMeta(type):
+    """
+    Metaclass that takes care of creating the proper example tests for a given task.
+    It uses example_name to figure out which models support this task, and create a run example test for each of these
+    models.
+    """
+
+    def __new__(cls, name, bases, attrs, example_name=None):
+        models_to_test = []
+        if example_name is not None:
+            models_to_test = _SCRIPT_TO_MODEL_MAPPING.get(example_name)
+            if models_to_test is None:
+                raise AttributeError(f"Could not create class because no model was found for example {example_name}")
+        for _, names in models_to_test:
+            model_name, gaudi_config_name = names
+            attrs[f"test_{example_name}_{model_name}"] = cls._create_test(model_name, gaudi_config_name)
+        attrs["EXAMPLE_NAME"] = example_name
+        return super().__new__(cls, name, bases, attrs)
+
+    @classmethod
+    def _create_test(cls, model_name: str, gaudi_config_name: str) -> Callable[[], None]:
+        """
+        Creates a test function that runs an example for a specific (model_name, gaudi_config_name) pair.
+        Args:
+            model_name: the model_name_or_path.
+            gaudi_config_name: the gaudi config name.
+        Returns:
+            The test function that runs the example.
+        """
+
+        @slow
+        def test(self):
+            if self.EXAMPLE_NAME is None:
+                raise ValueError("An example name must be provided")
+            example_script = Path(self.EXAMPLE_DIR).glob(f"*/{self.EXAMPLE_NAME}.py")
+            example_script = list(example_script)
+            if len(example_script) == 0:
+                raise RuntimeError(f"Could not find {self.EXAMPLE_NAME}.py in examples located in {self.EXAMPLE_DIR}")
+            elif len(example_script) > 1:
+                raise RuntimeError(f"Found more than {self.EXAMPLE_NAME}.py in examples located in {self.EXAMPLE_DIR}")
+            else:
+                example_script = example_script[0]
+
+            self._install_requirements(example_script.parent / "requirements.txt")
+
+            with TemporaryDirectory() as tmp_dir:
+                cmd_line = self._create_command_line(
+                    example_script,
+                    model_name,
+                    gaudi_config_name,
+                    tmp_dir,
+                    task=self.TASK_NAME,
+                    do_eval=self.EVAL_IS_SUPPORTED,
+                    train_batch_size=self.TRAIN_BATCH_SIZE,
+                    eval_batch_size=self.EVAL_BATCH_SIZE,
+                )
+                p = subprocess.Popen(cmd_line)
+                return_code = p.wait()
+                self.assertEqual(return_code, 0)
+
+                if self.EVAL_IS_SUPPORTED:
+                    with open(Path(tmp_dir) / "all_results.json") as fp:
+                        results = json.load(fp)
+                    self.assertGreaterEqual(float(results[self.SCORE_NAME]), self.EVAL_SCORE_THRESHOLD)
+
+            # TODO: is a cleanup of the dataset cache needed?
+            # self._cleanup_dataset_cache()
+
+        return test
+
+
+class ExampleTesterBase(TestCase):
+    """
+    Base example tester class.
+    Attributes:
+        EXAMPLE_DIR (`str` or `os.Pathlike`): the directory containing the examples.
+        EXAMPLE_NAME (`str`): the name of the example script without the file extension, e.g. run_qa, run_glue, etc.
+        TASK_NAME (`str`): the name of the dataset to use.
+        EVAL_IS_SUPPORTED (`bool`): whether evaluation is currently supported on IPUs.
+            If True, the example will run evaluation, otherwise it will be skipped.
+        EVAL_SCORE_THRESHOLD (`float`): the score threshold from which training is assumed to have worked.
+        SCORE_NAME (`str`): the name of the metric to use for checking that the example ran successfully.
+        DATASET_PARAMETER_NAME (`str`): the argument name to use for the dataset parameter.
+            Most of the time it will be "dataset_name", but for some tasks on a benchmark it might be something else.
+        TRAIN_BATCH_SIZE (`int`): the batch size to give to the example script for training.
+        EVAL_BATCH_SIZE (`int`): the batch size to give to the example script for evaluation.
+        INFERENCE_DEVICE_ITERATIONS (`int`): the number of device iterations to use for evaluation.
+        GRADIENT_ACCUMULATION_STEPS (`int`): the number of gradient accumulation to use during training.
+    """
+
+    EXAMPLE_DIR = Path(os.path.dirname(__file__)).parent / "examples"
+    EXAMPLE_NAME = None
+    TASK_NAME = None
+    EVAL_IS_SUPPORTED = True
+    EVAL_SCORE_THRESHOLD = 0.75
+    SCORE_NAME = "eval_accuracy"
+    DATASET_PARAMETER_NAME = "dataset_name"
+    TRAIN_BATCH_SIZE = 2
+    EVAL_BATCH_SIZE = 2
+
+    def _create_command_line(
+        self,
+        script: str,
+        model_name: str,
+        ipu_config_name: str,
+        output_dir: str,
+        task: Optional[str] = None,
+        do_eval: bool = True,
+        lr: float = 1e-5,
+        train_batch_size: int = 2,
+        eval_batch_size: int = 2,
+        num_epochs: int = 2,
+        extra_command_line_arguments: Optional[List[str]] = None,
+    ) -> List[str]:
+        do_eval_option = "--do_eval" if do_eval else " "
+        task_option = f"--{self.DATASET_PARAMETER_NAME} {task}" if task else " "
+
+        cmd_line = [
+            f"{script}",
+            f"--model_name_or_path {model_name}",
+            f"--gaudi_config_name {ipu_config_name}",
+            f"{task_option}",
+            "--do_train",
+            f"{do_eval_option}",
+            f"--output_dir {output_dir}",
+            "--overwrite_output_dir true",
+            f"--learning_rate {lr}",
+            f"--per_device_train_batch_size {train_batch_size}",
+            f"--per_device_eval_batch_size {eval_batch_size}",
+            "--save_strategy epoch",
+            f" --num_train_epochs {num_epochs}",
+        ]
+        if extra_command_line_arguments is not None:
+            cmd_line += extra_command_line_arguments
+
+        pattern = re.compile(r"([\"\'].+?[\"\'])|\s")
+        return [x for y in cmd_line for x in re.split(pattern, y) if x]
+
+    def _install_requirements(self, requirements_filename: Union[str, os.PathLike]):
+        """
+        Installs the necessary requirements to run the example if the provided file exists, otherwise does nothing.
+        """
+        if not Path(requirements_filename).exists():
+            return
+        cmd_line = f"pip install -r {requirements_filename}".split()
+        p = subprocess.Popen(cmd_line)
+        return_code = p.wait()
+        self.assertEqual(return_code, 0)
+
+
+class TextClassificationExampleTester(ExampleTesterBase, metaclass=ExampleTestMeta, example_name="run_glue"):
+    TASK_NAME = "mrpc"
+    DATASET_PARAMETER_NAME = "task_name"
+
+
+class QuestionAnsweringExampleTester(ExampleTesterBase, metaclass=ExampleTestMeta, example_name="run_qa"):
+    TASK_NAME = "squad"
+    SCORE_NAME = "eval_f1"
