b440250217d0006c4ae8a6eeff10eac8adc73002
regisss <15324346+regisss@users.noreply.github.com>
2022-04-13 00:10:48 +0200

    Add non-regression tests for each task and each model in single/multi-card configuration
    Add non-regression tests covering the following cases:
    - datasets: SQuAD, MRPC
    - models: BERT base/large, RoBERTa base/large, ALBERT large/XXL, Distilbert
    - single- and multi-card training
    
    Baselines for each case are stored as JSON files in optimum-habana/tests/baselines/.

---
+++ tests/utils.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+# Copyright 2022 HuggingFace Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Mapping between model families and specific model names with their configuration
+# TODO: add configuration names once they have been pushed to the hub
+MODELS_TO_TEST_MAPPING = {
+    "bert": [
+        ("bert-base-uncased", ""),
+        ("bert-large-uncased-whole-word-masking", ""),
+    ],
+    "roberta": [
+        ("roberta-base", ""),
+        ("roberta-large", ""),
+    ],
+    "albert": [
+        ("albert-large-v2", ""),
+        ("albert-xxlarge-v1", ""),
+    ],
+    "distilbert": [
+        ("distilbert-base-uncased", ""),
+    ],
+}
+
+VALID_MODELS_FOR_QUESTION_ANSWERING = [
+    "bert",
+    "roberta",
+    "albert",
+    "distilbert",
+]
+
+# Only BERT is officially supported for sequence classification
+VALID_MODELS_FOR_SEQUENCE_CLASSIFICATION = [
+    "bert",
+    # "roberta",
+    # "albert",
+    # "distilbert",
+]
