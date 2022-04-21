896a5803b928d5270920af3d7fdf233d611ca1f9
regisss <15324346+regisss@users.noreply.github.com>
2022-04-21 13:50:43 +0000

    Test CI

--- ci_test.sh
+++ ci_test.sh
@@ -2,11 +2,11 @@ OS=ubuntu20.04
 
 # Install Docker
 sudo apt update
-sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo apt install -y ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # Install Habana
 curl -X GET https://vault.habana.ai/artifactory/api/gpg/key/public | sudo apt-key add --
