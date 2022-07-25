cb0c01cf0f3b680c04a10a62bc31983f2801ff85
Morgan Funtowicz <funtowiczmo@gmail.com>
2022-07-25 19:13:33 +0200

    Some missing places.

--- tests/ci/fast_tests.sh
+++ tests/ci/fast_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip install --upgrade pip
+pip install --upgrade --user pip
 make fast_tests
--- tests/ci/slow_tests_8x.sh
+++ tests/ci/slow_tests_8x.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip install --upgrade pip
+pip install --upgrade --user pip
 export RUN_SLOW=true
 make slow_tests_8x
