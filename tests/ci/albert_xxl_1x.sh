4e83bfbad139415499ad026b0b3abd69ec055398
regisss <15324346+regisss@users.noreply.github.com>
2022-08-02 08:56:48 +0000

    Add --force-reinstall to pip commands

--- tests/ci/albert_xxl_1x.sh
+++ tests/ci/albert_xxl_1x.sh
@@ -3,5 +3,5 @@
 pip install --user --upgrade pip
 export RUN_SLOW=true
 export RUN_ALBERT_XXL_1X=true
-pip install --user .[tests]
+pip install --user --force-reinstall .[tests]
 python -m pytest tests/test_examples.py -v -s -k "albert-xxlarge-v1_single_card"
