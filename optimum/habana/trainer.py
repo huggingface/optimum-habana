60f0f61bf13561eb20ad63306a9bae5efd95972a
regisss <15324346+regisss@users.noreply.github.com>
2022-08-05 22:52:25 +0000

    Add image classification example with ViT

--- optimum/habana/trainer.py
+++ optimum/habana/trainer.py
@@ -37,7 +37,8 @@ from transformers.data.data_collator import DataCollator
 from transformers.debug_utils import DebugOption, DebugUnderflowOverflow
 from transformers.integrations import hp_params
 from transformers.modeling_utils import ModuleUtilsMixin, PreTrainedModel, unwrap_model
-from transformers.models.albert.modeling_albert import AlbertModel
+from transformers.models.albert.modeling_albert import AlbertModel  # TODO: change how tweaked classes/functions are managed
+from transformers.models.vit.modeling_vit import ViTSelfAttention  # TODO: change how tweaked classes/functions are managed
 from transformers.pytorch_utils import ALL_LAYERNORM_LAYERS
 from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 from transformers.trainer_callback import TrainerCallback, TrainerState
@@ -80,6 +81,7 @@ from .modeling_utils import (
     gaudi_invert_attention_mask,
     to_gaudi_for_accelerated_generation,
 )
+from .models.vit import gaudi_vit_self_attention_forward  # TODO: change how tweaked classes/functions are managed
 from .trainer_utils import convert_into_dtypes, get_dtype, speed_metrics, to_device_dtype
 from .training_args import GaudiTrainingArguments
 
@@ -125,8 +127,12 @@ class GaudiTrainer(Trainer):
             logger.info(f"No `GaudiTrainingArguments` passed, using `output_dir={output_dir}`.")
             args = GaudiTrainingArguments(output_dir=output_dir)
 
+        # TODO: change how tweaked classes/functions are managed
+        ViTSelfAttention.forward = gaudi_vit_self_attention_forward
+
         # In lazy_mode, decoder or encoder-decoder architectures are slightly
         # modified to accelerate the generation process
+        # TODO: change how tweaked classes/functions are managed
         if args.use_habana and model.__class__ in PRETRAINED_TO_GAUDI_REGISTRY and model is not None:
             model = to_gaudi_for_accelerated_generation(model)
 
@@ -181,6 +187,7 @@ class GaudiTrainer(Trainer):
                             isVerbose=self.gaudi_config.hmp_is_verbose,
                         )
 
+                # TODO: change how tweaked classes/functions are managed
                 # When HMP is enabled, replace invert_attention_mask and get_extended_attention_mask
                 # so that HMP is disabled for specific parts of the code
                 ModuleUtilsMixin.invert_attention_mask = gaudi_invert_attention_mask
