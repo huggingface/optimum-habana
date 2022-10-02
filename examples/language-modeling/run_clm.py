6b354a4cbf1b1249af0d2eddb35ed7719e2bd707
regisss <15324346+regisss@users.noreply.github.com>
2022-10-02 19:05:10 +0000

    Update to Transformers 4.22 and to Synapse 1.6.1 (#98)

--- examples/language-modeling/run_clm.py
+++ examples/language-modeling/run_clm.py
@@ -29,8 +29,9 @@ from itertools import chain
 from typing import Optional
 
 import datasets
-from datasets import load_dataset, load_metric
+from datasets import load_dataset
 
+import evaluate
 import transformers
 from optimum.habana import GaudiConfig, GaudiTrainer, GaudiTrainingArguments
 from transformers import (
@@ -50,7 +51,7 @@ from transformers.utils.versions import require_version
 
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-check_min_version("4.21.0")
+check_min_version("4.22.0")
 
 require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/language-modeling/requirements.txt")
 
@@ -111,7 +112,7 @@ class ModelArguments:
         default=False,
         metadata={
             "help": (
-                "Will use the token generated when running `transformers-cli login` (necessary to use this script "
+                "Will use the token generated when running `huggingface-cli login` (necessary to use this script "
                 "with private models)."
             )
         },
@@ -498,7 +499,7 @@ def main():
                 logits = logits[0]
             return logits.argmax(dim=-1)
 
-        metric = load_metric("accuracy")
+        metric = evaluate.load("accuracy")
 
         def compute_metrics(eval_preds):
             preds, labels = eval_preds
