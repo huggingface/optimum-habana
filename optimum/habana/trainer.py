18ab29554efd938fd625d7d8fdb859877eb10b01
Ubuntu <ubuntu@ip-172-31-90-245.ec2.internal>
2022-10-07 16:47:04 +0000

    Add debug print

--- optimum/habana/trainer.py
+++ optimum/habana/trainer.py
@@ -963,7 +963,7 @@ class GaudiTrainer(Trainer):
             self.model_wrapped = deepspeed_engine
             self.deepspeed = deepspeed_engine
 
-        model = self._wrap_model(self.model.to(dtype=torch.float32, device="hpu"), training=False, dataloader=dataloader)
+        model = self._wrap_model(self.model, training=False, dataloader=dataloader)
 
         batch_size = self.args.eval_batch_size
 
@@ -1535,3 +1535,39 @@ class GaudiTrainer(Trainer):
         output = {**logs, **{"step": self.state.global_step}}
         self.state.log_history.append(output)
         self.control = self.callback_handler.on_log(self.args, self.state, self.control, logs)
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        """
+        How the loss is computed by Trainer. By default, all models return the loss in the first element.
+        Subclass and override for custom behavior.
+        """
+        print("INPUTS")
+        for key, value in inputs.items():
+            print(key, value.dtype)
+        if self.label_smoother is not None and "labels" in inputs:
+            labels = inputs.pop("labels")
+        else:
+            labels = None
+        outputs = model(**inputs)
+        # Save past state if it exists
+        # TODO: this needs to be fixed and made cleaner later.
+        if self.args.past_index >= 0:
+            self._past = outputs[self.args.past_index]
+
+        if labels is not None:
+            if unwrap_model(model)._get_name() in MODEL_FOR_CAUSAL_LM_MAPPING_NAMES.values():
+                loss = self.label_smoother(outputs, labels, shift_labels=True)
+            else:
+                loss = self.label_smoother(outputs, labels)
+        else:
+            if isinstance(outputs, dict) and "loss" not in outputs:
+                raise ValueError(
+                    "The model did not return a loss from the inputs, only the following keys: "
+                    f"{','.join(outputs.keys())}. For reference, the inputs it received are {','.join(inputs.keys())}."
+                )
+            # We don't use .loss here since the model may return tuples instead of ModelOutput.
+            loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
+
+        print("LOSS", loss, loss.dtype)
+
+        return (loss, outputs) if return_outputs else loss
