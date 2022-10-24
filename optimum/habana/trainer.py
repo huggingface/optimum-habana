fc1240679172a19fdb9b1a65af9d72ffbe2f911e
regisss <15324346+regisss@users.noreply.github.com>
2022-10-24 21:32:39 +0000

    Debug

--- optimum/habana/trainer.py
+++ optimum/habana/trainer.py
@@ -1554,32 +1554,3 @@ class GaudiTrainer(Trainer):
         # Moving a model to HPU disconnects the tied weights, so we have to retie them.
         if self.args.use_habana and hasattr(model, "tie_weights"):
             model.tie_weights()
-
-    def _get_train_sampler(self) -> Optional[torch.utils.data.Sampler]:
-        if self.train_dataset is None or not has_length(self.train_dataset):
-            return None
-
-        generator = None
-        if self.args.world_size <= 1:
-            generator = torch.Generator()
-            # for backwards compatibility, we generate a seed here (which is sampled from a generator seeded with
-            # `args.seed`) if data_seed isn't provided.
-            # Further on in this method, we default to `args.seed` instead.
-            if self.args.data_seed is None:
-                seed = int(torch.empty((), dtype=torch.int64).random_().item())
-            else:
-                seed = self.args.data_seed
-            generator.manual_seed(seed)
-
-        seed = self.args.data_seed if self.args.data_seed is not None else self.args.seed
-
-        # Build the sampler.
-        # if self.args.world_size <= 1:
-        return RandomSampler(self.train_dataset, generator=generator)
-        # else:
-        #     return DistributedSampler(
-        #         self.train_dataset,
-        #         num_replicas=self.args.world_size,
-        #         rank=self.args.process_index,
-        #         seed=seed,
-        #     )
