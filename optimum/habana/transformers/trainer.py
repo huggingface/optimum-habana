8024e9d3826682d66f24628d89d3471d5be8dfd9
regisss <15324346+regisss@users.noreply.github.com>
2022-12-02 11:14:44 +0100

    Update to Transformers 4.25.1 (#136)

--- optimum/habana/transformers/trainer.py
+++ optimum/habana/transformers/trainer.py
@@ -1185,7 +1185,15 @@ class GaudiTrainer(Trainer):
             Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]: A tuple with the loss,
             logits and labels (each being optional).
         """
-        has_labels = all(inputs.get(k) is not None for k in self.label_names)
+        has_labels = False if len(self.label_names) == 0 else all(inputs.get(k) is not None for k in self.label_names)
+        # For CLIP-like models capable of returning loss values.
+        # If `return_loss` is not specified or being `None` in `inputs`, we check if the default value of `return_loss`
+        # is `True` in `model.forward`.
+        return_loss = inputs.get("return_loss", None)
+        if return_loss is None:
+            return_loss = self.can_return_loss
+        loss_without_labels = True if len(self.label_names) == 0 and return_loss else False
+
         inputs = self._prepare_inputs(inputs)
         if ignore_keys is None:
             if hasattr(self.model, "config"):
@@ -1194,7 +1202,7 @@ class GaudiTrainer(Trainer):
                 ignore_keys = []
 
         # labels may be popped when computing the loss (label smoothing for instance) so we grab them first.
-        if has_labels:
+        if has_labels or loss_without_labels:
             labels = nested_detach(tuple(inputs.get(name) for name in self.label_names))
             if len(labels) == 1:
                 labels = labels[0]
@@ -1202,7 +1210,7 @@ class GaudiTrainer(Trainer):
             labels = None
 
         with torch.no_grad():
-            if has_labels:
+            if has_labels or loss_without_labels:
                 with self.compute_loss_context_manager():
                     loss, outputs = self.compute_loss(model, inputs, return_outputs=True)
                 loss = loss.mean().detach()
