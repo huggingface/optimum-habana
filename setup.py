c1c2a87e2ea1cd230b768934879fbaec909a9c11
regisss <regis@huggingface.co>
2023-02-22 10:43:09 +0100

    Install hf_doc_builder from source again

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ TESTS_REQUIRE = [
 QUALITY_REQUIRES = [
     "black",
     "ruff",
-    "hf_doc_builder",
+    "hf_doc_builder @ git+https://github.com/huggingface/doc-builder.git",
 ]
 
 EXTRAS_REQUIRE = {
