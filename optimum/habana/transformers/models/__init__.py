c8858de56abead360f40e432ffeb560fd5e83a66
regisss <15324346+regisss@users.noreply.github.com>
2023-06-22 15:14:35 +0000

    Add support for ESM

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -11,6 +11,8 @@ from .esm import (
     gaudi_esm_for_protein_folding_forward,
     gaudi_esmfold_self_attention_forward,
     gaudi_esmfolding_trunk_forward,
+    gaudi_esmoutput_forward,
+    gaudi_esmselfoutput_forward,
     gaudi_rot_matmul,
     gaudi_rot_vec_mul,
 )
