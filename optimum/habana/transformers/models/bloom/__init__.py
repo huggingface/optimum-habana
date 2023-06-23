466f0af27b5178ea5beac30254d8146a7aaab32f
regisss <15324346+regisss@users.noreply.github.com>
2023-06-23 19:28:11 +0200

    Update BLOOM modeling for SynapseAI 1.10 (#277)

--- optimum/habana/transformers/models/bloom/__init__.py
+++ optimum/habana/transformers/models/bloom/__init__.py
@@ -1,7 +1,9 @@
 from .modeling_bloom import (
     GaudiBloomForCausalLM,
     GaudiBloomMLP,
-    GaudiBloomModel,
     gaudi_bloom_attention_forward,
     gaudi_bloom_block_forward,
+    gaudi_bloom_convert_to_bloom_cache,
+    gaudi_bloom_convert_to_standard_cache,
+    gaudi_bloom_model_forward,
 )
