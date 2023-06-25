4520bf11a9c54f6bc6ab0eca89944b63eea27374
zzhai <zzhai@habana.ai>
2023-06-25 15:36:07 +0800

    gaudi2 support opt-66b

--- examples/text-generation/run_generation.py
+++ examples/text-generation/run_generation.py
@@ -180,7 +180,7 @@ def main():
             with deepspeed.OnDevice(dtype=model_dtype, device="meta"):
                 model = AutoModelForCausalLM.from_config(config, torch_dtype=model_dtype)
         else:
-            with deepspeed.OnDevice(dtype=model_dtype, device=args.device):
+            with deepspeed.OnDevice(dtype=model_dtype, device="cpu"):
                 model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, torch_dtype=model_dtype)
         model = model.eval()
 
