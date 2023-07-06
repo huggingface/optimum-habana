bf2b644e9ae8eb03f4b2a0dfd004f70812e90aee
regisss <15324346+regisss@users.noreply.github.com>
2023-07-06 23:03:41 +0000

    Add test

--- tests/test_diffusers.py
+++ tests/test_diffusers.py
@@ -210,6 +210,49 @@ class GaudiStableDiffusionPipelineTester(TestCase):
 
             self.assertLess(np.abs(image_slice.flatten() - expected_slice).max(), 1e-2)
 
+    def test_stable_diffusion_prompt_embeds_with_plain_negative_prompt_list(self):
+        components = self.get_dummy_components()
+        gaudi_config = GaudiConfig(use_habana_mixed_precision=False)
+
+        sd_pipe = GaudiStableDiffusionPipeline(
+            use_habana=True,
+            gaudi_config=gaudi_config,
+            **components,
+        )
+        sd_pipe.set_progress_bar_config(disable=None)
+
+        inputs = self.get_dummy_inputs("cpu")
+        negative_prompt = 3 * ["this is a negative prompt"]
+        inputs["negative_prompt"] = negative_prompt
+        inputs["prompt"] = 3 * [inputs["prompt"]]
+
+        # forward
+        output = sd_pipe(**inputs)
+        image_slice_1 = output.images[0][-3:, -3:, -1]
+
+        inputs = self.get_dummy_inputs("cpu")
+        inputs["negative_prompt"] = negative_prompt
+        prompt = 3 * [inputs.pop("prompt")]
+
+        text_inputs = sd_pipe.tokenizer(
+            prompt,
+            padding="max_length",
+            max_length=sd_pipe.tokenizer.model_max_length,
+            truncation=True,
+            return_tensors="pt",
+        )
+        text_inputs = text_inputs["input_ids"].to("cpu")
+
+        prompt_embeds = sd_pipe.text_encoder(text_inputs)[0]
+
+        inputs["prompt_embeds"] = prompt_embeds
+
+        # forward
+        output = sd_pipe(**inputs)
+        image_slice_2 = output.images[0][-3:, -3:, -1]
+
+        assert np.abs(image_slice_1.flatten() - image_slice_2.flatten()).max() < 1e-4
+
     def test_stable_diffusion_no_safety_checker(self):
         gaudi_config = GaudiConfig(use_habana_mixed_precision=False)
         scheduler = GaudiDDIMScheduler(
