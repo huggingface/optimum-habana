6e0f446044ac61f128553627a931ba93014e283b
regisss <15324346+regisss@users.noreply.github.com>
2023-07-07 12:37:26 +0000

    Add variable to register optimized models

--- optimum/habana/transformers/generation/__init__.py
+++ optimum/habana/transformers/generation/__init__.py
@@ -1,2 +1,11 @@
 from .configuration_utils import GaudiGenerationConfig
 from .utils import GaudiGenerationMixin
+
+
+MODELS_OPTIMIZED_WITH_STATIC_SHAPES = [
+    "bloom",
+    "gpt2",
+    "opt",
+    "gptj",
+    "gpt_neox",
+]
