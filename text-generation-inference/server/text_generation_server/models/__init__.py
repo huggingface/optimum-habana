25915819a7fa58c5f473617d4264697e03b20e48
regisss <15324346+regisss@users.noreply.github.com>
2023-07-16 21:36:10 +0000

    Add custom files

---
+++ text-generation-inference/server/text_generation_server/models/__init__.py
@@ -0,0 +1,35 @@
+import os
+from typing import Optional
+
+import torch
+from loguru import logger
+from text_generation_server.models.bloom import BLOOM
+from text_generation_server.models.causal_lm import CausalLM
+from text_generation_server.models.model import Model
+from text_generation_server.models.santacoder import SantaCoder
+from transformers import AutoConfig
+from transformers.configuration_utils import PretrainedConfig
+from transformers.models.auto import modeling_auto
+
+
+# Disable gradients
+torch.set_grad_enabled(False)
+
+
+def get_model(
+    model_id: str,
+    revision: Optional[str],
+) -> Model:
+    config = AutoConfig.from_pretrained(model_id, revision=revision)
+    model_type = config.model_type
+
+    if model_type == "gpt_bigcode":
+        return SantaCoder(model_id, revision)
+
+    if model_type == "bloom":
+        return BLOOM(model_id, revision)
+
+    if model_type in modeling_auto.MODEL_FOR_CAUSAL_LM_MAPPING_NAMES:
+        return CausalLM(model_id, revision)
+
+    raise ValueError(f"Unsupported model type {model_type}")
---
+++ text-generation-inference/server/text_generation_server/server.py
@@ -0,0 +1,156 @@
+import asyncio
+from pathlib import Path
+from typing import List, Optional
+
+import torch
+from grpc import aio
+from grpc_reflection.v1alpha import reflection
+from loguru import logger
+from text_generation_server.cache import Cache
+from text_generation_server.interceptor import ExceptionInterceptor
+from text_generation_server.models import Model, get_model
+from text_generation_server.pb import generate_pb2, generate_pb2_grpc
+from text_generation_server.tracing import UDSOpenTelemetryAioServerInterceptor
+
+
+class TextGenerationService(generate_pb2_grpc.TextGenerationServiceServicer):
+    def __init__(self, model: Model, cache: Cache, server_urls: List[str]):
+        self.cache = cache
+        self.model = model
+        self.server_urls = server_urls
+        # For some reason, inference_mode does not work well with GLOO which we use on CPU
+        if model.device.type == "hpu":
+            # Force inference mode for the lifetime of TextGenerationService
+            self._inference_mode_raii_guard = torch._C._InferenceMode(True)
+            print("INFERNCE MOOOOOODE")
+
+    async def Info(self, request, context):
+        return self.model.info
+
+    async def Health(self, request, context):
+        if self.model.device.type == "hpu":
+            torch.zeros((2, 2)).to("hpu")
+        return generate_pb2.HealthResponse()
+
+    async def ServiceDiscovery(self, request, context):
+        return generate_pb2.ServiceDiscoveryResponse(urls=self.server_urls)
+
+    async def ClearCache(self, request, context):
+        if request.HasField("id"):
+            self.cache.delete(request.id)
+        else:
+            self.cache.clear()
+        return generate_pb2.ClearCacheResponse()
+
+    async def FilterBatch(self, request, context):
+        batch = self.cache.pop(request.batch_id)
+        if batch is None:
+            raise ValueError(f"Batch ID {request.batch_id} not found in cache.")
+        filtered_batch = batch.filter(request.request_ids)
+        self.cache.set(filtered_batch)
+
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
+        return generate_pb2.FilterBatchResponse(batch=filtered_batch.to_pb())
+
+    async def Warmup(self, request, context):
+        batch = self.model.batch_type.from_pb(request.batch, self.model.tokenizer, self.model.dtype, self.model.device)
+        self.model.warmup(batch, request.max_total_tokens)
+
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
+        return generate_pb2.WarmupResponse()
+
+    async def Prefill(self, request, context):
+        batch = self.model.batch_type.from_pb(
+            request.batch, self.model.tokenizer, self.model.dtype, self.model.device, self.model.is_optimized_for_gaudi
+        )
+
+        generations, next_batch = self.model.generate_token(batch)
+        self.cache.set(next_batch)
+
+        return generate_pb2.PrefillResponse(
+            generations=[generation.to_pb() for generation in generations],
+            batch=next_batch.to_pb() if next_batch else None,
+        )
+
+    async def Decode(self, request, context):
+        if len(request.batches) == 0:
+            raise ValueError("Must provide at least one batch")
+
+        batches = []
+        for batch_pb in request.batches:
+            batch = self.cache.pop(batch_pb.id)
+            if batch is None:
+                raise ValueError(f"Batch ID {batch_pb.id} not found in cache.")
+            batches.append(batch)
+
+        if len(batches) == 0:
+            raise ValueError("All batches are empty")
+
+        if len(batches) > 1:
+            batch = self.model.batch_type.concatenate(batches)
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
+        else:
+            batch = batches[0]
+
+        generations, next_batch = self.model.generate_token(batch)
+        self.cache.set(next_batch)
+
+        return generate_pb2.DecodeResponse(
+            generations=[generation.to_pb() for generation in generations],
+            batch=next_batch.to_pb() if next_batch else None,
+        )
+
+
+def serve(
+    model_id: str,
+    revision: Optional[str],
+    dtype: Optional[str],
+    uds_path: Path,
+):
+    async def serve_inner(
+        model_id: str,
+        revision: Optional[str],
+        dtype: Optional[str] = None,
+    ):
+        unix_socket_template = "unix://{}-{}"
+        local_url = unix_socket_template.format(uds_path, 0)
+        server_urls = [local_url]
+
+        try:
+            model = get_model(model_id, revision, dtype)
+        except Exception:
+            logger.exception("Error when initializing model")
+            raise
+
+        server = aio.server(
+            interceptors=[
+                ExceptionInterceptor(),
+                UDSOpenTelemetryAioServerInterceptor(),
+            ]
+        )
+        generate_pb2_grpc.add_TextGenerationServiceServicer_to_server(
+            TextGenerationService(model, Cache(), server_urls), server
+        )
+        SERVICE_NAMES = (
+            generate_pb2.DESCRIPTOR.services_by_name["TextGenerationService"].full_name,
+            reflection.SERVICE_NAME,
+        )
+        reflection.enable_server_reflection(SERVICE_NAMES, server)
+        server.add_insecure_port(local_url)
+
+        await server.start()
+
+        logger.info("Server started at {}".format(local_url))
+
+        try:
+            await server.wait_for_termination()
+        except KeyboardInterrupt:
+            logger.info("Signal received. Shutting down")
+            await server.stop(0)
+
+    asyncio.run(serve_inner(model_id, revision, dtype))
