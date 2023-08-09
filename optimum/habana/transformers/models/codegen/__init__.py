dc3248f3b131ea9932c9e084e0831c3006ad6c83
Wang, Yi <yi.a.wang@intel.com>
2023-08-09 06:27:59 -0400

    fix codegen HCCL issue occured in DDP
    Signed-off-by: Wang, Yi <yi.a.wang@intel.com>

--- optimum/habana/transformers/models/codegen/__init__.py
+++ optimum/habana/transformers/models/codegen/__init__.py
@@ -1,6 +1,6 @@
 from .modeling_codegen import (
+    GaudiCodeGenAttention,
     GaudiCodeGenForCausalLM,
-    gaudi_codegen_attention_forward,
     gaudi_codegen_block_forward,
     gaudi_codegen_model_forward,
 )
