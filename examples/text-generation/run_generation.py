6f8de575bf8c3246324564da5553495f4953e86f
Jin, Baihui <baihui.jin@intel.com>
2023-09-13 01:53:55 +0000

    fix help/remove debug

--- examples/text-generation/run_generation.py
+++ examples/text-generation/run_generation.py
@@ -105,7 +105,7 @@ def main():
     parser.add_argument(
         "--trim_logits",
         action="store_true",
-        help="Calculate logits only for thCalculate logits only for the last token to save the memory in first step.",
+        help="Calculate logits only for the last token to save the memory in first step.",
     )
     parser.add_argument(
         "--seed",
@@ -430,7 +430,6 @@ def main():
         duration = time.perf_counter() - t0
         total_new_tokens_generated = args.n_iterations * args.batch_size * args.max_new_tokens
         throughput = total_new_tokens_generated / duration
-        print("tp: ", throughput)
         if rank in [-1, 0]:
             print()
             print("Input/outputs:")
