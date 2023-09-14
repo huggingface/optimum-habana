71bee9b47f753272189dc3c4fccb33f4991ee664
P V R K Jyothendra Varma <jvarma@habana.ai>
2023-09-14 08:43:38 +0300

    move limit_hpu_graphs to config
    Signed-off-by: P V R K Jyothendra Varma <jvarma@habana.ai>

--- optimum/habana/transformers/generation/configuration_utils.py
+++ optimum/habana/transformers/generation/configuration_utils.py
@@ -25,3 +25,4 @@ class GaudiGenerationConfig(GenerationConfig):
         self.static_shapes = kwargs.get("static_shapes", None)
         self.ignore_eos = kwargs.get("ignore_eos", None)
         self.attn_softmax_bf16 = kwargs.get("attn_softmax_bf16", None)
+        self.limit_hpu_graphs = kwargs.get("limit_hpu_graphs", None)
