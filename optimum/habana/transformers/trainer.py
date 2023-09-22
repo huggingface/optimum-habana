f8321d36bb72c853153aeff196c24ef11fa4627a
Liu, Mingzhi <mingzhi.liu@intel.com>
2023-09-22 08:32:27 +0000

    fix format

--- optimum/habana/transformers/trainer.py
+++ optimum/habana/transformers/trainer.py
@@ -772,7 +772,7 @@ class GaudiTrainer(Trainer):
             self.log_evaluate_save_time = 0
         else:
             self.log_evaluate_save_time = None
-            
+
         hb_profiler = HabanaProfile(warmup=self.args.profiling_warmup_steps, active=self.args.profiling_steps, record_shapes=self.args.profiling_record_shapes)
         hb_profiler.start()
 
