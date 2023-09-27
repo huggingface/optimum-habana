e9a5a3b9eb50cc04a2d41a51396111e3905f86d4
Sayantan Sarkar <supersarkar@gmail.com>
2023-09-27 09:33:53 -0700

    Update optimum/habana/transformers/generation/configuration_utils.py
    Co-authored-by: regisss <15324346+regisss@users.noreply.github.com>

--- optimum/habana/transformers/generation/configuration_utils.py
+++ optimum/habana/transformers/generation/configuration_utils.py
@@ -25,7 +25,7 @@ class GaudiGenerationConfig(GenerationConfig):
         Whether to reuse key/value cache for decoding. It should save memory.
     bucket_size (`int`, *optional*):
         If negative (default=-1) pad to max if `static_shapes` is set. Else start with
-        `shape = bucket_size * ceil(prompt_len/bucket_size)` and then grow space by `bucket_size` when needed
+        `shape = bucket_size * ceil(prompt_len/bucket_size)` and then grow space by `bucket_size` when needed.
         Only active if `static_shapes` is used. Can't be used with `reuse_cache`
     """
 
