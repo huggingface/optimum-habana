2f1e056782c6adbf7dd8cfaeff90c81e9ff343d7
Libin Tang <litang@habana.ai>
2023-09-28 12:09:34 -0700

    Modify loraft llama falcon (#415)

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -37,6 +37,7 @@ from .falcon import (
     GaudiFalconForCausalLM,
     GaudiFalconModel,
     gaudi_falcon_attention_forward,
+    gaudi_falcon_attention_split_heads,
     gaudi_falcon_decoder_layer_forward,
     gaudi_falcon_rotary_embedding_forward,
 )
