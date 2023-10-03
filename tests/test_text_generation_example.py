22ad463639818148efc924fdfa5ad048a40e69c7
Mohit Deopujari <mdeopujari@habana.ai>
2023-10-02 17:55:42 -0700

    Test text generation using prompts

--- tests/test_text_generation_example.py
+++ tests/test_text_generation_example.py
@@ -1,4 +1,5 @@
 import json
+import math
 import re
 import subprocess
 from pathlib import Path
@@ -6,7 +7,7 @@ from tempfile import TemporaryDirectory
 
 import pytest
 
-from .test_examples import TIME_PERF_FACTOR
+from .test_examples import BASELINE_DIRECTORY, TIME_PERF_FACTOR
 
 
 MODELS_TO_TEST = {
@@ -27,10 +28,22 @@ MODELS_TO_TEST = {
     "deepspeed": [
         ("bigscience/bloomz-7b1", 27.34439410425298),
     ],
+    "prompt": [
+        ("bigscience/bloomz-7b1", False),
+        ("meta-llama/llama-2-13b-hf", False),
+        ("meta-llama/llama-2-70b-hf", True),
+    ],
 }
 
 
-def _test_text_generation(model_name: str, baseline: float, token: str, deepspeed: bool = False, world_size: int = 8):
+def _test_text_generation(
+    model_name: str,
+    baseline: float = math.inf,
+    token: str = "",
+    deepspeed: bool = False,
+    world_size: int = 8,
+    prompt: bool = False,
+):
     command = ["python3"]
     path_to_example_dir = Path(__file__).resolve().parent.parent / "examples"
 
@@ -43,21 +56,36 @@ def _test_text_generation(model_name: str, baseline: float, token: str, deepspee
 
     command += [
         f"{path_to_example_dir / 'text-generation' / 'run_generation.py'}",
-        f"--model_name_or_path {model_name}",
         "--batch_size 1",
         "--use_hpu_graphs",
         "--use_kv_cache",
-        "--max_new_tokens 100",
     ]
 
-    if not deepspeed:
+    if prompt or not deepspeed:
         command.append("--bf16")
+
+    if prompt:
+        path_to_baseline = BASELINE_DIRECTORY / Path(model_name.split("/")[-1].replace("-", "_")).with_suffix(".json")
+        with path_to_baseline.open("r") as json_file:
+            baseline = json.load(json_file)["prompt"]
+
+        distribution = "single_card" if not deepspeed else "deepspeed"
+
+        command += [
+            f"--max_new_tokens {baseline.get('max_new_tokens')}",
+            f"--model_name_or_path {baseline.get('model_name_or_path')}",
+            f"--prompt '{baseline.get('prompt')}'",
+            "--bucket_size 50",
+        ]
+    else:
+        command += ["--max_new_tokens 100", f"--model_name_or_path {model_name}"]
 
     with TemporaryDirectory() as tmp_dir:
         command.append(f"--output_dir {tmp_dir}")
         print(f"\n\nCommand to test: {' '.join(command)}\n")
 
-        command.append(f"--token {token.value}")
+        if not prompt:
+            command.append(f"--token {token.value}")
 
         pattern = re.compile(r"([\"\'].+?[\"\'])|\s")
         command = [x for y in command for x in re.split(pattern, y) if x]
@@ -77,7 +105,9 @@ def _test_text_generation(model_name: str, baseline: float, token: str, deepspee
             results = json.load(fp)
 
         # Ensure performance requirements (throughput) are met
-        assert results["throughput"] >= (2 - TIME_PERF_FACTOR) * baseline
+        assert results["throughput"] >= (2 - TIME_PERF_FACTOR) * baseline.get("distribution").get(distribution).get(
+            "throughput"
+        )
 
 
 @pytest.mark.parametrize("model_name, baseline", MODELS_TO_TEST["bf16"])
@@ -88,3 +118,8 @@ def test_text_generation_bf16(model_name: str, baseline: float, token: str):
 @pytest.mark.parametrize("model_name, baseline", MODELS_TO_TEST["deepspeed"])
 def test_text_generation_deepspeed(model_name: str, baseline: float, token: str):
     _test_text_generation(model_name, baseline, token, deepspeed=True)
+
+
+@pytest.mark.parametrize("model_name, deepspeed", MODELS_TO_TEST["prompt"])
+def test_text_generation_prompt(model_name: str, deepspeed: bool):
+    _test_text_generation(model_name, deepspeed=deepspeed, prompt=True)
