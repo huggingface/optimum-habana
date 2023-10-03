2793fe288fc93332cae075ebb813e655ad4f5617
Sayantan Sarkar <sasarkar@habana.ai>
2023-10-03 10:43:36 -0700

    Update comment

--- examples/text-generation/run_generation.py
+++ examples/text-generation/run_generation.py
@@ -545,7 +545,7 @@ def main():
         # Remove unused columns
         raw_dataset = raw_dataset.remove_columns([name for name in raw_dataset.column_names if name != column_name])
 
-        # Set the prompt length to args.max_input_tokens if > 0 else default to 16
+        # Set the prompt length to args.max_input_tokens if > 0 else (if 0 truncate to 16, otherwise use full length)
         prompt_length = args.max_input_tokens if args.max_input_tokens > 0 else (-1, 16)[args.max_input_tokens == 0]
 
         def preprocess_function(examples):
