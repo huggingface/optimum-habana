03ec2a5038a0394ac91f084d1bb707939f9f94b4
Vivek <vgoel@habana.ai>
2023-10-11 11:51:53 +0300

    Add gradient_checkpointing as part of loading configuration

--- examples/audio-classification/run_audio_classification.py
+++ examples/audio-classification/run_audio_classification.py
@@ -377,6 +377,7 @@ def main():
         revision=model_args.model_revision,
         token=model_args.token,
         trust_remote_code=model_args.trust_remote_code,
+        gradient_checkpointing=training_args.gradient_checkpointing,
     )
     model = AutoModelForAudioClassification.from_pretrained(
         model_args.model_name_or_path,
