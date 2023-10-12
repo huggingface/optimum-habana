f5a315bc0e7c778fab2738dc6264a3f9462107c7
Vivek <vgoel@habana.ai>
2023-10-12 11:22:56 +0300

    Changes to support hpu graphs

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -89,4 +89,5 @@ from .wav2vec2 import (
     _gaudi_wav2vec2_mask_hidden_states,
     _gaudi_wav2vec2_sample_negative_indices,
     gaudi_wav2vec2_forward,
+    gaudi_wav2vec2_encoder_forward,
 )
--- optimum/habana/transformers/models/wav2vec2/__init__.py
+++ optimum/habana/transformers/models/wav2vec2/__init__.py
@@ -3,4 +3,5 @@ from .modeling_wav2vec2 import (
     _gaudi_wav2vec2_mask_hidden_states,
     _gaudi_wav2vec2_sample_negative_indices,
     gaudi_wav2vec2_forward,
+    gaudi_wav2vec2_encoder_forward,
 )
