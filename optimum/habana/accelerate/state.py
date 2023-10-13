3c2564ef13c4e9799a0b4a3a709360488245efdd
regisss <15324346+regisss@users.noreply.github.com>
2023-10-13 23:42:48 +0200

    Switch to SynapseAI v1.12.0 (#453)

--- optimum/habana/accelerate/state.py
+++ optimum/habana/accelerate/state.py
@@ -51,7 +51,7 @@ class GaudiPartialState(PartialState):
                     if not is_deepspeed_available():
                         raise ImportError(
                             "DeepSpeed is not available, install it with: `pip install"
-                            " git+https://github.com/HabanaAI/DeepSpeed.git@1.11.0`."
+                            " git+https://github.com/HabanaAI/DeepSpeed.git@1.12.0`."
                         )
                     self.distributed_type = GaudiDistributedType.DEEPSPEED
                     if not torch.distributed.is_initialized():
