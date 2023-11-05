0a1ff342ab1fb1b67a006d2fd00b773fcb237f33
Sayantan Sarkar <sasarkar@habana.ai>
2023-11-05 10:05:37 -0800

    adjust_batch

--- examples/text-generation/run_generation.py
+++ examples/text-generation/run_generation.py
@@ -50,6 +50,21 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+def adjust_batch(batch, size):
+    curr_size = batch['input_ids'].shape[1]
+    if curr_size >= size:
+        print('Chopping batch in adjust_batch')
+        adjusted_batch = {'input_ids': batch['input_ids'][:,:size], 'attention_mask': batch['attention_mask'][:,:size]}
+    else:
+        print('Extending batch in adjust_batch')
+        adjusted_batch = {}
+        for k in batch.keys():
+            last_colm = batch[k][:,-1]
+            expanded = last_colm.tile((size-curr_size,1)).T
+            adjusted_batch[k] = torch.concat([batch[k], expanded],1)
+    assert adjusted_batch['input_ids'].shape[1] == size
+    assert adjusted_batch['attention_mask'].shape[1] == size
+    return adjusted_batch
 
 def main():
     # Arguments management
@@ -211,6 +226,12 @@ def main():
         "--reuse_cache",
         action="store_true",
         help="Whether to reuse key/value cache for decoding. It should save memory.",
+    )
+    parser.add_argument(
+        "--simulate_dyn_prompt",
+        default="",
+        type=str,
+        help="If empty static prompt is used. If a comma separated list of integers are passed, we warmup and use those shapes for prompt length",
     )
 
     args = parser.parse_args()
@@ -422,7 +443,7 @@ def main():
         elif args.batch_size < len(input_sentences):
             input_sentences = input_sentences[: args.batch_size]
 
-        def generate():
+        def generate(size=None):
             """Generates sequences from the input sentences and returns them."""
             t0 = time.perf_counter()
             print(f"Step4+ starting time is {time.perf_counter()*1000}") 
@@ -437,7 +458,8 @@ def main():
                 )
             else:
                 input_tokens = tokenizer.batch_encode_plus(input_sentences, return_tensors="pt", padding=True)
-
+            if size is not None:
+                input_tokens = adjust_batch(input_tokens, size)
             # Move inputs to target device(s)
             for t in input_tokens:
                 if torch.is_tensor(input_tokens[t]):
@@ -467,8 +489,14 @@ def main():
             logger.info("Graph compilation...")
         t0 = time.perf_counter()
         # The first three iterations take longer because of graph compilation
+        if len(args.simulate_dyn_prompt) > 0:
+            dyn_prompt_lens = [int(k) for k in args.simulate_dyn_prompt.split(',')]
+        else:
+            dyn_prompt_lens = None
+        assert len(set(dyn_prompt_lens)) == 1 # this branch is only for static prompt checking
         for _ in range(args.warmup):
-            generate()
+            print('Warming up for shape,', dyn_prompt_lens[0])
+            generate(dyn_prompt_lens[0])
         torch_hpu.synchronize()
         compilation_duration = time.perf_counter() - t0
         HabanaProfile.enable()
@@ -478,7 +506,8 @@ def main():
         t0 = time.perf_counter()
         # Benchmark over n_iterations iterations
         for i in range(args.n_iterations):
-            generated = generate()
+            print('Generating for shape,', dyn_prompt_lens[0])
+            generated = generate(dyn_prompt_lens[0])
         duration = time.perf_counter() - t0
         total_new_tokens_generated = args.n_iterations * args.batch_size * args.max_new_tokens
         throughput = total_new_tokens_generated / duration
