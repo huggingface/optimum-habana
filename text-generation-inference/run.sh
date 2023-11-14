fa02f4eb61c9afb4c1b6ef449074f8f1942224fb
Sayantan Sarkar <sasarkar@habana.ai>
2023-11-14 21:36:26 +0200

    build setup

--- text-generation-inference/run.sh
+++ text-generation-inference/run.sh
@@ -1,4 +1,4 @@
 model=bigscience/bloom-560m
 volume=/software/data/pytorch/bloom/bloom-weights/ # share a volume with the Docker container to avoid downloading weights every run
 
-docker run -p 8080:80 -v $volume:/data --runtime=habana -e HABANA_VISIBLE_DEVICES=all -e OMPI_MCA_btl_vader_single_copy_mechanism=none --cap-add=sys_nice --ipc=host tgi_gaudi_lt --model-id $model --revision ac2ae5fab2ce3f9f40dc79b5ca9f637430d24971 2>&1 | tee log.txt
+docker run -p 8082:80 -td --name sarkar_tgi -v $volume:/data -v /home/sasarkar/:/sarkar --runtime=habana -e HABANA_VISIBLE_DEVICES=all -e OMPI_MCA_btl_vader_single_copy_mechanism=none --cap-add=sys_nice --ipc=host tgi_gaudi_ss 2>&1 | tee log.txt
