e895124f2a2d1d2ac3c077f90105f002ef239fb0
Sayantan Sarkar <sasarkar@habana.ai>
2023-12-11 09:32:29 -0800

    Review cmnt

--- optimum/habana/transformers/generation/utils.py
+++ optimum/habana/transformers/generation/utils.py
@@ -582,9 +582,9 @@ class GaudiGenerationMixin(GenerationMixin):
             or self._get_generation_mode(generation_config, assistant_model) == GenerationMode.BEAM_SEARCH
         )
         model_kwargs["bucket_size"] = generation_config.bucket_size if generation_config.static_shapes else -1
-        model_kwargs["reduce_recompile"] = (generation_config.reduce_recompile, False)[
-            generation_config.reduce_recompile is None
-        ]
+        model_kwargs["reduce_recompile"] = (
+            generation_config.reduce_recompile if generation_config.reduce_recompile is not None else False
+        )
         if model_kwargs["reduce_recompile"]:
             assert generation_config.bucket_size
         if generation_config.reuse_cache:
