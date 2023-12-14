46d3b8151e2ee4bb0b202240ba5a419a559828f1
sjagtap1803 <siddhant.jagtap@intel.com>
2023-12-14 19:10:14 +0000

    refactored pipeline scripts to incorporate utils

--- examples/text-generation/textgen_pipeline/pipeline.py
+++ examples/text-generation/textgen_pipeline/pipeline.py
@@ -1,27 +1,15 @@
-import copy
 import os
-import tempfile
+import sys
 
-import habana_frameworks.torch.hpu as torch_hpu
 import torch
-from habana_frameworks.torch.distributed.hccl import initialize_distributed_hpu
-from habana_frameworks.torch.hpu import wrap_in_hpu_graph
-from transformers import (
-    AutoConfig,
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    TextGenerationPipeline,
-)
+from transformers import TextGenerationPipeline
 
-from optimum.habana.checkpoint_utils import (
-    get_ds_injection_policy,
-    get_repo_root,
-    model_is_optimized,
-    model_on_meta,
-    write_checkpoints_json,
-)
-from optimum.habana.transformers.modeling_utils import adapt_transformers_to_gaudi
-from optimum.habana.utils import set_seed
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+try:
+    from utils import initialize_model
+except:
+    raise
 
 
 class GaudiTextGenerationPipeline(TextGenerationPipeline):
@@ -29,113 +17,26 @@ class GaudiTextGenerationPipeline(TextGenerationPipeline):
     An end-to-end text-generation pipeline that can used to initialize LangChain classes. It supports both single-hpu and multi-hpu inference.
     """
 
-    def __init__(self, model_name_or_path=None, bf16=True, **kwargs):
-        self.use_deepspeed = "deepspeed" in os.environ["_"]
+    def __init__(self, args, logger):
+        self.model, self.tokenizer, self.generation_config = initialize_model(args, logger)
 
-        if self.use_deepspeed:
-            world_size, _, self.local_rank = initialize_distributed_hpu()
+        self.device = args.device
 
-            import deepspeed
+        if args.do_sample:
+            self.generation_config.temperature = args.temperature
+            self.generation_config.top_p = args.top_p
 
-            # Initialize Deepspeed processes
-            deepspeed.init_distributed(dist_backend="hccl")
+        self.max_padding_length = args.max_input_tokens if args.max_input_tokens > 0 else 100
+        self.use_hpu_graphs = args.use_hpu_graphs
+        self.profiling_steps = args.profiling_steps
+        self.profiling_warmup_steps = args.profiling_warmup_steps
 
-        self.task = "text-generation"
-        self.device = "hpu"
+        import habana_frameworks.torch.hpu as torch_hpu
 
-        # Tweak generation so that it runs faster on Gaudi
-        adapt_transformers_to_gaudi()
-        set_seed(27)
-
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-
-        if self.use_deepspeed or bf16:
-            model_dtype = torch.bfloat16
-        else:
-            model_dtype = torch.float
-
-        if self.use_deepspeed:
-            config = AutoConfig.from_pretrained(model_name_or_path)
-            is_optimized = model_is_optimized(config)
-            load_to_meta = model_on_meta(config)
-
-            if load_to_meta:
-                # Construct model with fake meta tensors, later will be replaced on devices during ds-inference ckpt load
-                with deepspeed.OnDevice(dtype=model_dtype, device="meta"):
-                    model = AutoModelForCausalLM.from_config(config, torch_dtype=model_dtype)
-            else:
-                get_repo_root(model_name_or_path, local_rank=self.local_rank)
-                # placement on cpu if meta tensors are not supported
-                with deepspeed.OnDevice(dtype=model_dtype, device="cpu"):
-                    model = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_dtype=model_dtype)
-            model = model.eval()
-
-            # Initialize the model
-            ds_inference_kwargs = {"dtype": model_dtype}
-            ds_inference_kwargs["tensor_parallel"] = {"tp_size": world_size}
-            ds_inference_kwargs["enable_cuda_graph"] = True
-
-            if load_to_meta:
-                # model loaded to meta is managed differently
-                checkpoints_json = tempfile.NamedTemporaryFile(suffix=".json", mode="+w")
-                write_checkpoints_json(model_name_or_path, self.local_rank, checkpoints_json)
-
-            # Make sure all devices/nodes have access to the model checkpoints
-            torch.distributed.barrier()
-
-            ds_inference_kwargs["injection_policy"] = get_ds_injection_policy(config)
-            if load_to_meta:
-                ds_inference_kwargs["checkpoint"] = checkpoints_json.name
-
-            model = deepspeed.init_inference(model, **ds_inference_kwargs)
-            model = model.module
-        else:
-            get_repo_root(model_name_or_path)
-            model = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_dtype=model_dtype)
-            model = model.eval().to(self.device)
-            is_optimized = model_is_optimized(model.config)
-            model = wrap_in_hpu_graph(model)
-
-        self.model = model
-
-        # Used for padding input to fixed length
-        self.tokenizer.padding_side = "left"
-        self.max_padding_length = kwargs.get("max_padding_length", self.model.config.max_position_embeddings)
-
-        # Define config params for llama models
-        if self.model.config.model_type == "llama":
-            self.model.generation_config.pad_token_id = 0
-            self.model.generation_config.bos_token_id = 1
-            self.model.generation_config.eos_token_id = 2
-            self.tokenizer.bos_token_id = self.model.generation_config.bos_token_id
-            self.tokenizer.eos_token_id = self.model.generation_config.eos_token_id
-            self.tokenizer.pad_token_id = self.model.generation_config.pad_token_id
-            self.tokenizer.pad_token = self.tokenizer.decode(self.tokenizer.pad_token_id)
-            self.tokenizer.eos_token = self.tokenizer.decode(self.tokenizer.eos_token_id)
-            self.tokenizer.bos_token = self.tokenizer.decode(self.tokenizer.bos_token_id)
-
-        # Applicable to models that do not have pad tokens
-        if self.tokenizer.pad_token is None:
-            self.tokenizer.pad_token = self.tokenizer.eos_token
-            self.model.generation_config.pad_token_id = self.model.generation_config.eos_token_id
-
-        # Edit generation configuration based on input arguments
-        self.generation_config = copy.deepcopy(self.model.generation_config)
-        self.generation_config.max_new_tokens = kwargs.get("max_new_tokens", 100)
-        self.generation_config.use_cache = kwargs.get("use_kv_cache", True)
-        self.generation_config.static_shapes = is_optimized
-        self.generation_config.do_sample = kwargs.get("do_sample", False)
-        self.generation_config.num_beams = kwargs.get("num_beams", 1)
-        self.generation_config.temperature = kwargs.get("temperature", 1.0)
-        self.generation_config.top_p = kwargs.get("top_p", 1.0)
-        self.generation_config.repetition_penalty = kwargs.get("repetition_penalty", 1.0)
-        self.generation_config.num_return_sequences = kwargs.get("num_return_sequences", 1)
-        self.generation_config.bad_words_ids = None
-        self.generation_config.force_words_ids = None
-        self.generation_config.ignore_eos = False
-
-        if self.use_deepspeed:
-            torch.distributed.barrier()
+        # Compile Graph
+        for _ in range(3):
+            self("Here is my prompt")
+        torch_hpu.synchronize()
 
     def __call__(self, prompt: str):
         model_inputs = self.tokenizer.encode_plus(
@@ -150,71 +51,10 @@ class GaudiTextGenerationPipeline(TextGenerationPipeline):
             **model_inputs,
             generation_config=self.generation_config,
             lazy_mode=True,
-            hpu_graphs=True,
-            profiling_steps=0,
-            profiling_warmup_steps=0,
+            hpu_graphs=self.use_hpu_graphs,
+            profiling_steps=self.profiling_steps,
+            profiling_warmup_steps=self.profiling_warmup_steps,
         ).cpu()
 
         output_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
-        del output, model_inputs
-
-        return [{"generated_text": output_text}]
-
-    def get_process_rank(self):
-        """
-        Function that returns process id during distributed inference.
-        """
-        if self.use_deepspeed:
-            return self.local_rank
-        return -1
-
-    def compile_graph(self):
-        """
-        Function to compile computation graphs and synchronize hpus.
-        """
-        for _ in range(3):
-            self("Here is my prompt")
-        torch_hpu.synchronize()
-
-
-def main():
-    pipe = GaudiTextGenerationPipeline(
-        model_name_or_path="meta-llama/Llama-2-7b-chat-hf",
-        max_new_tokens=100,
-        temperature=0.2,
-        top_p=0.95,
-        repetition_penalty=1.15,
-        do_sample=True,
-    )
-    pipe.compile_graph()
-
-    # Test model on different input prompts
-    print("Test 1: short prompt")
-    print(pipe("Once upon a time"))
-    print("Success!\n")
-
-    print("Test 2: long prompt")
-    print(
-        pipe(
-            "Antibiotics are a type of medication used to treat bacterial infections. They work by either killing the bacteria or preventing them from reproducing, allowing the body’s immune system to fight off the infection. Antibiotics are usually taken orally in the form of pills, capsules, or liquid solutions, or sometimes administered intravenously. They are not effective against viral infections, and using them"
-        )
-    )
-    print("Success!\n")
-
-    print("Test 3: qa prompt")
-    print(
-        pipe(
-            """Answer the question based on the context below. If the question cannot be answered using the information provided answer with "I don't know".
-
-Context: Large Language Models (LLMs) are the latest models used in NLP. Their superior performance over smaller models has made them incredibly useful for developers building NLP enabled applications. These models can be accessed via Hugging Face's `transformers` library, via OpenAI using the `openai` library, and via Cohere using the `cohere` library.
-
-Question: Which libraries and model providers offer LLMs?
-
-Answer: """
-        )
-    )
-    print("Success!")
-
-
-if __name__ == "__main__":
-    main()
+        return output_text
--- examples/text-generation/textgen_pipeline/run_pipeline.py
+++ examples/text-generation/textgen_pipeline/run_pipeline.py
@@ -1,9 +1,21 @@
 import argparse
+import logging
+import time
 
 from pipeline import GaudiTextGenerationPipeline
 
 
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+
 def setup_parser(parser):
+    # Arguments management
+    parser.add_argument("--device", "-d", type=str, choices=["hpu"], help="Device to run", default="hpu")
     parser.add_argument(
         "--model_name_or_path",
         default=None,
@@ -17,17 +29,177 @@ def setup_parser(parser):
         help="Whether to perform generation in bf16 precision.",
     )
     parser.add_argument("--max_new_tokens", type=int, default=100, help="Number of tokens to generate.")
-    parser.add_argument("--temperature", type=float, default=1.0, help="Temperature value for text generation.")
-    parser.add_argument("--top_p", type=float, default=1.0, help="Top_p value for sampling output probabilities.")
     parser.add_argument(
-        "--repetition_penalty", type=float, default=1.0, help="Repetition penalty for text generation."
+        "--max_input_tokens",
+        type=int,
+        default=0,
+        help="If > 0 then pad and truncate the input sequences to this specified length of tokens. \
+            if == 0, then truncate to 16 (original default) \
+            if < 0, then do not truncate, use full input prompt",
+    )
+    parser.add_argument("--batch_size", type=int, default=1, help="Input batch size.")
+    parser.add_argument("--warmup", type=int, default=3, help="Number of warmup iterations for benchmarking.")
+    parser.add_argument("--n_iterations", type=int, default=5, help="Number of inference iterations for benchmarking.")
+    parser.add_argument("--local_rank", type=int, default=0, metavar="N", help="Local process rank.")
+    parser.add_argument(
+        "--use_kv_cache",
+        action="store_true",
+        help="Whether to use the key/value cache for decoding. It should speed up generation.",
     )
     parser.add_argument(
-        "--do_sample", action="store_true", help="Whether to perfrom sampling instead of greedy decoding."
+        "--use_hpu_graphs",
+        action="store_true",
+        help="Whether to use HPU graphs or not. Using HPU graphs should give better latencies.",
     )
-    parser.add_argument("--prompt", type=str, required=True, help="Input prompt for text generation.")
+    parser.add_argument(
+        "--dataset_name",
+        default=None,
+        type=str,
+        help="Optional argument if you want to assess your model on a given dataset of the HF Hub.",
+    )
+    parser.add_argument(
+        "--column_name",
+        default=None,
+        type=str,
+        help="If `--dataset_name` was given, this will be the name of the column to use as prompts for generation.",
+    )
+    parser.add_argument(
+        "--do_sample",
+        action="store_true",
+        help="Whether to use sampling for generation.",
+    )
+    parser.add_argument(
+        "--num_beams",
+        default=1,
+        type=int,
+        help="Number of beams used for beam search generation. 1 means greedy search will be performed.",
+    )
+    parser.add_argument(
+        "--trim_logits",
+        action="store_true",
+        help="Calculate logits only for the last token to save memory in the first step.",
+    )
+    parser.add_argument(
+        "--seed",
+        default=27,
+        type=int,
+        help="Seed to use for random generation. Useful to reproduce your runs with `--do_sample`.",
+    )
+    parser.add_argument(
+        "--profiling_warmup_steps",
+        default=0,
+        type=int,
+        help="Number of steps to ignore for profiling.",
+    )
+    parser.add_argument(
+        "--profiling_steps",
+        default=0,
+        type=int,
+        help="Number of steps to capture for profiling.",
+    )
+    parser.add_argument(
+        "--prompt",
+        default=None,
+        type=str,
+        nargs="*",
+        help='Optional argument to give a prompt of your choice as input. Can be a single string (eg: --prompt "Hello world"), or a list of space-separated strings (eg: --prompt "Hello world" "How are you?")',
+    )
+    parser.add_argument(
+        "--bad_words",
+        default=None,
+        type=str,
+        nargs="+",
+        help="Optional argument list of words that are not allowed to be generated.",
+    )
+    parser.add_argument(
+        "--force_words",
+        default=None,
+        type=str,
+        nargs="+",
+        help="Optional argument list of words that must be generated.",
+    )
+    parser.add_argument(
+        "--peft_model",
+        default=None,
+        type=str,
+        help="Optional argument to give a path to a PEFT model.",
+    )
+    parser.add_argument("--num_return_sequences", type=int, default=1)
+    parser.add_argument(
+        "--token",
+        default=None,
+        type=str,
+        help="The token to use as HTTP bearer authorization for remote files. If not specified, will use the token "
+        "generated when running `huggingface-cli login` (stored in `~/.huggingface`).",
+    )
+    parser.add_argument(
+        "--model_revision",
+        default="main",
+        type=str,
+        help="The specific model version to use (can be a branch name, tag name or commit id).",
+    )
+    parser.add_argument(
+        "--attn_softmax_bf16",
+        action="store_true",
+        help="Whether to run attention softmax layer in lower precision provided that the model supports it and "
+        "is also running in lower precision.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        default=None,
+        type=str,
+        help="Output directory to store results in.",
+    )
+    parser.add_argument(
+        "--bucket_size",
+        default=-1,
+        type=int,
+        help="Bucket size to maintain static shapes. If this number is negative (default is -1) \
+            then we use `shape = prompt_length + max_new_tokens`. If a positive number is passed \
+            we increase the bucket in steps of `bucket_size` instead of allocating to max (`prompt_length + max_new_tokens`).",
+    )
+    parser.add_argument(
+        "--dataset_max_samples",
+        default=-1,
+        type=int,
+        help="If a negative number is passed (default = -1) perform inference on the whole dataset, else use only `dataset_max_samples` samples.",
+    )
+    parser.add_argument(
+        "--limit_hpu_graphs",
+        action="store_true",
+        help="Skip HPU Graph usage for first token to save memory",
+    )
+    parser.add_argument(
+        "--reuse_cache",
+        action="store_true",
+        help="Whether to reuse key/value cache for decoding. It should save memory.",
+    )
+    parser.add_argument(
+        "--skip_hash_with_views",
+        action="store_true",
+        help="Whether to skip hash with views for HPU graphs. When skip_hash_with_views is not used, the input to HPU graphs includes both view and base tensors.",
+    )
+    parser.add_argument("--verbose_workers", action="store_true", help="Enable output from non-master workers")
+
+    parser.add_argument(
+        "--kv_cache_fp8",
+        action="store_true",
+        help="Store kv-cache in float8 when kv-cache is used",
+    )
+    parser.add_argument("--fp8", action="store_true", help="Enable Quantization to fp8")
+    parser.add_argument(
+        "--use_flash_attention",
+        action="store_true",
+        help="Whether to enable Habana Flash Attention, provided that the model supports it.",
+    )
+    parser.add_argument("--temperature", default=1.0, type=float, help="Temperature value for text generation")
+    parser.add_argument("--top_p", default=1.0, type=float, help="Top_p value for generating text via sampling")
 
     args = parser.parse_args()
+
+    if not args.use_hpu_graphs:
+        args.limit_hpu_graphs = False
+
     return args
 
 
@@ -35,28 +207,31 @@ def main():
     parser = argparse.ArgumentParser()
     args = setup_parser(parser)
 
-    if args.do_sample:
-        pipe = GaudiTextGenerationPipeline(
-            model_name_or_path=args.model_name_or_path,
-            bf16=args.bf16,
-            max_new_tokens=args.max_new_tokens,
-            temperature=args.temperature,
-            top_p=args.top_p,
-            repetition_penalty=args.repetition_penalty,
-            do_sample=True,
-        )
+    if args.prompt:
+        input_sentences = args.prompt
     else:
-        pipe = GaudiTextGenerationPipeline(
-            model_name_or_path=args.model_name_or_path,
-            bf16=args.bf16,
-            max_new_tokens=args.max_new_tokens,
-            repetition_penalty=args.repetition_penalty,
-        )
-    pipe.compile_graph()
+        input_sentences = [
+            "DeepSpeed is a machine learning framework",
+            "He is working on",
+            "He has a",
+            "He got all",
+            "Everyone is happy and I can",
+            "The new movie that got Oscar this year",
+            "In the far far distance from our galaxy,",
+            "Peace is the only way",
+        ]
 
-    print(f"Prompt: {args.prompt}")
-    for i in range(10):
-        print(f"Response {i+1}: {pipe(args.prompt)}")
+    pipe = GaudiTextGenerationPipeline(args, logger)
+
+    for input_sentence in input_sentences:
+        print(f"Prompt: {input_sentence}")
+        t0 = time.perf_counter()
+        output = pipe(input_sentence)
+        duration = time.perf_counter() - t0
+        throughput = args.max_new_tokens / duration
+        print(f"Generated Text: {repr(output)}")
+        print(f"Inference Duration: {duration} seconds")
+        print(f"Throughput: {throughput} tokens/second")
 
 
 if __name__ == "__main__":
