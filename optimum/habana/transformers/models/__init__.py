08454618a2c1b195c9d417352eb72cce4ad8aa0c
Wang, Yi A <yi.a.wang@intel.com>
2024-01-21 20:12:18 -0800

    add optimization for blip text model generation
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -10,6 +10,15 @@ from .bart import (
     gaudi_BartLearnedPositionalEmbedding,
     gaudi_BartModel_forward,
 )
+from .blip import (
+    gaudi_BlipTextAttention_forward,
+    gaudi_BlipTextEncoder_forward,
+    gaudi_BlipTextLayer_forward,
+    gaudi_BlipTextLMHead_forward,
+    gaudi_BlipTextLMHead_prepare_inputs_for_generation,
+    gaudi_BlipTextModel_forward,
+    gaudi_BlipTextSelfAttention_forward,
+)
 from .bloom import (
     GaudiBloomForCausalLM,
     GaudiBloomMLP,
---
+++ optimum/habana/transformers/models/blip/__init__.py
@@ -0,0 +1,9 @@
+from .modeling_blip_text import (
+    gaudi_BlipTextAttention_forward,
+    gaudi_BlipTextEncoder_forward,
+    gaudi_BlipTextLayer_forward,
+    gaudi_BlipTextLMHead_forward,
+    gaudi_BlipTextLMHead_prepare_inputs_for_generation,
+    gaudi_BlipTextModel_forward,
+    gaudi_BlipTextSelfAttention_forward,
+)
