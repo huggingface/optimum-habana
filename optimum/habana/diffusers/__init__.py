937a411fc59b3a926f4894876929b3b60f4c634b
Neelesh Gokhale <neelesh.gokhale@intel.com>
2024-01-30 18:53:14 +0530

    Add ControlNet Pipeline (#585)

--- optimum/habana/diffusers/__init__.py
+++ optimum/habana/diffusers/__init__.py
@@ -1,3 +1,4 @@
+from .pipelines.controlnet.pipeline_controlnet import GaudiStableDiffusionControlNetPipeline
 from .pipelines.pipeline_utils import GaudiDiffusionPipeline
 from .pipelines.stable_diffusion.pipeline_stable_diffusion import GaudiStableDiffusionPipeline
 from .pipelines.stable_diffusion.pipeline_stable_diffusion_ldm3d import GaudiStableDiffusionLDM3DPipeline
