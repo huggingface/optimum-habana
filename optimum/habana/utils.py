44be55566c07561a9de6589f5822057b5a434d4a
Vidya Galli <vidya.s.galli@intel.com>
2024-02-06 21:14:24 -0800

    Fix version check when console output is disabled (#688)

--- optimum/habana/utils.py
+++ optimum/habana/utils.py
@@ -222,14 +222,11 @@ def get_driver_version():
     """
     Returns the driver version.
     """
+    # Enable console printing for `hl-smi` check
     output = subprocess.run(
-        "hl-smi",
-        shell=True,
-        text=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
+        "hl-smi", shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env={"ENABLE_CONSOLE": "true"}
     )
-    if output.returncode == 0:
+    if output.returncode == 0 and output.stdout:
         return version.parse(output.stdout.split("\n")[2].replace(" ", "").split(":")[1][:-1].split("-")[0])
     return None
 
