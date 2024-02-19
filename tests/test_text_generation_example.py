1ed69043187c4fa8fc99ee718d179962079d4412
Kalyan Kumar <kalyanjkk@gmail.com>
2024-02-19 21:13:10 +0530

    Enable torch_compile mode for distributed (#659)

--- tests/test_text_generation_example.py
+++ tests/test_text_generation_example.py
@@ -33,6 +33,9 @@ if os.environ.get("GAUDI2_CI", "0") == "1":
         "torch_compile": [
             ("meta-llama/Llama-2-7b-hf", 12.468247401430999),
         ],
+        "torch_compile_distributed": [
+            ("meta-llama/Llama-2-7b-hf", 20.178927030275947),
+        ],
     }
 else:
     # Gaudi1 CI baselines
@@ -54,6 +57,7 @@ else:
             ("bigscience/bloomz-7b1", 31.044523676681507),
         ],
         "torch_compile": [],
+        "torch_compile_distributed": [],
     }
 
 
@@ -68,7 +72,6 @@ def _test_text_generation(
     command = ["python3"]
     path_to_example_dir = Path(__file__).resolve().parent.parent / "examples"
 
-    deepspeed = deepspeed and not torch_compile
     if deepspeed:
         command += [
             f"{path_to_example_dir / 'gaudi_spawn.py'}",
@@ -143,3 +146,11 @@ def test_text_generation_torch_compile(model_name: str, baseline: float, token: 
     os.environ["PT_HPU_LAZY_MODE"] = "0"
     os.environ["WORLD_SIZE"] = "0"
     _test_text_generation(model_name, baseline, token, torch_compile=True)
+
+
+@pytest.mark.parametrize("model_name, baseline", MODELS_TO_TEST["torch_compile_distributed"])
+def test_text_generation_torch_compile_distributed(model_name: str, baseline: float, token: str):
+    world_size = 8
+    os.environ["PT_ENABLE_INT64_SUPPORT"] = "1"
+    os.environ["PT_HPU_LAZY_MODE"] = "0"
+    _test_text_generation(model_name, baseline, token, deepspeed=True, world_size=world_size, torch_compile=True)
