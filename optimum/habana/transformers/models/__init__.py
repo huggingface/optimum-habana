24a76f2ad6bb448150826253dfb71cc51e5f8ecb
Yaser Afshar <ya.afshar@gmail.com>
2024-02-19 23:17:57 -0800

    Update the Mistral model naming (#726)

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -78,7 +78,7 @@ from .llama import (
 )
 from .mistral import (
     GaudiMistralForCausalLM,
-    gaudi_mistral_attn_forward,
+    gaudi_mistral_attention_forward,
     gaudi_mistral_decoder_layer_forward,
     gaudi_mistral_model_forward,
 )
