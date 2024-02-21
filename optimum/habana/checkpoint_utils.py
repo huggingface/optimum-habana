2619e264b4ccc6385847a75fe5c328dd2f117374
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2024-02-21 09:52:54 -0700

    make style

--- optimum/habana/checkpoint_utils.py
+++ optimum/habana/checkpoint_utils.py
@@ -81,7 +81,10 @@ def get_checkpoint_files(model_name_or_path, local_rank, token=None):
     safe_index_present = os.path.isfile(safe_index_file)
 
     if not index_present and not safe_index_present:
-        filenames = (transformers.modeling_utils.WEIGHTS_INDEX_NAME, transformers.modeling_utils.SAFE_WEIGHTS_INDEX_NAME)
+        filenames = (
+            transformers.modeling_utils.WEIGHTS_INDEX_NAME,
+            transformers.modeling_utils.SAFE_WEIGHTS_INDEX_NAME,
+        )
         raise ValueError(f"Can't find a checkpoint index ({' or '.join(filenames)}) in {cached_repo_dir}.")
 
     load_index = safe_index_file if safe_index_present else index_file
