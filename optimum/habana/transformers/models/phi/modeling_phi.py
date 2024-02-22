f7f652c29c25af06551dec98b655ca040347ca42
Lv, Kaokao <kaokao.lv@intel.com>
2024-02-23 00:54:24 +0800

    remove useless import.

--- optimum/habana/transformers/models/phi/modeling_phi.py
+++ optimum/habana/transformers/models/phi/modeling_phi.py
@@ -23,8 +23,6 @@ import math
 from typing import List, Optional, Tuple, Union
 
 import torch
-import torch.nn.functional as F
-import torch.utils.checkpoint
 from torch import nn
 from torch.nn import CrossEntropyLoss
 
