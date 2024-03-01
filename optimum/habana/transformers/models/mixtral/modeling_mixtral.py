3796e05f702c0674a4fd0776f2b20e6719f886ad
regisss <15324346+regisss@users.noreply.github.com>
2024-03-01 01:59:23 +0000

    Make style

--- optimum/habana/transformers/models/mixtral/modeling_mixtral.py
+++ optimum/habana/transformers/models/mixtral/modeling_mixtral.py
@@ -18,7 +18,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" PyTorch Mixtral model."""
+"""PyTorch Mixtral model."""
+
 import math
 import warnings
 from typing import List, Optional, Tuple, Union
