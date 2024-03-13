5d2c23ce250b006042bb895409c1c43ae7b79dc0
XinyuYe-Intel <xinyu.ye@intel.com>
2024-03-13 09:24:02 +0800

    Update Copyright year.

--- optimum/habana/transformers/models/qwen2/modeling_qwen2.py
+++ optimum/habana/transformers/models/qwen2/modeling_qwen2.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2023 The HuggingFace Team. All rights reserved.
+# Copyright 2024 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###############################################################################
-# Copyright (C) 2022-2023 Habana Labs, Ltd. an Intel Company
+# Copyright (C) 2022-2024 Habana Labs, Ltd. an Intel Company
 ###############################################################################
 
 import math
