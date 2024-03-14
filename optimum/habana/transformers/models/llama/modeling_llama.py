ad1e5f5269dfaba1e808c58b2b45b2f0b8ec81b5
Wang, Yi A <yi.a.wang@intel.com>
2024-03-14 02:36:46 -0700

    add prompt tuning/prefix tuning and p tuning support for llama. also add an example for it
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- optimum/habana/transformers/models/llama/modeling_llama.py
+++ optimum/habana/transformers/models/llama/modeling_llama.py
@@ -52,8 +52,7 @@ def update(prev, cur, dim, idx, inp_seq_len):
         # Initialize
         prev[:, :, :inp_seq_len, :].copy_(cur)
         return orig_cur
-    assert cur.shape[2] == 1, f"Cannot update kv-cache. Unsupported shapes. prev:{prev.shape} cur:{cur.shape}"
-    if idx is not None:
+    if idx is not None and prev.shape[-2] >= idx:
         prev.index_copy_(dim, idx - 1, cur)
         prev_cast = prev.to(orig_cur.dtype)
         return prev_cast
@@ -356,7 +355,7 @@ class GaudiLlamaAttention(LlamaAttention):
 
         kv_seq_len = key_states.shape[-2]
         if past_key_value is not None:
-            if token_idx is None:
+            if token_idx is None or past_key_value[0].shape[-2] < token_idx:
                 if hasattr(past_key_value, "get_usable_length"):
                     kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
                 else:
