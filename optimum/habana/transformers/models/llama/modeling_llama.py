300e2ef91984eff83feff9df45000e2b6f8cd2b6
Kalyan Kumar <kalyanjkk@gmail.com>
2024-03-14 11:56:38 +0530

    Added additional check to run with distributed enabled and  world_size=1 (#96)
    * Added additionla check to run with distributed enabled and  world_size = 1
    
    * Reduce the number of graph splits to avoid memory allocation error
    for 1x LLAMA1_7b_ft
    
    ---------
    
    Co-authored-by: Kalyan <kkumar@habana.ai>

--- optimum/habana/transformers/models/llama/modeling_llama.py
+++ optimum/habana/transformers/models/llama/modeling_llama.py
@@ -758,7 +758,8 @@ class GaudiLlamaModel(LlamaModel):
         next_decoder_cache = () if not use_new_cache else None
 
         for layer_idx, decoder_layer in enumerate(self.layers):
-            if lazy_mode and torch.distributed.is_initialized() == False:
+            if lazy_mode and use_flash_attention and \
+                (torch.distributed.is_initialized() is False or torch.distributed.get_world_size() == 1):
                 htcore.mark_step()
 
             if output_hidden_states:
