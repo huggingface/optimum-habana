607c356ce8276cd76eb8c1ae7c9a9821a5cec9b0
regisss <15324346+regisss@users.noreply.github.com>
2024-03-14 15:19:07 +0000

    [skip ci] Add model to Gaudi2 CI

--- tests/test_text_generation_example.py
+++ tests/test_text_generation_example.py
@@ -25,6 +25,7 @@ if os.environ.get("GAUDI2_CI", "0") == "1":
             ("mosaicml/mpt-30b", 35.64501131267502),
             ("mistralai/Mistral-7B-v0.1", 125.26115369093216),
             ("mistralai/Mixtral-8x7B-v0.1", 23.78652574031883),
+            ("microsoft/phi-2", 218.08752713569007),
         ],
         "deepspeed": [
             ("bigscience/bloomz", 36.34664210641816),
