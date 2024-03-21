a01d17560cb3795a70b761fd831e199556b94610
Kalyan Kumar <kalyanjkk@gmail.com>
2024-03-21 09:10:43 +0530

    Add mark_step only for inference (#126)
    Co-authored-by: Kalyan <kkumar@habana.ai>

--- optimum/habana/transformers/models/llama/modeling_llama.py
+++ optimum/habana/transformers/models/llama/modeling_llama.py
@@ -696,7 +696,7 @@ class GaudiLlamaModel(LlamaModel):
             htcore.mark_step()
 
         for layer_idx, decoder_layer in enumerate(self.layers):
-            if lazy_mode and use_flash_attention and \
+            if lazy_mode and not self.training and \
                 (torch.distributed.is_initialized() is False or torch.distributed.get_world_size() == 1):
                 htcore.mark_step()
 
