44388aa36e6c03afc303bf812996143443d3a264
Sayantan Sarkar <supersarkar@gmail.com>
2024-03-21 16:21:23 -0700

    Mistral optimizations (#804)
    Co-authored-by: Libin Tang <litang@habana.ai>

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -81,10 +81,11 @@ from .llama import (
     gaudi_llama_rmsnorm_forward,
 )
 from .mistral import (
+    GaudiMistralAttention,
+    GaudiMistralDecoderLayer,
     GaudiMistralForCausalLM,
-    gaudi_mistral_attention_forward,
-    gaudi_mistral_decoder_layer_forward,
-    gaudi_mistral_model_forward,
+    GaudiMistralModel,
+    gaudi_mistral_rmsnorm_forward,
 )
 from .mixtral import (
     GaudiMixtralForCausalLM,
