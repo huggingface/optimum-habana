1d067aced606b7ad4b30b7fa7fe04bdc49ccdb40
Libin Tang <litang@habana.ai>
2024-03-29 18:12:01 +0000

    Fix the pytest for llama and falcon when token_idx is None.

--- optimum/habana/transformers/models/falcon/modeling_falcon.py
+++ optimum/habana/transformers/models/falcon/modeling_falcon.py
@@ -334,16 +334,12 @@ class GaudiFalconAttention(FalconAttention):
                             device=self.query_key_value.weight.device,
                         )
                         layer_past = (past_key, past_value)
-                        key_layer = self.k_cache.update(
-                            layer_past[0], key_layer, -2, token_idx, self.inp_seq_len
-                        )  # k_layer bs*1, q_len, head_dim
-                        value_layer = self.v_cache.update(layer_past[1], value_layer, -2, token_idx, self.inp_seq_len)
-                    else:
-                        key_layer = self.k_cache.update(
-                            layer_past[0], key_layer, -2, token_idx, self.inp_seq_len
-                        )  # k_layer bs*1, q_len, head_dim
-                        value_layer = self.v_cache.update(layer_past[1], value_layer, -2, token_idx, self.inp_seq_len)
-                        layer_past = (key_layer.contiguous(), value_layer.contiguous())
+                    key_layer = self.k_cache.update(
+                        layer_past[0], key_layer, -2, token_idx, self.inp_seq_len
+                    )  # k_layer bs*1, q_len, head_dim
+                    value_layer = self.v_cache.update(layer_past[1], value_layer, -2, token_idx, self.inp_seq_len)
+                    if token_idx is None:
+                        layer_past = (key_layer, value_layer)
                     present = layer_past
 
                 if cache_idx is not None and query_length == 1:
