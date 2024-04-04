e5717addbb9d14c6033fdeff56c4c90445d4c326
Libin Tang <litang@habana.ai>
2024-04-04 07:40:17 +0300

    make style fix.

--- examples/text-generation/utils.py
+++ examples/text-generation/utils.py
@@ -105,13 +105,17 @@ def setup_inference(args, model):
         htcore.hpu_initialize(model)
     return model
 
+
 def setup_const_serialization(const_serialization_path):
     import uuid
-    const_serialization_path = os.path.join(const_serialization_path  + uuid.uuid4().hex)
+
+    const_serialization_path = os.path.join(const_serialization_path + uuid.uuid4().hex)
     os.makedirs(const_serialization_path)
     from habana_frameworks.torch.hpu import enable_const_section_serialization
+
     print("Serializing const params to {}".format(const_serialization_path))
     enable_const_section_serialization(const_serialization_path, True)
+
 
 def setup_env(args):
     # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
