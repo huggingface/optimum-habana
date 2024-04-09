35501c1a4a0beb5a3c60553cdaa1b2752328165e
Raymond Lau <kwun.fung.lau@intel.com>
2024-04-09 21:07:15 +0000

    Added an example code to demonstrate how to use deterministic image generation on Gaudi platform.

---
+++ examples/stable-diffusion/deterministic_image_generation.py
@@ -0,0 +1,240 @@
+import argparse
+import logging
+import sys
+from pathlib import Path
+
+import numpy as np
+import torch
+
+from optimum.habana.diffusers import (
+    GaudiDDIMScheduler,
+    GaudiEulerAncestralDiscreteScheduler,
+    GaudiEulerDiscreteScheduler,
+)
+from optimum.habana.utils import set_seed
+from optimum.habana.diffusers import GaudiStableDiffusionPipeline
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--model_name_or_path",
+        default="runwayml/stable-diffusion-v1-5",
+        type=str,
+        help="Path/Name of the pre-trained model",
+    )
+
+    parser.add_argument(
+        "--scheduler",
+        default="ddim",
+        choices=["euler_discrete", "euler_ancestral_discrete", "ddim"],
+        type=str,
+        help="Name of scheduler",
+    )
+
+    parser.add_argument(
+        "--timestep_spacing",
+        default="linspace",
+        choices=["linspace", "leading", "trailing"],
+        type=str,
+        help="The way the timesteps should be scaled.",
+    )
+    # Pipeline arguments
+    parser.add_argument(
+        "--prompts",
+        type=str,
+        nargs="*",
+        default="An image of a squirrel in Picasso style",
+        help="The prompt or prompts to guide the image generation.",
+    )
+    parser.add_argument(
+        "--prompts_2",
+        type=str,
+        nargs="*",
+        default=None,
+        help="The second prompt or prompts to guide the image generation (applicable to SDXL).",
+    )
+    parser.add_argument(
+        "--control_image",
+        type=str,
+        default=None,
+        help=("Path to the controlnet conditioning image"),
+    )
+    parser.add_argument(
+        "--control_preprocessing_type",
+        type=str,
+        default="canny",
+        help=(
+            "The type of preprocessing to apply on contol image. Only `canny` is supported."
+            " Defaults to `canny`. Set to unsupported value to disable preprocessing."
+        ),
+    )
+    parser.add_argument(
+        "--num_images_per_prompt", type=int, default=1, help="The number of images to generate per prompt."
+    )
+    parser.add_argument("--batch_size", type=int, default=1, help="The number of images in a batch.")
+    parser.add_argument(
+        "--height",
+        type=int,
+        default=0,
+        help="The height in pixels of the generated images (0=default from model config).",
+    )
+    parser.add_argument(
+        "--width",
+        type=int,
+        default=0,
+        help="The width in pixels of the generated images (0=default from model config).",
+    )
+    parser.add_argument(
+        "--num_inference_steps",
+        type=int,
+        default=50,
+        help=(
+            "The number of denoising steps. More denoising steps usually lead to a higher quality image at the expense"
+            " of slower inference."
+        ),
+    )
+    parser.add_argument(
+        "--guidance_scale",
+        type=float,
+        default=7.5,
+        help=(
+            "Guidance scale as defined in [Classifier-Free Diffusion Guidance](https://arxiv.org/abs/2207.12598)."
+            " Higher guidance scale encourages to generate images that are closely linked to the text `prompt`,"
+            " usually at the expense of lower image quality."
+        ),
+    )
+    parser.add_argument(
+        "--negative_prompts",
+        type=str,
+        nargs="*",
+        default=None,
+        help="The prompt or prompts not to guide the image generation.",
+    )
+    parser.add_argument(
+        "--negative_prompts_2",
+        type=str,
+        nargs="*",
+        default=None,
+        help="The second prompt or prompts not to guide the image generation (applicable to SDXL).",
+    )
+    parser.add_argument(
+        "--eta",
+        type=float,
+        default=0.0,
+        help="Corresponds to parameter eta (η) in the DDIM paper: https://arxiv.org/abs/2010.02502.",
+    )
+    parser.add_argument(
+        "--output_type",
+        type=str,
+        choices=["pil", "np"],
+        default="pil",
+        help="Whether to return PIL images or Numpy arrays.",
+    )
+
+    parser.add_argument(
+        "--pipeline_save_dir",
+        type=str,
+        default=None,
+        help="The directory where the generation pipeline will be saved.",
+    )
+    parser.add_argument(
+        "--image_save_dir",
+        type=str,
+        default="./stable-diffusion-generated-images",
+        help="The directory where images will be saved.",
+    )
+
+    parser.add_argument("--seed", type=int, default=42, help="Random seed for initialization.")
+
+    # HPU-specific arguments
+    parser.add_argument("--use_habana", action="store_true", help="Use HPU.")
+    parser.add_argument(
+        "--use_hpu_graphs", 
+        action="store_true", 
+        help="Use HPU graphs on HPU. This should lead to faster generations."
+    )
+    parser.add_argument(
+        "--gaudi_config_name",
+        type=str,
+        default="Habana/stable-diffusion",
+        help=(
+            "Name or path of the Gaudi configuration. In particular, it enables to specify how to apply Habana Mixed"
+            " Precision."
+        ),
+    )
+    parser.add_argument("--bf16", action="store_true", help="Whether to perform generation in bf16 precision.")
+    parser.add_argument(
+        "--ldm3d", action="store_true", help="Use LDM3D to generate an image and a depth map from a given text prompt."
+    )
+    parser.add_argument(
+        "--profiling_warmup_steps",
+        default=0,
+        type=int,
+        help="Number of steps to ignore for profiling.",
+    )
+    parser.add_argument(
+        "--profiling_steps",
+        default=0,
+        type=int,
+        help="Number of steps to capture for profiling.",
+    )
+    args = parser.parse_args()
+
+    res = {}
+    if args.width > 0 and args.height > 0:
+        res["width"] = args.width
+        res["height"] = args.height
+
+    kwargs = {"timestep_spacing": args.timestep_spacing}
+    scheduler = GaudiDDIMScheduler.from_pretrained(args.model_name_or_path , **kwargs, subfolder="scheduler")
+
+    kwargs = {
+        "scheduler": scheduler,
+        "use_habana": args.use_habana,
+        "use_hpu_graphs": args.use_hpu_graphs,
+        "gaudi_config": args.gaudi_config_name,
+    }
+    
+    pipeline = GaudiStableDiffusionPipeline.from_pretrained(
+        args.model_name_or_path,
+        **kwargs,
+    )
+
+    # Patch for the deterministic generation - Need to specify CPU as the torch generator
+    generator = [torch.Generator(device="cpu").manual_seed(i) for i in range(args.num_images_per_prompt)]
+
+    outputs = pipeline(
+        prompt=args.prompts,
+        num_images_per_prompt=args.num_images_per_prompt,
+        batch_size=args.batch_size,
+        num_inference_steps=args.num_inference_steps,
+        guidance_scale=args.guidance_scale,
+        negative_prompt=args.negative_prompts,
+        eta=args.eta,
+        output_type=args.output_type,
+        generator=generator,
+        **res,
+    )
+
+    # Save images in the specified directory if not None and if they are in PIL format
+    if args.image_save_dir is not None:
+        import pdb; pdb.set_trace()
+        if args.output_type == "pil":
+            image_save_dir = Path(args.image_save_dir)
+            image_save_dir.mkdir(parents=True, exist_ok=True)
+            #logger.info(f"Saving images in {image_save_dir.resolve()}...")
+            if args.ldm3d:
+                for i, rgb in enumerate(outputs.rgb):
+                    rgb.save(image_save_dir / f"rgb_{i+1}.png")
+                for i, depth in enumerate(outputs.depth):
+                    depth.save(image_save_dir / f"depth_{i+1}.png")
+            else:
+                for i, image in enumerate(outputs.images):
+                    image.save(image_save_dir / f"image_{i+1}.png")
+        else:
+            logger.warning("--output_type should be equal to 'pil' to save images in --image_save_dir.")
+
+if __name__ == "__main__":
+    main()
