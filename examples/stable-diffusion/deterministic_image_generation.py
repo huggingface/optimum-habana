cd76a4e93cb7d60860956008086f9273475a6f2c
Raymond Lau <kwun.fung.lau@intel.com>
2024-04-22 18:16:41 +0000

    Removed unnecessary codes and added license info.

--- examples/stable-diffusion/deterministic_image_generation.py
+++ examples/stable-diffusion/deterministic_image_generation.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+
 import argparse
 import logging
 import sys
@@ -220,7 +235,6 @@ def main():
 
     # Save images in the specified directory if not None and if they are in PIL format
     if args.image_save_dir is not None:
-        import pdb; pdb.set_trace()
         if args.output_type == "pil":
             image_save_dir = Path(args.image_save_dir)
             image_save_dir.mkdir(parents=True, exist_ok=True)
