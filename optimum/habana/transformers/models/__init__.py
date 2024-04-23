9e54a265a66e14c2dc3afccfdd17077a13cbf485
Ye, Xinyu <xinyu.ye@intel.com>
2024-04-23 02:14:27 -0400

    fix style error.
    Signed-off-by: Ye, Xinyu <xinyu.ye@intel.com>

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -119,9 +119,9 @@ from .opt import (
 )
 from .persimmon import (
     GaudiPersimmonForCausalLM,
-    gaudi_persimmon_model_forward,
     gaudi_persimmon_attention_forward,
     gaudi_persimmon_decoder_layer_forward,
+    gaudi_persimmon_model_forward,
 )
 from .phi import (
     GaudiPhiForCausalLM,
--- optimum/habana/transformers/models/persimmon/modeling_persimmon.py
+++ optimum/habana/transformers/models/persimmon/modeling_persimmon.py
@@ -1,15 +1,14 @@
 import math
-import torch
-import warnings
+from typing import List, Optional, Tuple, Union
 
+import torch
 from torch import nn
 from torch.nn import CrossEntropyLoss
 from transformers.cache_utils import Cache, DynamicCache
-from transformers.modeling_attn_mask_utils import _prepare_4d_causal_attention_mask_for_sdpa
 from transformers.modeling_outputs import BaseModelOutputWithPast, CausalLMOutputWithPast
 from transformers.models.persimmon.modeling_persimmon import PersimmonForCausalLM, apply_rotary_pos_emb
 from transformers.utils import logging
-from typing import List, Optional, Tuple, Union
+
 from ...modeling_attn_mask_utils import (
     _gaudi_prepare_4d_causal_attention_mask,
 )
