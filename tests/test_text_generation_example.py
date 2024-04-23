e14de8df1d45bac0ac2bfd12f276c54003f1cc6d
Ye, Xinyu <xinyu.ye@intel.com>
2024-04-23 02:04:35 -0400

    updated stabilityai/stablelm-2-12b throughput.
    Signed-off-by: Ye, Xinyu <xinyu.ye@intel.com>

--- tests/test_text_generation_example.py
+++ tests/test_text_generation_example.py
@@ -29,9 +29,8 @@ if os.environ.get("GAUDI2_CI", "0") == "1":
             ("meta-llama/Meta-Llama-3-8B", 1, True, 129),
             ("meta-llama/Llama-2-7b-hf", 512, True, 12808),
             ("meta-llama/Llama-2-7b-hf", 512, False, 8711),  # in some cases like TGI, reuse_cache isnt used
-            ("stabilityai/stablelm-2-12b", 1, False, 81.842),
+            ("stabilityai/stablelm-2-12b", 4, False, 311.46584535679483),
             ("codellama/CodeLlama-34b-hf", 1, True, 32.644),
-            ("stabilityai/stablelm-zephyr-3b", 1, False, 821.9666453354841),
         ],
         "fp8": [
             ("tiiuae/falcon-180B", 52.85086442722326),
