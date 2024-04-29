9bc130eb2d43e481735c8ee49e24bdf49539f160
regisss <15324346+regisss@users.noreply.github.com>
2024-04-29 12:33:43 +0000

    [skip ci] Fix workflow

--- tests/ci/sentence_transformers.sh
+++ tests/ci/sentence_transformers.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+python -m pip install --upgrade pip
+python -m pip install /root/workspace/optimum-habana[tests]
 cd /root/workspace/sentence-transformers
+python -m pip install .
 python -m pytest test_compute_embeddings.py
 cd /root/workspace/optimum-habana
 python -m pytest test_sentence_transformers.py
