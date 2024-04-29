b734a3b527bc9cf12bbf3ac4513928b6c1640bc4
regisss <15324346+regisss@users.noreply.github.com>
2024-04-29 12:40:27 +0000

    [skip ci] Fix CI script

--- tests/ci/sentence_transformers.sh
+++ tests/ci/sentence_transformers.sh
@@ -2,8 +2,8 @@
 
 python -m pip install --upgrade pip
 python -m pip install /root/workspace/optimum-habana[tests]
-cd /root/workspace/sentence-transformers
-python -m pip install .
+cd /root/workspace/sentence-transformers/tests
+python -m pip install ..
 python -m pytest test_compute_embeddings.py
-cd /root/workspace/optimum-habana
+cd /root/workspace/optimum-habana/tests
 python -m pytest test_sentence_transformers.py
