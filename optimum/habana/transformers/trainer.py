5535812e61b2390a6e8fb4dc0188ead72ba318f3
regisss <15324346+regisss@users.noreply.github.com>
2024-05-01 18:17:33 +0000

    Fix

--- optimum/habana/transformers/trainer.py
+++ optimum/habana/transformers/trainer.py
@@ -1184,11 +1184,11 @@ class GaudiTrainer(Trainer):
                 grad_norm = model.get_global_grad_norm()
             else:
                 if _grad_norm is not None and self.accelerator.distributed_type != GaudiDistributedType.FSDP:
-                    grad_norm = _grad_norm.item() if _grad_norm.size() == 1 else _grad_norm.tolist()
+                    grad_norm = _grad_norm.item() if _grad_norm.size() == torch.Size([1]) else _grad_norm.tolist()
                 else:
                     grad_norm = None
 
-            if grad_norm is not None:
+            if grad_norm is not None and not isinstance(grad_norm, list):
                 logs["grad_norm"] = grad_norm
             logs["learning_rate"] = self._get_learning_rate()
 
