017ac2d78ec5f700634402c68177d0eaeb8173a7
Wang, Yi A <yi.a.wang@intel.com>
2024-05-15 18:00:37 -0700

    stable diffusion image variation pipeline
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- optimum/habana/diffusers/__init__.py
+++ optimum/habana/diffusers/__init__.py
@@ -1,6 +1,9 @@
 from .pipelines.controlnet.pipeline_controlnet import GaudiStableDiffusionControlNetPipeline
 from .pipelines.pipeline_utils import GaudiDiffusionPipeline
 from .pipelines.stable_diffusion.pipeline_stable_diffusion import GaudiStableDiffusionPipeline
+from .pipelines.stable_diffusion.pipeline_stable_diffusion_image_variation import (
+    GaudiStableDiffusionImageVariationPipeline,
+)
 from .pipelines.stable_diffusion.pipeline_stable_diffusion_ldm3d import GaudiStableDiffusionLDM3DPipeline
 from .pipelines.stable_diffusion.pipeline_stable_diffusion_upscale import GaudiStableDiffusionUpscalePipeline
 from .pipelines.stable_diffusion_xl.pipeline_stable_diffusion_xl import GaudiStableDiffusionXLPipeline
