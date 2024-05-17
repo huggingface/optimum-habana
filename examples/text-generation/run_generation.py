8fc0b8d4701ce562d7d51a5bfe441de79e8e193e
Alexey Fadeev <alexey.fadeev@intel.com>
2024-05-17 11:52:30 +0200

    Fix for the simulate_dyn_prompt flag assertion (#984)

--- examples/text-generation/run_generation.py
+++ examples/text-generation/run_generation.py
@@ -487,7 +487,7 @@ def main():
         from datasets import load_dataset
         from torch.utils.data import DataLoader
 
-        assert args.simulate_dyn_prompt == "", "Both dataset_name and simulate_dyn_prompt are set"
+        assert not args.simulate_dyn_prompt, "Both dataset_name and simulate_dyn_prompt are set"
 
         raw_dataset = load_dataset(args.dataset_name)
         if "test" in raw_dataset:
