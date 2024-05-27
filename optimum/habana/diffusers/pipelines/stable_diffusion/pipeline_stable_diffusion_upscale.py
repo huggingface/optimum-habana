d40629d207c2a338dc7da2bd2e93ab064a8f7d1a
yuanwu2017 <yuan.wu@intel.com>
2024-05-27 15:59:09 +0800

    Use the lenght of timesteps as the inference step num (#986)
    Signed-off-by: yuanwu <yuan.wu@intel.com>

--- optimum/habana/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_upscale.py
+++ optimum/habana/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_upscale.py
@@ -458,7 +458,7 @@ class GaudiStableDiffusionUpscalePipeline(GaudiDiffusionPipeline, StableDiffusio
                 noise_level_batch = noise_level_batches[0]
                 noise_level_batches = torch.roll(noise_level_batches, shifts=-1, dims=0)
 
-                for i in range(num_inference_steps):
+                for i in range(len(timesteps)):
                     if use_warmup_inference_steps and i == throughput_warmup_steps:
                         t1_inf = time.time()
                         t1 += t1_inf - t0_inf
