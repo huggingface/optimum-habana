1e291871cd3e012f2995c76dc0ba17687afcd053
Vidya S Galli <vidya.s.galli@intel.com>
2024-05-31 21:11:10 +0000

    fixes

--- optimum/habana/transformers/modeling_utils.py
+++ optimum/habana/transformers/modeling_utils.py
@@ -26,7 +26,6 @@ from .generation import (
     gaudi_MaxNewTokensCriteria_call,
     gaudi_MaxTimeCriteria_call,
     gaudi_StoppingCriteriaList_call,
-    gaudi_StoppingCriteriaList_call,
 )
 from .models import (
     DeciLMConfig,
@@ -271,7 +270,6 @@ def adapt_transformers_to_gaudi():
     transformers.generation.MaxNewTokensCriteria.__call__ = gaudi_MaxNewTokensCriteria_call
     transformers.generation.MaxTimeCriteria.__call__ = gaudi_MaxTimeCriteria_call
     transformers.generation.EosTokenCriteria.__call__ = gaudi_EosTokenCriteria_call
-    transformers.generation.StoppingCriteriaList.__call__ = gaudi_StoppingCriteriaList_call
     transformers.generation.StoppingCriteriaList.__call__ = gaudi_StoppingCriteriaList_call
 
     # Optimization for BLOOM generation on Gaudi
