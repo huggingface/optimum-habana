1690733b0e84d3ecca6edf94c571486a91ea3192
Kavulya, Soila P <soila.p.kavulya@intel.com>
2024-06-04 14:36:23 -0700

    Fix reference to contrastive search and formatting changes

--- optimum/habana/transformers/models/gpt2/__init__.py
+++ optimum/habana/transformers/models/gpt2/__init__.py
@@ -1,8 +1,7 @@
 from .modeling_gpt2 import (
     GaudiGPT2Attention,
+    GaudiGPT2Block,
     GaudiGPT2DoubleHeadsModel,
     GaudiGPT2LMHeadModel,
-    GaudiGPT2Block,
-    GaudiGPT2LMHeadModel,
-    gaudi_gpt2_forward
+    gaudi_gpt2_forward,
 )
