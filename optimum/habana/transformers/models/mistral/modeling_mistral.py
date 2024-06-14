7f50ac8b0a73d9c998b723a62c4d836c6c744568
Shiv Kaul <skaul@habana.ai>
2024-06-14 01:02:33 -0700

    add long sequence to mistral (#255)
    huggingface#985

--- optimum/habana/transformers/models/mistral/modeling_mistral.py
+++ optimum/habana/transformers/models/mistral/modeling_mistral.py
@@ -66,6 +66,9 @@ except ImportError:
     print("Not using HPU fused scaled dot-product attention kernel.")
     FusedSDPA = None
 
+logger = logging.get_logger(__name__)
+
+
 #  FusedScaledDotProductAttention
 class ModuleFusedSDPA(torch.nn.Module):
     def __init__(self, fusedSDPA):
@@ -83,7 +86,32 @@ class Matmul(torch.nn.Module):
         return torch.matmul(x, y)
 
 
-logger = logging.get_logger(__name__)
+# Copy from GaudiMixtralAttentionLongSequence
+class GaudiMistralAttentionLongSequence:
+    @staticmethod
+    def forward(q, k, v, mask, causal, q_block_size):
+        """
+        Support long sequence at prompt phase
+        """
+        q_len = q.size(-2)
+        q_tiles = (q_len // q_block_size) if (q_len % q_block_size == 0) else math.ceil(q_len / q_block_size)
+        q_padding = q_tiles * q_block_size - q_len
+        q = F.pad(q, (0, 0, 0, q_padding), "constant", 0)
+        if mask is not None:
+            mask = F.pad(mask, (0, 0, 0, q_padding), "constant", -10000.0)
+        attn_output = torch.zeros_like(q)
+
+        for i in range(q_tiles):
+            s, e = i * q_block_size, (i + 1) * q_block_size
+            row_q = q[:, :, s:e, :]
+            row_mask = mask[:, :, s:e, :]
+            row_o = attn_output[:, :, s:e, :]
+            row_o.fill_(FusedSDPA.apply(row_q, k, v, row_mask, 0.0, causal, None))
+
+        if q_padding != 0:
+            attn_output = attn_output[:, :, :-q_padding, :]
+
+        return attn_output
 
 
 def gaudi_mistral_rmsnorm_forward(self, hidden_states):
@@ -153,6 +181,7 @@ class GaudiMistralAttention(MistralAttention):
         self.fused_scaled_dot_product_attention = ModuleFusedSDPA(FusedSDPA) if FusedSDPA else None
         self.inp_seq_len = -1
         self.norm_factor = 1.0 / math.sqrt(self.head_dim)
+        self.block_size = 1024
 
     def allocate_kv_cache(self, batch_size, max_seq_len, inp_seq_len):
         cache_shape = (batch_size, self.num_key_value_heads, max_seq_len, self.head_dim)
@@ -273,8 +302,8 @@ class GaudiMistralAttention(MistralAttention):
         else:
             past_key_value = None
 
-        if use_flash_attention and FusedSDPA:
-            import habana_frameworks.torch.hpu as ht
+        import habana_frameworks.torch.hpu as ht
+        if  FusedSDPA and use_flash_attention:
             if q_len == 1:
                 # next token
                 use_recompute = True if os.getenv("QUANT_CONFIG", "") else False
@@ -293,6 +322,17 @@ class GaudiMistralAttention(MistralAttention):
                         attn_output = self.fused_scaled_dot_product_attention(
                             query_states, key_states, value_states, attention_mask, 0.0, False, None
                         )
+        elif FusedSDPA and not self.training and q_len == key_states.size(-2) and q_len > 8192:
+            htcore.mark_step()
+            attn_output = GaudiMistralAttentionLongSequence.forward(
+                query_states,
+                key_states,
+                value_states,
+                attention_mask,
+                False,
+                self.block_size,
+            )
+            htcore.mark_step()
         else:
             # repeat k/v heads if n_kv_heads < n_heads
             query_states, key_states, value_states, attention_mask = gaudi_mistral_repeat_kv(
