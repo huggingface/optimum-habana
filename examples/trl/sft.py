a10533c5d3bef0158f717520f286012bf8aa5bc1
Wang, Yi A <yi.a.wang@intel.com>
2024-06-24 21:50:09 -0700

    fix test issue
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- examples/trl/sft.py
+++ examples/trl/sft.py
@@ -208,9 +208,10 @@ if __name__ == "__main__":
     if training_args.do_eval:
         logger.info("*** Evaluate ***")
         metrics = trainer.evaluate()
+        if isinstance(eval_dataset, torch.utils.data.IterableDataset):
+            eval_dataset = list(eval_dataset)
 
-        max_eval_samples = len(eval_dataset)
-        metrics["eval_samples"] = min(max_eval_samples, len(eval_dataset))
+        metrics["eval_samples"] = len(eval_dataset)
 
         try:
             perplexity = math.exp(metrics["eval_loss"])
