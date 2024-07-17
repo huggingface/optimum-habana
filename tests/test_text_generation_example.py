a698a1b76335c433ecc1e7929eb93f76fa1e99f6
Kavulya, Soila P <soila.p.kavulya@intel.com>
2024-07-17 16:32:59 -0700

    Add contrastive search to text generation example tests

--- tests/test_text_generation_example.py
+++ tests/test_text_generation_example.py
@@ -74,6 +74,9 @@ if os.environ.get("GAUDI2_CI", "0") == "1":
         "distributed_tp": [
             ("meta-llama/Llama-2-7b-hf", 1345.2369318328463),
         ],
+        "contrastive_search": [
+            ("gpt2-xl", 1, False, 25.058159824825506),
+        ],
     }
 else:
     # Gaudi1 CI baselines
@@ -105,6 +108,7 @@ else:
         "torch_compile": [],
         "torch_compile_distributed": [],
         "distributed_tp": [],
+        "contrastive_search": [],
     }
 
 
@@ -121,6 +125,7 @@ def _test_text_generation(
     max_input_tokens: int = 0,
     max_output_tokens: int = 100,
     parallel_strategy: str = None,
+    contrastive_search: bool = False,
 ):
     command = ["python3"]
     path_to_example_dir = Path(__file__).resolve().parent.parent / "examples"
@@ -175,6 +180,9 @@ def _test_text_generation(
 
     if not deepspeed:
         command.append("--bf16")
+
+    if contrastive_search:
+        command += ["--top_k 4", "--penalty_alpha 0.5"]
 
     if fp8:
         if "--trim_logits" not in command:
@@ -324,6 +332,11 @@ def test_text_generation_distributed_tp(model_name: str, baseline: float, token:
         torch_compile=True,
         parallel_strategy="tp",
     )
+
+
+@pytest.mark.parametrize("model_name, batch_size, reuse_cache, baseline", MODELS_TO_TEST["contrastive_search"])
+def test_text_generation_contrastive_search(model_name: str, baseline: float, batch_size: int, reuse_cache: bool, token: str):
+    _test_text_generation(model_name, baseline, token, batch_size, reuse_cache, contrastive_search=True)
 
 
 class TextGenPipeline(TestCase):
