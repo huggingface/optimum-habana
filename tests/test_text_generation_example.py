78eb1282ce3e1c22dc8f40b64ddbf8025fc1904f
Wang, Yi A <yi.a.wang@intel.com>
2024-07-21 19:33:59 -0700

    add ci case for DeciLM 7B
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- tests/test_text_generation_example.py
+++ tests/test_text_generation_example.py
@@ -41,6 +41,7 @@ if os.environ.get("GAUDI2_CI", "0") == "1":
             ("Qwen/Qwen1.5-7B", 4, False, 518.894516133132),
             ("google/gemma-7b", 1, False, 109.70751574382221),
             ("state-spaces/mamba-130m-hf", 1536, False, 8600),
+            ("Deci/DeciLM-7B", 1, False, 120),
         ],
         "fp8": [
             ("tiiuae/falcon-180B", 4, 950, True, 128, 128, 2506.68),
