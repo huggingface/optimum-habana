ea98a17e9b26d8301ef465dd94e5efa25ae95d5f
Sayantan Sarkar <sasarkar@habana.ai>
2024-07-25 16:34:24 +0000

    Parameterize test for both 7b and 72b model

--- tests/test_sft_train.py
+++ tests/test_sft_train.py
@@ -5,15 +5,40 @@ from pathlib import Path
 import os
 from datasets import load_dataset
 import shutil
+import tempfile
 
-def test_sft_train():
+@pytest.mark.parametrize("model_name,expected", [("Qwen/Qwen2-7B", (30.12, 4.8347)), ("Qwen/Qwen2-72B", (6.969, 3.6))])
+def test_sft_train(model_name, expected):
+    ds_config = '''{
+    "train_batch_size": "auto",
+    "train_micro_batch_size_per_gpu" :"auto",
+    "gradient_accumulation_steps": "auto",
+    "gradient_clipping": "auto",
+    "bf16": {
+        "enabled": "auto"
+    },
+    "zero_optimization": {
+    "stage": 3,
+    "overlap_comm": true,
+    "stage3_gather_16bit_weights_on_model_save": true
+    },
+    "flops_profiler": {
+        "enabled": false,
+        "profile_step": 1,
+        "module_depth": -1,
+        "top_modules": 1,
+        "detailed": true,
+        "output_file": null
+    }
+}
+    '''
     env_variables = os.environ.copy()
     path_to_example_dir = Path(__file__).resolve().parent.parent / "examples"
     filename = f"{path_to_example_dir / 'trl' / 'sft.py'}"
     gaudispawn_filename = f"{path_to_example_dir / 'gaudi_spawn.py'}"
 
     command = ['python3', gaudispawn_filename, '--world_size', '8', '--use_deepspeed', filename, \
-    '--model_name_or_path', 'Qwen/Qwen2-7B', \
+    '--model_name_or_path', model_name, \
     '--dataset_name', 'philschmid/dolly-15k-oai-style', '--streaming', 'False', '--bf16', \
     'True', '--output_dir', './model_qwen', '--num_train_epochs', '1', '--per_device_train_batch_size', '8', \
     '--evaluation_strategy', 'no', '--save_strategy', 'no', '--learning_rate', '3e-4', \
@@ -21,16 +46,25 @@ def test_sft_train():
     '--logging_steps', '1', '--do_train', '--do_eval', '--use_habana', '--use_lazy_mode', \
     '--throughput_warmup_steps', '3', '--lora_r', '4', '--lora_alpha=16', '--lora_dropout=0.05', \
     '--lora_target_modules', 'q_proj', 'v_proj', 'k_proj', 'o_proj', '--max_seq_length', \
-    '512', '--adam_epsilon', '1e-08', '--packing', 'False', '--num_bucket', '8', '--subset', "''", '--max_steps', '100']
+    '512', '--adam_epsilon', '1e-08', '--packing', 'False', '--num_bucket', '8', '--subset', "''", ]
+    if '72' in model_name:
+        command += ['--max_steps', '50', '--gradient_checkpointing', 'True', '--pipelining_fwd_bwd', 'True']  ## TODO create ds_config.json
+    else:
+        command += ['--max_steps', '100']
     env_variables["DEEPSPEED_HPU_ZERO3_SYNC_MARK_STEP_REQUIRED"] = "1"
-    print(f"\n\nCommand to test: {' '.join(command)}\n")
-    proc = subprocess.run(command, env=env_variables, stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = True)
+    with tempfile.NamedTemporaryFile() as fp:
+        fp.write(str.encode(ds_config))
+        fp.flush()
+        if '72' in model_name:
+            command += ['--deepspeed', 'fp.name']
+        proc = subprocess.run(command, env=env_variables, stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = True)
 
     assert proc.returncode == 0
     alllines = proc.stdout.split('\n')
     train_samples_per_second = float([line for line in alllines if 'train_samples_per_second' in line][-1].split('=')[-1])
     perplexity = float([line for line in alllines if 'perplexity' in line][-1].split('=')[-1])
-    assert train_samples_per_second > 0.9 * 30.12
-    assert perplexity < 1.05 * 4.8347
+    train_samples_per_second_expected, perplexity_expected = expected
+    assert train_samples_per_second > 0.9 * train_samples_per_second_expected, f"Got {train_samples_per_second}, expected 0.9*{train_samples_per_second_expected}"
+    assert perplexity < 1.05 * perplexity_expected, f"Got {perplexity}, expected 1.05*{perplexity_expected}"
 
 
