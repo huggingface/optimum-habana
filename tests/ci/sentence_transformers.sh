ace577ade85a1259485e76237b24219634853489
Vidya S Galli <vidya.s.galli@intel.com>
2024-08-07 17:56:40 +0000

    updating to avoid hardcoding tests in CI framework

--- tests/ci/sentence_transformers.sh
+++ tests/ci/sentence_transformers.sh
@@ -4,6 +4,6 @@ python -m pip install --upgrade pip
 python -m pip install /root/workspace/optimum-habana[tests]
 cd /root/workspace/sentence-transformers/tests
 python -m pip install ..
-python -m pytest test_compute_embeddings.py test_evaluator.py test_multi_process.py test_pretrained_stsb.py test_util.py
+python -m pytest test_cmnrl.py test_cross_encoder.py test_evaluator.py test_image_embeddings.py test_model_card_data.py test_sentence_transformer.py test_compute_embeddings.py test_train_stsb.py test_trainer.py test_evaluator.py test_multi_process.py test_pretrained_stsb.py test_util.py
 cd /root/workspace/optimum-habana/tests
 python -m pytest test_sentence_transformers.py
