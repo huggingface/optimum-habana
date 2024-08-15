427d325cac69111cff002f65e8e2625289b4c188
Yeonsil Yoon <yyoon@habana.ai>
2024-08-15 15:10:26 -0700

    Gpt bigcode fused sdpa (#1260)
    Co-authored-by: regisss <15324346+regisss@users.noreply.github.com>

--- examples/text-generation/utils.py
+++ examples/text-generation/utils.py
@@ -284,7 +284,7 @@ def setup_model(args, model_dtype, model_kwargs, logger):
             if model.peft_type == "ADAPTION_PROMPT":
                 model.base_model.model = wrap_in_hpu_graph(model.base_model.model)
 
-    if args.torch_compile and model.config.model_type == "llama":
+    if args.torch_compile:
         model = get_torch_compiled_model(model)
         # if args.assistant_model is not None:
         #     assistant_model = get_torch_compiled_model(assistant_model)
@@ -351,7 +351,7 @@ def setup_distributed_model_tp(args, model_dtype, model_kwargs, logger, cache_di
 
         model = wrap_in_hpu_graph(model)
 
-    if args.torch_compile and model.config.model_type == "llama":
+    if args.torch_compile:
         model = get_torch_compiled_model(model)
 
     return model, args.assistant_model
@@ -425,7 +425,7 @@ def setup_distributed_model(args, model_dtype, model_kwargs, logger):
     if args.quant_config:
         model = setup_quantization(model, args)
 
-    if args.torch_compile and model.config.model_type == "llama":
+    if args.torch_compile:
         model = get_torch_compiled_model(model)
         # if args.assistant_model is not None:
         #     assistant_model = get_torch_compiled_model(assistant_model)
@@ -636,6 +636,7 @@ def initialize_model(args, logger):
         if not args.parallel_strategy == "tp"
         else setup_distributed_model_tp(args, model_dtype, model_kwargs, logger, cache_dir)
     )
+
     tokenizer, model, assistant_model = setup_tokenizer(args, model, assistant_model)
     generation_config = setup_generation_config(args, model, assistant_model, tokenizer)
 
