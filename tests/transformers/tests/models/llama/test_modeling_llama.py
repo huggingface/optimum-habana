d6696f13e6236fd190dd3778fb362e4f0770f4b5
Ranjeet Kumar <rankumar@habana.ai>
2024-09-05 22:43:16 +0530

    Update test_modeling_llama.py

--- tests/transformers/tests/models/llama/test_modeling_llama.py
+++ tests/transformers/tests/models/llama/test_modeling_llama.py
@@ -28,6 +28,7 @@ from ...generation.test_utils import GenerationTesterMixin
 from ...test_configuration_common import ConfigTester
 from ...test_modeling_common import ModelTesterMixin, ids_tensor, random_attention_mask
 
+
 torch_device = "hpu"
 adapt_transformers_to_gaudi()
 
