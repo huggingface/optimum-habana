67b3624545a83dd43e39c23fad31f2ee8d85c253
Ye, Xinyu <xinyu.ye@intel.com>
2024-09-09 04:33:09 -0400

    Optimized inference of GPT-NEO model on HPU.
    Signed-off-by: Ye, Xinyu <xinyu.ye@intel.com>

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -83,6 +83,13 @@ from .gpt_bigcode import (
     gaudi_gpt_bigcode_block_forward,
     gaudi_gpt_bigcode_model_forward,
 )
+from .gpt_neo import (
+    GaudiGPTNeoForCausalLM,
+    GaudiGPTNeoBlock,
+    gaudi_gpt_neo_attention_forward,
+    gaudi_gpt_neo_selfattention_forward,
+    gaudi_gpt_neo_model_forward,
+)
 from .gpt_neox import (
     GaudiGPTNeoXForCausalLM,
     GaudiGPTNeoXLayer,
---
+++ optimum/habana/transformers/models/gpt_neo/__init__.py
@@ -0,0 +1,7 @@
+from .modeling_gpt_neo import (
+    GaudiGPTNeoForCausalLM,
+    GaudiGPTNeoBlock,
+    gaudi_gpt_neo_attention_forward,
+    gaudi_gpt_neo_selfattention_forward,
+    gaudi_gpt_neo_model_forward,
+)
