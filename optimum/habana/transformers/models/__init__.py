bdd8f5454db7681139d20806b99721d02f69e084
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2024-09-09 17:20:06 +0000

    Optimizations for Detr Model to run in lazy mode
    - Add capability to ignore targets that have an out-of-range ID
    - This helps to pad target objects to avoid graph recompilation and
      yet not affect the loss computation in training.

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -52,7 +52,11 @@ from .decilm import (
 from .detr import (
     gaudi_DetrConvModel_forward,
     gaudi_DetrHungarianMatcher_forward,
-    gaudi_DetrLoss_loss_labels
+    gaudi_DetrLoss_forward,
+    gaudi_DetrLoss_get_targets_without_no_objects,
+    gaudi_DetrLoss_loss_boxes,
+    gaudi_DetrLoss_loss_cardinality,
+    gaudi_DetrLoss_loss_labels,
 )
 from .esm import (
     gaudi_esm_for_protein_folding_forward,
--- optimum/habana/transformers/models/detr/__init__.py
+++ optimum/habana/transformers/models/detr/__init__.py
@@ -1,5 +1,9 @@
 from .modeling_detr import (
     gaudi_DetrConvModel_forward,
     gaudi_DetrHungarianMatcher_forward,
-    gaudi_DetrLoss_loss_labels
+    gaudi_DetrLoss_forward,
+    gaudi_DetrLoss_get_targets_without_no_objects,
+    gaudi_DetrLoss_loss_boxes,
+    gaudi_DetrLoss_loss_cardinality,
+    gaudi_DetrLoss_loss_labels,
 )
