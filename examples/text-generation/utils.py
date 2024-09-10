5d80d407b1982caa7586e1a3acc2e76a0a5de18a
yixiu.chen <yixchen@habana.ai>
2024-09-10 08:47:48 +0300

    [SW-181067] Added support for transformers without self.model to handle broader model compatibility
    Change-Id: Ic25d98ec19d108540b26cdb500b0389421295cdc

--- examples/text-generation/utils.py
+++ examples/text-generation/utils.py
@@ -173,10 +173,12 @@ def patch_scoped_linear_all_reduce(model):
 
 
 def get_torch_compiled_model(model):
-    if model.config.model_type in ["gpt_bigcode"]:
+    if model.config.model_type in ["gpt_bigcode", "mpt", "bloom"]:
         model.transformer = torch.compile(
             model.transformer, backend="hpu_backend", options={"keep_input_mutations": True}
         )
+    elif model.config.model_type in ["gpt_neox"]:
+        model.gpt_neox = torch.compile(model.gpt_neox, backend="hpu_backend", options={"keep_input_mutations": True})
     else:
         model.model = torch.compile(model.model, backend="hpu_backend", options={"keep_input_mutations": True})
     return model
