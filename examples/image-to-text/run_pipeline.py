baca7e90e674d1f679f76b81231403a516f77f76
Wang, Yi A <yi.a.wang@intel.com>
2024-09-17 06:26:18 -0700

    blip does not has chat template
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- examples/image-to-text/run_pipeline.py
+++ examples/image-to-text/run_pipeline.py
@@ -162,7 +162,7 @@ def main():
             "https://github.com/haotian-liu/LLaVA/blob/1a91fc274d7c35a9b50b3cb29c4247ae5837ce39/images/llava_v1_5_radar.jpg?raw=true"
         ]
 
-    if args.prompt is None:
+    if args.prompt is None and model_type in ["llava", "idefics2", "llava_next"]:
         processor = AutoProcessor.from_pretrained(args.model_name_or_path)
         conversation = [
             {
