951cd4e724eaaf441402e96075b4f058218e1720
Daniel Huang <daniel1.huang@intel.com>
2024-09-19 18:33:46 +0000

    Make style

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -121,6 +121,7 @@ from .mamba import (
     gaudi_MambaForCausalLM_prepare_inputs_for_generation,
     gaudi_MambaForCausalLM_update_model_kwargs_for_generation,
 )
+from .minicpm import MiniCPM3Config, MiniCPM3ForCausalLM, MiniCPMTokenizer
 from .mistral import (
     GaudiMistralAttention,
     GaudiMistralDecoderLayer,
@@ -129,7 +130,6 @@ from .mistral import (
     MistralConfig,
     gaudi_mistral_rmsnorm_forward,
 )
-from .minicpm import MiniCPM3Config, MiniCPM3ForCausalLM, MiniCPMTokenizer
 from .mixtral import (
     GaudiMixtralAttention,
     GaudiMixtralDecoderLayer,
--- optimum/habana/transformers/models/minicpm/modeling_minicpm.py
+++ optimum/habana/transformers/models/minicpm/modeling_minicpm.py
@@ -50,7 +50,6 @@ from transformers.utils import (
     add_start_docstrings_to_model_forward,
     is_flash_attn_greater_or_equal_2_10,
     logging,
-    replace_return_docstrings,
 )
 from transformers.utils.import_utils import is_torch_fx_available
 
@@ -60,7 +59,7 @@ from .configuration_minicpm import MiniCPM3Config
 try:
     from flash_attn import flash_attn_func, flash_attn_varlen_func
     from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input  # noqa
-except:
+except ImportError:
     pass
 
 
