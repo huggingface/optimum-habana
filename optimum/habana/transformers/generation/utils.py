421e24d413e4255bc81a83528150d0ad409ef877
Sayantan Sarkar <supersarkar@gmail.com>
2024-10-07 16:05:26 -0700

    Valid sequence length for sdpa (#1183)
    Co-authored-by: Harish <hsubramony@habana.ai>
    Co-authored-by: Libin Tang <litang@habana.ai>
    Co-authored-by: regisss <15324346+regisss@users.noreply.github.com>

--- optimum/habana/transformers/generation/utils.py
+++ optimum/habana/transformers/generation/utils.py
@@ -1216,6 +1216,8 @@ class GaudiGenerationMixin(GenerationMixin):
             True if generation_config.flash_attention_fast_softmax else False
         )
         model_kwargs["num_virtual_tokens"] = num_virtual_tokens
+        if generation_config.valid_sequence_lengths is not None:
+            model_kwargs["valid_sequence_lengths"] = generation_config.valid_sequence_lengths
 
         if not self.config.is_encoder_decoder:
             calculated_max_length = input_ids.shape[1] + num_virtual_tokens
