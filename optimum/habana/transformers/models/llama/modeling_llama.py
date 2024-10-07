d85e50fd0eadf1ad2fc917a320971391fa119718
Adam Stachowicz <105052242+astachowiczhabana@users.noreply.github.com>
2024-10-07 10:43:13 +0200

    Truncate the cached max seq len (#1394)
    Co-authored-by: Jay <jaygala@habana.ai>

--- optimum/habana/transformers/models/llama/modeling_llama.py
+++ optimum/habana/transformers/models/llama/modeling_llama.py
@@ -119,6 +119,9 @@ class GaudiLlamaRotaryEmbedding(torch.nn.Module):
             else:
                 self.rope_type = "default"
             self.max_seq_len_cached = config.max_position_embeddings
+            # Truncate the cached max sequence length to 8k to limit cached register buffer size
+            if config.max_position_embeddings >= 8192:
+                self.max_seq_len_cached = 8192
             self.original_max_seq_len = config.max_position_embeddings
 
         self.config = config
