104478a2b9b9367771d0033e3b3699036adae184
Sun Choi <schoi@habana.ai>
2024-10-23 20:44:27 +0000

    enable DeepSpeed for image-to-text example

--- optimum/habana/transformers/models/clip/modeling_clip.py
+++ optimum/habana/transformers/models/clip/modeling_clip.py
@@ -156,7 +156,7 @@ class GaudiCLIPAttention(CLIPAttention):
 
         attn_output = attn_output.view(bsz, self.num_heads, tgt_len, self.head_dim)
         attn_output = attn_output.transpose(1, 2)
-        attn_output = attn_output.reshape(bsz, tgt_len, embed_dim)
+        attn_output = attn_output.reshape(bsz, tgt_len, -1)
 
         attn_output = self.out_proj(attn_output)
 
