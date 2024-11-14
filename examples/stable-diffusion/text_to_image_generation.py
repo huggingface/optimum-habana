0a625eb5dc60f236ae14c0b6dda729d423614121
sushil Dubey <sdubey@habana.ai>
2024-11-15 00:08:29 +0530

    [SW-204303] Enable Fp8 flow with INC for sdxl in OH (#11)
    Change-Id: I5bf82613200b4a1dd08882c3731dab57d28992cd

--- examples/stable-diffusion/text_to_image_generation.py
+++ examples/stable-diffusion/text_to_image_generation.py
@@ -439,10 +439,29 @@ def main():
                 **kwargs,
             )
 
-            pipeline.to(torch.device("hpu"))
             pipeline.unet.set_default_attn_processor(pipeline.unet)
+            pipeline.to(torch.device("hpu"))
+
+            quant_config_path = os.getenv("QUANT_CONFIG")
+            if quant_config_path:
+                import habana_frameworks.torch.core as htcore
+                from neural_compressor.torch.quantization import FP8Config, convert, prepare
+
+                htcore.hpu_set_env()
+
+                config = FP8Config.from_json_file(quant_config_path)
+
+                if config.measure:
+                    logger.info("Running measurements")
+                    pipeline.unet = prepare(pipeline.unet, config)
+                elif config.quantize:
+                    logger.info("Running quantization")
+                    pipeline.unet = convert(pipeline.unet, config)
+                htcore.hpu_initialize(pipeline.unet, mark_only_scales_as_const=True)
+
             if args.use_hpu_graphs:
                 pipeline.unet = torch_hpu.wrap_in_hpu_graph(pipeline.unet)
+
         else:
             from optimum.habana.diffusers import GaudiStableDiffusionXLPipeline
 
@@ -587,6 +606,12 @@ def main():
                 outputs = pipeline(prompt_embeds=prompt_embeds, **kwargs_call)
         else:
             outputs = pipeline(prompt=args.prompts, **kwargs_call)
+
+    if args.optimize and quant_config_path and config.measure:
+        from neural_compressor.torch.quantization import finalize_calibration
+
+        logger.info("Finalizing calibration...")
+        finalize_calibration(pipeline.unet)
 
     # Save the pipeline in the specified directory if not None
     if args.pipeline_save_dir is not None:
