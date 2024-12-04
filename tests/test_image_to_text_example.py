d738a4700883ad1e5c6deb0db1a06e86a42f3dc2
Sun Choi <schoi@habana.ai>
2024-12-04 22:34:32 +0000

    update README and target performance

--- tests/test_image_to_text_example.py
+++ tests/test_image_to_text_example.py
@@ -21,8 +21,8 @@ if os.environ.get("GAUDI2_CI", "0") == "1":
             ("llava-hf/llava-v1.6-vicuna-13b-hf", 1, 23.527610042925),
             ("google/paligemma-3b-mix-224", 1, 132.8949150246155),
             ("HuggingFaceM4/idefics2-8b", 1, 21.89944593215077),
-            ("meta-llama/Llama-3.2-11B-Vision-Instruct", 1, 20.407843538649303),
-            ("tiiuae/falcon-11B-vlm", 1, 27.0566558689559327),
+            ("meta-llama/Llama-3.2-11B-Vision-Instruct", 1, 18.974541922240313),
+            ("tiiuae/falcon-11B-vlm", 1, 23.69260849957278),
         ],
         "fp8": [
             ("llava-hf/llava-1.5-7b-hf", 1, 98.72578382705062),
