9361434ebfe728b2b2ea79654021d44d6cca7b4a
Kalyan Kumar <kalyan.kumar@intel.com>
2024-12-10 14:04:42 +0530

    [SW-207965] Add batch splitting in attention layer to hide NIC latency (#14)
        - Introduced the `--attn_batch_split` parameter to enable batch
        splitting in the attention and mlp layer.
        - This approach aims to overlap communication and computation,
        effectively hiding NIC latency during distributed attention operations.
    
        - Perform the add in the beginning of the next layer for better pipelining
    
        - Updated Readme
    
        - [SW-212702] Fix the  attn_batch_split argument specific to llama config (#74)
    
    Co-authored-by: Kalyan <kkumar@habana.ai>

--- optimum/habana/transformers/generation/configuration_utils.py
+++ optimum/habana/transformers/generation/configuration_utils.py
@@ -37,6 +37,8 @@ class GaudiGenerationConfig(GenerationConfig):
         Whether to enable causal_mask if use Habana flash attention.
     flash_attention_fast_softmax_mode (`bool`, *optional*):
         Whether to use fast softmax with reduced precision if use Habana flash attention.
+    attn_batch_split (`int`, *optional*):
+        Specify the batch size split for attention and mlp layers. 1 for no split. This is enabled only for prompt.
     """
 
     def __init__(self, **kwargs):
@@ -56,3 +58,4 @@ class GaudiGenerationConfig(GenerationConfig):
         self.flash_attention_fast_softmax = kwargs.get("flash_attention_fast_softmax", None)
         self.use_fused_rope = kwargs.get("use_fused_rope", None)
         self.valid_sequence_lengths = kwargs.get("valid_sequence_lengths", None)
+        self.attn_batch_split = kwargs.get("attn_batch_split", 1)
