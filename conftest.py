a44fc10f40f5b9c8577e39288a41bac8f1fa2d75
regisss <15324346+regisss@users.noreply.github.com>
2024-12-11 16:34:12 +0000

    Make style

--- conftest.py
+++ conftest.py
@@ -119,9 +119,10 @@ def pytest_collection_modifyitems(items):
 
 
 def pytest_addoption(parser):
-    parser.addoption("--token", action="store", default=None)    
-    
+    parser.addoption("--token", action="store", default=None)
+
     from transformers.testing_utils import pytest_addoption_shared
+
     pytest_addoption_shared(parser)
 
 
