c3f52260891d55ecccf463b2ae81ea2d5d42256f
Iman Gohari <s.m.iman.gohari@intel.com>
2024-12-13 22:41:19 +0000

    fix(test_diffusers): Added extra set_default_attn_processor

--- tests/test_diffusers.py
+++ tests/test_diffusers.py
@@ -660,6 +660,7 @@ class GaudiStableDiffusionPipelineTester(TestCase):
             gaudi_config=GaudiConfig.from_pretrained("Habana/stable-diffusion"),
             torch_dtype=torch.bfloat16,
         )
+        pipeline.unet.set_default_attn_processor(pipeline.unet)
         set_seed(27)
         outputs = pipeline(
             prompt=prompts,
@@ -714,6 +715,7 @@ class GaudiStableDiffusionPipelineTester(TestCase):
             use_hpu_graphs=True,
             gaudi_config=GaudiConfig(use_torch_autocast=False),
         )
+        pipeline.unet.set_default_attn_processor(pipeline.unet)
 
         prompt = "An image of a squirrel in Picasso style"
         generator = torch.manual_seed(seed)
