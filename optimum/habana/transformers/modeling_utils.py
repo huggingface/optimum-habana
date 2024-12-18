5f3f0eaa4d61c7add423360071ad518fd880837f
Urszula Golowicz <urszula.golowicz@intel.com>
2024-12-18 08:37:49 +0100

    [SW-209763] Reenable fused sdpa in wav2vec (#79)
    Fixing a bug where fused sdpa attention for wav2vec was not used
    despite monkey-patching the class.
    
    Signed-off-by: Urszula Golowicz <urszula.golowicz@intel.com>

--- optimum/habana/transformers/modeling_utils.py
+++ optimum/habana/transformers/modeling_utils.py
@@ -248,6 +248,9 @@ def adapt_transformers_to_gaudi():
     transformers.models.wav2vec2.modeling_wav2vec2.Wav2Vec2ForCTC.forward = gaudi_wav2vec2forctc_forward
     transformers.models.wav2vec2.modeling_wav2vec2.TDNNLayer.forward = gaudi_wav2vec2_tdnnlayer_forward
     transformers.models.wav2vec2.modeling_wav2vec2.Wav2Vec2SdpaAttention = GaudiWav2Vec2SdpaAttention
+    transformers.models.wav2vec2.modeling_wav2vec2.WAV2VEC2_ATTENTION_CLASSES.update(
+        {"sdpa": GaudiWav2Vec2SdpaAttention}
+    )
 
     # Generation is modified to run faster in lazy mode
     transformers.generation.GenerationMixin.generate = GaudiGenerationMixin.generate
