1e747dd6d522eb26f2f058faba5e81407f58cd6c
Shiv Kaul <skaul@habana.ai>
2024-12-20 11:16:59 -0800

    add sdp_on_bf16 to controlnet (#1631)
    * add sdp_on_bf16 to controlnet
    
    * Update pipeline_controlnet.py
    
    pass sdp_on_bf16 to controlnet_pipeline
    
    * Update text_to_image_generation.py
    
    * Update text_to_image_generation.py

--- optimum/habana/diffusers/pipelines/controlnet/pipeline_controlnet.py
+++ optimum/habana/diffusers/pipelines/controlnet/pipeline_controlnet.py
@@ -100,6 +100,7 @@ class GaudiStableDiffusionControlNetPipeline(GaudiDiffusionPipeline, StableDiffu
         use_hpu_graphs: bool = False,
         gaudi_config: Union[str, GaudiConfig] = None,
         bf16_full_eval: bool = False,
+        sdp_on_bf16: bool = True,
     ):
         GaudiDiffusionPipeline.__init__(
             self,
@@ -107,6 +108,7 @@ class GaudiStableDiffusionControlNetPipeline(GaudiDiffusionPipeline, StableDiffu
             use_hpu_graphs,
             gaudi_config,
             bf16_full_eval,
+            sdp_on_bf16,
         )
 
         StableDiffusionControlNetPipeline.__init__(
