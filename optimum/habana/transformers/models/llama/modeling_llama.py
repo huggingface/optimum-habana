1da6c35c29bfd9fa5a625b9ebae0538ee31a53c4
Yeonsil Yoon <yyoon@habana.ai>
2024-12-24 07:34:32 -0800

    Added missing parameter for llama function call (#1663)
    Co-authored-by: Libin Tang <litang@habana.ai>

--- optimum/habana/transformers/models/llama/modeling_llama.py
+++ optimum/habana/transformers/models/llama/modeling_llama.py
@@ -634,7 +634,7 @@ class GaudiLlamaAttention(LlamaAttention):
             )
             position_ids = position_ids.unsqueeze(0)
 
-        query_states, key_states = apply_customized_rope(query_states, key_states, cos, sin, position_ids)
+        query_states, key_states = apply_customized_rope(query_states, key_states, cos, sin, position_ids, self.training)
 
         if use_cache:
             # reuse k, v, self_attention
