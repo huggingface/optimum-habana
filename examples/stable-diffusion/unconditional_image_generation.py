af1334da71fcf2e96ba8ac4ae52ce4b18c1e6ea4
regisss <15324346+regisss@users.noreply.github.com>
2024-12-25 16:44:08 +0000

    Release: v1.15.0

--- examples/stable-diffusion/unconditional_image_generation.py
+++ examples/stable-diffusion/unconditional_image_generation.py
@@ -20,7 +20,7 @@ except ImportError:
 
 
 check_min_version("4.45.0")
-check_optimum_habana_min_version("1.14.0.dev0")
+check_optimum_habana_min_version("1.15.0")
 
 # Setup logging
 logging.basicConfig(
