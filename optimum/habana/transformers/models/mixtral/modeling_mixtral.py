c3ec0150dcd455a5d567c12d77b4d61791720a34
Sheng Yang <yang.sheng@intel.com>
2025-01-15 17:03:25 +0800

    Fix graph breaks in Mixtral (#65)

--- optimum/habana/transformers/models/mixtral/modeling_mixtral.py
+++ optimum/habana/transformers/models/mixtral/modeling_mixtral.py
@@ -20,7 +20,6 @@
 
 """PyTorch Mixtral model."""
 
-import contextlib
 import math
 from typing import List, Optional, Tuple, Union
 
@@ -73,18 +72,12 @@ except ImportError:
     print("Not using HPU fused scaled dot-product attention kernel.")
     FusedSDPA = None
 
-try:
-    from habana_frameworks.torch.hpu import sdp_kernel
-
-    SDPContext = True
-except ImportError:
-    SDPContext = False
-
+deepspeed_available = is_deepspeed_available()
 logger = logging.get_logger(__name__)
 
 
 def apply_customized_rope(q, k, cos, sin, position_ids):
-    if q.device.type == "hpu" and FusedRoPE:
+    if q.device.type == "hpu" and FusedRoPE is not None:
         return FusedRoPE.apply(
             q, cos.unsqueeze(0).unsqueeze(0), sin.unsqueeze(0).unsqueeze(0), position_ids
         ), FusedRoPE.apply(k, cos.unsqueeze(0).unsqueeze(0), sin.unsqueeze(0).unsqueeze(0), position_ids)
@@ -98,7 +91,7 @@ def gaudi_mixtral_rmsnorm_forward(self, hidden_states):
     The only differences are:
         - override RMSNorm with Habana fused RMSNorm
     """
-    if hidden_states.device.type == "hpu" and FusedRMSNorm:
+    if hidden_states.device.type == "hpu" and FusedRMSNorm is not None:
         # mixed dtypes are not good for FusedRMSNorm, both inputs need to have same dtype
         if hidden_states.dtype != self.weight.dtype:
             orig_dtype = hidden_states.dtype
@@ -201,7 +194,7 @@ class GaudiMixtralSparseMoeBlock(MixtralSparseMoeBlock):
         # router_logits: (batch * sequence_length, n_experts)
         router_logits = self.gate(hidden_states)
 
-        if is_deepspeed_available() and (not self.training):
+        if deepspeed_available and (not self.training):
             from deepspeed import comm as dist
 
             if dist.is_initialized():
@@ -216,7 +209,7 @@ class GaudiMixtralSparseMoeBlock(MixtralSparseMoeBlock):
             expert_routing_table=selected_experts,
             router_weights=routing_weights,
         )
-        if is_deepspeed_available() and (not self.training):
+        if deepspeed_available and (not self.training):
             from deepspeed import comm as dist
 
             if dist.is_initialized():
@@ -404,7 +397,7 @@ class GaudiMixtralAttention(MixtralAttention):
         else:
             past_key_value = None
 
-        if FusedSDPA:
+        if FusedSDPA is not None:
             if query_states.dtype != key_states.dtype:
                 key_states = key_states.type(query_states.dtype)
                 value_states = value_states.type(query_states.dtype)
@@ -421,12 +414,17 @@ class GaudiMixtralAttention(MixtralAttention):
                 )
                 htcore.mark_step()
             else:
-                with (
-                    sdp_kernel(enable_recompute=flash_attention_recompute) if SDPContext else contextlib.nullcontext()
-                ):
-                    attn_output = FusedSDPA.apply(
-                        query_states, key_states, value_states, attention_mask, 0.0, False, None
-                    )
+                attn_output = FusedSDPA.apply(
+                    query_states,
+                    key_states,
+                    value_states,
+                    attention_mask,
+                    0.0,
+                    False,
+                    None,
+                    "None",
+                    flash_attention_recompute,
+                )
         else:
             query_states, key_states, value_states, attention_mask = gaudi_mixtral_repeat_kv(
                 query_states, key_states, value_states, attention_mask, self.num_key_value_groups
@@ -450,7 +448,7 @@ class GaudiMixtralAttention(MixtralAttention):
 
         attn_output = self.o_proj(attn_output)
 
-        if not output_attentions or FusedSDPA:
+        if not output_attentions or FusedSDPA is not None:
             attn_weights = None
 
         return attn_output, attn_weights, past_key_value
