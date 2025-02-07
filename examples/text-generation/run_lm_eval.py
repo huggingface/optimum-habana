4689b0a537a5027b9157f821c350bc7fb8e7541d
12010486 <silvia.colabrese@intel.com>
2025-02-07 10:56:43 +0000

    gen_kwargs from config_generation

--- examples/text-generation/run_lm_eval.py
+++ examples/text-generation/run_lm_eval.py
@@ -385,12 +385,12 @@ def main() -> None:
 
     with torch.no_grad():
         lm = HabanaModelAdapter(tokenizer, model, args, generation_config)
-
-    # Regroup some args in gen_kwargs, defined as "String arguments for model generation on greedy_until tasks, e.g. `temperature=0,top_k=0,top_p=0`."
-    gen_kwargs = f"temperature={args.temperature}"
+    
+    # Regroup part of generation_config as gen_kwargs, defined as "String arguments for model generation on greedy_until tasks, e.g. `temperature=0,top_k=0,top_p=0`."
+    gen_kwargs = f"temperature={generation_config.temperature}"
     if args.top_k is not None:
-        gen_kwargs += f",top_k={args.top_k}"
-    gen_kwargs += f",top_p={args.top_p}"
+        gen_kwargs += f",top_k={generation_config.top_k}"
+    gen_kwargs += f",top_p={generation_config.top_p}"
 
     eval_start = time.perf_counter()
     with torch.no_grad():
