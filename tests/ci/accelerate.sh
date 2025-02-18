1e2d3f16e20ba229f23b40882c8e8550156be2a2
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2025-02-18 15:10:31 +0000

    fix

--- tests/ci/accelerate.sh
+++ tests/ci/accelerate.sh
@@ -6,13 +6,13 @@ echo "HABANA_VISIBLE_MODULES=${HABANA_VISIBLE_MODULES}"
 
 git clone https://github.com/huggingface/accelerate.git --branch hpu-support --depth 1 && cd accelerate && pip install -e .[testing] git+https://github.com/HabanaAI/DeepSpeed.git@1.19.0
 
-RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 HABANA_VISIBLE_MODULES="" make test_cli
+RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 make test_cli
 #       PT_ENABLE_INT64_SUPPORT: 1 # for tokenizers
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 PT_HPU_LAZY_MODE=0 HABANA_VISIBLE_MODULES="" make test_core
+RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 PT_HPU_LAZY_MODE=0 make test_core
 #      PT_ENABLE_INT64_SUPPORT: 1 # for tokenizers
 #      PT_HPU_LAZY_MODE: 0 # for fsdp
 if [ $? -ne 0 ]; then
