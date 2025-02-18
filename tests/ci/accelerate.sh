b6537273b5424d113a6b490958e9511f3349c04d
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2025-02-18 12:26:14 +0000

    use runner's HABANA_VISIBLE_DEVICES

--- tests/ci/accelerate.sh
+++ tests/ci/accelerate.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+# Description: Accelerate CI script
 
-OPTIMUM_HABANA_PATH=${CI_OPTIMUM_HABANA_PATH:-/root/workspace/optimum-habana}
-ACCELERATE_PATH=${CI_SENTENCE_TRANSFORMER_PATH:-/root/workspace/accelerate}
+git clone https://github.com/huggingface/accelerate.git --branch hpu-support --depth 1 && cd accelerate
+pip install -e .[testing] git+https://github.com/HabanaAI/DeepSpeed.git@1.19.0
 
-python -m pip install --upgrade pip
-python -m pip install $OPTIMUM_HABANA_PATH[tests]
-cd $ACCELERATE_PATH
-python -m pip install .
-RUN_SLOW=1 pytest tests/test_accelerator.py
+RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 make test_cli
+RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 PT_HPU_LAZY_MODE=0 make test_core
+RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 PT_HPU_LAZY_MODE=0 make test_fsdp
+RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 PT_HPU_LAZY_MODE=0 make test_deepspeed
+RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 PT_HPU_LAZY_MODE=0 make test_big_modeling
