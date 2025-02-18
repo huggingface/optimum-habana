62c64fdb6b881e53bf09f015ac14f0eaafa0c822
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2025-02-18 19:42:11 +0000

    fix

--- tests/ci/accelerate.sh
+++ tests/ci/accelerate.sh
@@ -5,37 +5,40 @@ echo "HABANA_VISIBLE_DEVICES=${HABANA_VISIBLE_DEVICES}"
 echo "HABANA_VISIBLE_MODULES=${HABANA_VISIBLE_MODULES}"
 
 git clone https://github.com/huggingface/accelerate.git --branch hpu-support --depth 1 && cd accelerate && pip install -e .[testing] git+https://github.com/HabanaAI/DeepSpeed.git@1.19.0
+export ACCELERATE_BYPASS_DEVICE_MAP=true
+export PT_ENABLE_INT64_SUPPORT=1
+export PT_HPU_LAZY_MODE=0
+export RUN_SLOW=1
 
-RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 make test_cli
-#       PT_ENABLE_INT64_SUPPORT: 1 # for tokenizers
+fuser -k 29500/tcp
+
+echo "Running CLI tests"
+make test_cli
+
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 PT_HPU_LAZY_MODE=0 make test_core
-#      PT_ENABLE_INT64_SUPPORT: 1 # for tokenizers
-#      PT_HPU_LAZY_MODE: 0 # for fsdp
+echo "Running Core tests"
+make test_core
+
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 PT_HPU_LAZY_MODE=0 make test_fsdp
-#      PT_ENABLE_INT64_SUPPORT: 1 # for tokenizers
-#      PT_HPU_LAZY_MODE: 0 # for fsdp
+echo "Running FSDP tests"
+make test_fsdp
+
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 PT_HPU_LAZY_MODE=0 make test_deepspeed
-#      PT_ENABLE_INT64_SUPPORT: 1 # for tokenizers
-#      PT_HPU_LAZY_MODE: 0 # for cpu offload
+echo "Running DeepSpeed tests"
+make test_deepspeed
+
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-RUN_SLOW=1 PT_ENABLE_INT64_SUPPORT=1 PT_HPU_LAZY_MODE=0 make test_big_modeling
-#      PT_ENABLE_INT64_SUPPORT: 1 # for tokenizers
-#      PT_HPU_LAZY_MODE: 0 # for cpu offload
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+echo "Running Big Modeling tests"
+make test_big_modeling
