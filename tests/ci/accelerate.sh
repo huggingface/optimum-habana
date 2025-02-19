7a7815a9299ff769b50d8231347e6cbc2ef1e9cf
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2025-02-19 11:05:11 +0000

    test

--- tests/ci/accelerate.sh
+++ tests/ci/accelerate.sh
@@ -4,29 +4,30 @@ hl-smi
 echo "HABANA_VISIBLE_DEVICES=${HABANA_VISIBLE_DEVICES}"
 echo "HABANA_VISIBLE_MODULES=${HABANA_VISIBLE_MODULES}"
 
-# Install Accelerate and DeepSpeed
-git clone https://github.com/huggingface/accelerate.git
-cd accelerate
-git checkout hpu-support
-git pull
-pip install -e .[testing]
-pip install git+https://github.com/HabanaAI/DeepSpeed.git@1.19.0
-
 # Install Rust and build Safetensors
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 rustup update
 git clone https://github.com/huggingface/safetensors
 cd safetensors
-git checkout fa833511664338bfc927fc02653ddb7d38d40be9
 pip install setuptools_rust
+git checkout fa833511664338bfc927fc02653ddb7d38d40be9
 pip install -e bindings/python
 cd ..
+
+# Install Accelerate and DeepSpeed
+git clone https://github.com/huggingface/accelerate.git
+cd accelerate
+git checkout hpu-support
+pip install -e .[testing]
+pip install git+https://github.com/HabanaAI/DeepSpeed.git@1.19.0
 
 # Set environment variables
 export PT_ENABLE_INT64_SUPPORT=1
 export PT_HPU_LAZY_MODE=0
 export RUN_SLOW=1
+
+# CORE FEATURES TESTS
 
 echo "Running CLI tests"
 make test_cli
@@ -47,6 +48,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # INTEGRATION TESTS
+
 echo "Running FSDP integration tests"
 make test_fsdp
 if [ $? -ne 0 ]; then
