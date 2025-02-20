7190fbe05b20c26b515be66b698fa0daf783eaad
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2025-02-20 09:56:04 +0000

    test

--- tests/ci/accelerate.sh
+++ tests/ci/accelerate.sh
@@ -23,6 +23,9 @@ pip install -e .[testing]
 pip install git+https://github.com/HabanaAI/DeepSpeed.git@1.19.0
 
 # Set environment variables
+export LOG_LEVEL_PT_BRIDGE=0 
+export LOG_LEVEL_GC=0 
+export ENABLE_CONSOLE=true
 export PT_HPU_EAGER_PIPELINE_ENABLE=0
 export PT_ENABLE_INT64_SUPPORT=1
 export PT_HPU_LAZY_MODE=0
@@ -30,42 +33,41 @@ export RUN_SLOW=1
 
 # CUSTOM TESTS
 
-pytest tests/ -s -vvvv -k "test_cpu_offload_gpt2"
-pytest tests/ -s -vvvv -k "test_disk_offload_gpt2"
-pytest tests/ -s -vvvv -k "test_accelerated_optimizer_step_was_skipped"
-pytest tests/ -s -vvvv -k "test_pad_across_processes"
-pytest tests/ -s -vvvv -k "test_multi_device_merge_fsdp_weights"
+pytest tests/ -s -vvvv -k "test_cpu_offload_gpt2 or test_disk_offload_gpt2"
+# pytest tests/ -s -vvvv -k "test_accelerated_optimizer_step_was_skipped"
+# pytest tests/ -s -vvvv -k "test_pad_across_processes"
+# pytest tests/ -s -vvvv -k "test_multi_device_merge_fsdp_weights"
 
-# CORE FEATURES TESTS
+# # CORE FEATURES TESTS
 
-echo "Running CLI tests"
-make test_cli
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+# echo "Running CLI tests"
+# make test_cli
+# if [ $? -ne 0 ]; then
+#     exit 1
+# fi
 
-echo "Running Core tests"
-make test_core
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+# echo "Running Core tests"
+# make test_core
+# if [ $? -ne 0 ]; then
+#     exit 1
+# fi
 
-echo "Running Big Modeling tests"
-make test_big_modeling
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+# echo "Running Big Modeling tests"
+# make test_big_modeling
+# if [ $? -ne 0 ]; then
+#     exit 1
+# fi
 
-# INTEGRATION TESTS
+# # INTEGRATION TESTS
 
-echo "Running FSDP integration tests"
-make test_fsdp
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+# echo "Running FSDP integration tests"
+# make test_fsdp
+# if [ $? -ne 0 ]; then
+#     exit 1
+# fi
 
-echo "Running DeepSpeed integration tests"
-make test_deepspeed
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+# echo "Running DeepSpeed integration tests"
+# make test_deepspeed
+# if [ $? -ne 0 ]; then
+#     exit 1
+# fi
