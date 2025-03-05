f6fa5463507848f1743e174c805433c02e8c8c44
Kalyan Kumar <kalyan.kumar@intel.com>
2025-03-05 10:22:48 +0530

    [SW-218544] Perform the transpose in BF16 to optimize execution time (#186)

--- optimum/habana/transformers/models/mllama/modeling_mllama.py
+++ optimum/habana/transformers/models/mllama/modeling_mllama.py
@@ -84,17 +84,18 @@ def _prepare_cross_attention_mask(
     The only differences are:
         - if there's pading in cross_attention_mask in the right. do not masked it, or else it will impact softmax in crossattention
     """
-    # reshape so it can be used by attn module
-    batch_size, text_total_length, *_ = cross_attention_mask.shape
-    cross_attention_mask = cross_attention_mask.repeat_interleave(num_vision_tokens, dim=3)
-    cross_attention_mask = cross_attention_mask.view(batch_size, text_total_length, -1)
-    cross_attention_mask = cross_attention_mask.unsqueeze(1)
 
     # invert the mask
     inverted_cross_attn_mask = (1.0 - cross_attention_mask).to(dtype)
     cross_attention_mask = inverted_cross_attn_mask.masked_fill(
         inverted_cross_attn_mask.to(torch.bool), torch.finfo(dtype).min
     )
+
+    # reshape so it can be used by attn module
+    batch_size, text_total_length, *_ = cross_attention_mask.shape
+    cross_attention_mask = cross_attention_mask.repeat_interleave(num_vision_tokens, dim=3)
+    cross_attention_mask = cross_attention_mask.view(batch_size, text_total_length, -1)
+    cross_attention_mask = cross_attention_mask.unsqueeze(1)
 
     # apply full-row bias, which return 4D tensor of shape [B, H, S1, 1] where value is 0 if the a full row in cross attn mask's
     # last dimension contains negative infinity values, otherwise it's 1
