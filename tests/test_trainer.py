64072f89330bd04cbf8a24d35e098c1d2db90491
Luca Calabria <luca.calabria@intel.com>
2025-03-11 14:21:11 +0100

    update target value

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -834,7 +834,7 @@ class GaudiTrainerIntegrationPrerunTest(TestCasePlus, GaudiTrainerIntegrationCom
             self.assertLess(max(diff_truth), 0.01, f"Difference {max(diff_truth)} is not within 0.01")
 
             # max diff broken should be very off
-            self.assertGreater(max(diff_broken), 1.5, f"Difference {max(diff_broken)} is not greater than 2")
+            self.assertGreater(max(diff_broken), 1.5, f"Difference {max(diff_broken)} is not greater than 1.5")
 
             loss_base = sum(base_loss_callback.losses)
             loss_broken = sum(broken_loss_callback.losses)
