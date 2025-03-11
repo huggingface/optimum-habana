183d83de8ce92af39f0d5c30617d7b0d8597b2d8
Shmuel Markovich Golan <smarkovich@habana.ai>
2025-03-11 10:50:47 +0200

    Enable metrics in run_lm_eval.py (#176)
    * metrics
    
    * metrics
    
    * metrics
    
    ---------
    
    Co-authored-by: Ronit Wisenstern <rwisenstern@habana.ai>
    Co-authored-by: Dany Kiazada <141814181+kiazada@users.noreply.github.com>

--- examples/text-generation/run_lm_eval.py
+++ examples/text-generation/run_lm_eval.py
@@ -85,7 +85,12 @@ def setup_lm_eval_parser():
         help="Tasks to run",
         default=["hellaswag", "lambada_openai", "piqa", "winogrande"],
     )
-    parser.add_argument("--limit_iters", type=int, help="limit examples to run that many iterations", default=None)
+    parser.add_argument("--limit_iters", type=float, help="limit examples to run that many iterations", default=None)
+    parser.add_argument(
+        "--metrics",
+        action="store_true",
+        help="Store metrics per query",
+    )
     parser.add_argument("--max_graphs", type=int, help="Maximum number of HPU graphs", default=None)
     args = setup_parser(parser)
 
@@ -211,7 +216,13 @@ def main():
 
     eval_start = time.perf_counter()
     with torch.no_grad():
-        results = lm_eval.evaluator.evaluate(lm, lm_tasks, limit=args.limit_iters)
+        results = lm_eval.evaluator.evaluate(
+            lm,
+            lm_tasks,
+            limit=args.limit_iters,
+            write_out=args.metrics and args.local_rank == 0,
+            output_base_path=os.path.dirname(args.output_file),
+        )
     if args.device == "hpu":
         import habana_frameworks.torch.hpu as torch_hpu
 
