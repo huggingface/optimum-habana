80689f334a85ae19efe5c213e6505f3c89b8a1a8
Xinyu Chen <xichen@habana.ai>
2025-03-12 13:00:06 +0800

    [SW-221731] Support inline_inbuilt_nn_modules (#196)
    Change-Id: Id7a21c0c1e014e9dc8c503c645a252ad4da136fb

--- optimum/habana/transformers/training_args.py
+++ optimum/habana/transformers/training_args.py
@@ -109,6 +109,8 @@ class GaudiTrainingArguments(TrainingArguments):
             Whether to use regional compilation with deepspeed
         inline_inbuilt_nn_modules (`bool`, *optional*, defaults to `None`):
             Set value of 'inline_inbuilt_nn_modules' parameter for torch._dynamo.config.
+        allow_unspec_int_on_nn_module (`bool`, *optional*, defaults to `None`):
+            Set value of 'allow_unspec_int_on_nn_module' parameter for torch._dynamo.config.
         disable_tensor_cache_hpu_graphs (`bool`, *optional*, defaults to `False`):
             Whether to disable tensor cache when using hpu graphs. If True, tensors won't be cached in hpu graph and memory can be saved.
         max_hpu_graphs (`int`, *optional*):
@@ -205,6 +207,13 @@ class GaudiTrainingArguments(TrainingArguments):
     inline_inbuilt_nn_modules: Optional[bool] = field(
         default=None,
         metadata={"help": ("Set value of 'inline_inbuilt_nn_modules' parameter for torch._dynamo.config.")},
+    )
+
+    # This works only if compile kwarg "dynamic = None" or "dynamic = True" is
+    # set and has no effect when "dynamic = False"
+    allow_unspec_int_on_nn_module: Optional[bool] = field(
+        default=None,
+        metadata={"help": ("Set value of 'allow_unspec_int_on_nn_module' parameter for torch._dynamo.config.")},
     )
 
     disable_tensor_cache_hpu_graphs: Optional[bool] = field(
@@ -910,6 +919,9 @@ class GaudiTrainingArguments(TrainingArguments):
 
         if self.inline_inbuilt_nn_modules is not None:
             torch._dynamo.config.inline_inbuilt_nn_modules = self.inline_inbuilt_nn_modules
+
+        if self.allow_unspec_int_on_nn_module is not None:
+            torch._dynamo.config.allow_unspec_int_on_nn_module = self.allow_unspec_int_on_nn_module
 
         if self.sdp_on_bf16:
             torch._C._set_math_sdp_allow_fp16_bf16_reduction(True)
