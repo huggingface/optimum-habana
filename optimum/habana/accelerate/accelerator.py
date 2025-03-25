e4e77ce7eaa411ddf29fa59e0cfddb2d27b91fc6
Xinyu Chen <xichen@habana.ai>
2025-03-25 13:30:50 +0800

    [SW-221722] fix lost modules in regional compilation (#200)
    Change-Id: I8cd89a0f2e58e94582c603f3d936a6d013c7c125
    
    Co-authored-by: Vivek Goel <vgoel@habana.ai>

--- optimum/habana/accelerate/accelerator.py
+++ optimum/habana/accelerate/accelerator.py
@@ -589,8 +589,14 @@ class GaudiAccelerator(Accelerator):
                 else:
                     module.compile(**self.state.dynamo_plugin.to_kwargs())
         else:
-            for _, module in model.named_children():
-                self.compile_regions(module)
+            if model._modules:
+                for _, module in model.named_children():
+                    self.compile_regions(module)
+            else:
+                if self.dynamic is not None:
+                    model.compile(dynamic=self.dynamic, **self.state.dynamo_plugin.to_kwargs())
+                else:
+                    model.compile(**self.state.dynamo_plugin.to_kwargs())
 
     def _prepare_deepspeed(self, *args):
         import deepspeed
