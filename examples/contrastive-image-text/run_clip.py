a293fdc3894c6e0b1850193038013f75b8f1c2c4
regisss <15324346+regisss@users.noreply.github.com>
2025-04-11 22:00:17 +0000

    Bump dev version

--- examples/contrastive-image-text/run_clip.py
+++ examples/contrastive-image-text/run_clip.py
@@ -62,7 +62,7 @@ logger = logging.getLogger(__name__)
 
 # Will error if the minimal version of Transformers and Optimum Habana are not installed. Remove at your own risks.
 check_min_version("4.49.0")
-check_optimum_habana_min_version("1.17.0.dev0")
+check_optimum_habana_min_version("1.18.0.dev0")
 
 require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/contrastive-image-text/requirements.txt")
 
