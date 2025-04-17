1a0cf8dc220cb77cffea774988d504a42f81f4a5
varu060603 <127109909+varu060603@users.noreply.github.com>
2025-04-17 14:30:07 +0530

    Fix prompt argument handling in `run_pipeline.py`  (#1874)
    Signed-off-by: varu060603 <varshit.s@learner.manipal.edu>
    Co-authored-by: regisss <15324346+regisss@users.noreply.github.com>

--- examples/image-to-text/run_pipeline.py
+++ examples/image-to-text/run_pipeline.py
@@ -282,6 +282,18 @@ def main():
                 else:
                     args.prompt = f"User:{image_str}\nWhat is shown in this image?\nAssistant:"
 
+        else:
+            conversation = [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": f"{args.prompt}"},
+                        {"type": "image"},
+                    ],
+                }
+            ]
+            args.prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+
     image_paths = args.image_path
     image_paths_len = len(image_paths)
 
