b6fbd693c558884b6b8729eaf06c0f63e8ffb866
Edward Mascarenhas <edward.mascarenhas@intel.com>
2025-04-21 15:49:45 -0700

    Update image_processing_utils.py with copy comment.

--- optimum/habana/transformers/image_processing_utils.py
+++ optimum/habana/transformers/image_processing_utils.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Code copied from https://github.com/huggingface/transformers/blob/main/src/transformers/image_processing_utils.py
 
 from typing import Dict, Iterable, Optional, Union
 
