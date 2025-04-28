bfd88da988b8af052331104452cfbe7374cfa292
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-04-28 00:34:20 +0000

    Add WA patches to avoid IntImplicit Kernel Registry error in SDXL test

---
+++ optimum/habana/diffusers/models/unet_2d_blocks.py
@@ -0,0 +1,14 @@
+import torch
+from diffusers.models.unets.unet_2d_blocks import CrossAttnDownBlock2D, CrossAttnUpBlock2D
+
+CrossAttnDownBlock2D_original_forward = CrossAttnDownBlock2D.forward
+@torch.compiler.disable
+def gaudi_CrossAttnDownBlock2D_forward(self, *args, **kwargs):
+    return CrossAttnDownBlock2D_original_forward(self, *args, **kwargs)
+CrossAttnDownBlock2D.forward = gaudi_CrossAttnDownBlock2D_forward
+
+CrossAttnUpBlock2D_original_forward = CrossAttnUpBlock2D.forward
+@torch.compiler.disable
+def gaudi_CrossAttnUpBlock2D_forward(self, *args, **kwargs):
+    return CrossAttnUpBlock2D_original_forward(self, *args, **kwargs)
+CrossAttnUpBlock2D.forward = gaudi_CrossAttnUpBlock2D_forward
