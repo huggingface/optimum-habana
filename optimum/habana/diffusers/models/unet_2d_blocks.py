34dd0e3fbf3c27cfe32f867d176f34d3094ac0ad
Lau, Kiangpeng <kiangpeng.lau@intel.com>
2025-04-30 16:34:01 -0700

    Added torch.compile WA to disable UpDecoderBlock2D.forward
    Fixed generate blank image.

--- optimum/habana/diffusers/models/unet_2d_blocks.py
+++ optimum/habana/diffusers/models/unet_2d_blocks.py
@@ -1,5 +1,5 @@
 import torch
-from diffusers.models.unets.unet_2d_blocks import CrossAttnDownBlock2D, CrossAttnUpBlock2D
+from diffusers.models.unets.unet_2d_blocks import CrossAttnDownBlock2D, CrossAttnUpBlock2D, UpDecoderBlock2D
 
 CrossAttnDownBlock2D_original_forward = CrossAttnDownBlock2D.forward
 @torch.compiler.disable
@@ -12,3 +12,9 @@ CrossAttnUpBlock2D_original_forward = CrossAttnUpBlock2D.forward
 def gaudi_CrossAttnUpBlock2D_forward(self, *args, **kwargs):
     return CrossAttnUpBlock2D_original_forward(self, *args, **kwargs)
 CrossAttnUpBlock2D.forward = gaudi_CrossAttnUpBlock2D_forward
+
+UpDecoderBlock2D_original_forward = UpDecoderBlock2D.forward
+@torch.compiler.disable
+def gaudi_UpDecoderBlock2D_forward(self, *args, **kwargs):
+    return UpDecoderBlock2D_original_forward(self, *args, **kwargs)
+UpDecoderBlock2D.forward = gaudi_UpDecoderBlock2D_forward
