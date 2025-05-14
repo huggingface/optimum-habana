b1e7fb12aae9db3265943ab9d48c32b72cab9cf9
regisss <15324346+regisss@users.noreply.github.com>
2025-05-14 13:31:30 +0000

    Update workflows

--- tests/ci/sentence_transformers.sh
+++ tests/ci/sentence_transformers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-OPTIMUM_HABANA_PATH=${CI_OPTIMUM_HABANA_PATH:-/root/workspace/optimum-habana}
-SENTENCE_TRANSFORMER_PATH=${CI_SENTENCE_TRANSFORMER_PATH:-/root/workspace/sentence-transformers}
+OPTIMUM_HABANA_PATH=${CI_OPTIMUM_HABANA_PATH:-~/optimum-habana}
+SENTENCE_TRANSFORMER_PATH=${CI_SENTENCE_TRANSFORMER_PATH:-~/sentence-transformers}
 
 python -m pip install --upgrade pip
 python -m pip install $OPTIMUM_HABANA_PATH[tests]
 cd $SENTENCE_TRANSFORMER_PATH/tests
 python -m pip install ..
-pytest test_cmnrl.py test_multi_process.py test_compute_embeddings.py test_model_card_data.py test_util.py 
+pytest test_cmnrl.py test_multi_process.py test_compute_embeddings.py test_model_card_data.py test_util.py
 cd $OPTIMUM_HABANA_PATH/tests
 python -m pytest test_sentence_transformers.py
