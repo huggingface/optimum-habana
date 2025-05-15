85cf283e19278e50877ef9651424577c3510e839
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-05-15 13:36:23 +0200

    fix accelerate version (it's 1.7)

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ INSTALL_REQUIRES = [
     "transformers >= 4.49.0, < 4.50.0",
     "optimum",
     "torch",
-    "accelerate>=1.17.0",
+    "accelerate>=1.7.0",
     "diffusers >= 0.32.0, < 0.32.1",
     "huggingface_hub >= 0.24.7",
     "sentence-transformers == 3.3.1",
