cd09fb36b627f076edadafe6b786bd321306ef42
Ilyas Moutawwakil <57442720+IlyasMoutawwakil@users.noreply.github.com>
2025-06-13 14:04:08 +0100

    More makefile fixes (#2036)
    * add token to single card tests
    
    * remove non existent diffusers test group
    
    * more makefile and cli fixes
    
    * fix
    
    * Update Makefile

--- optimum/habana/accelerate/accelerator.py
+++ optimum/habana/accelerate/accelerator.py
@@ -396,7 +396,7 @@ class GaudiAccelerator(Accelerator):
             compile_kwargs = self.state.dynamo_plugin.to_kwargs()
             ############################################################################################################
             if self.use_regional_compilation:
-                compile_regions(model, compile_kwargs)
+                compile_regions(model, *compile_kwargs)
             else:
                 model = torch.compile(model, **compile_kwargs)
             ############################################################################################################
@@ -610,7 +610,7 @@ class GaudiAccelerator(Accelerator):
                 compile_kwargs = self.state.dynamo_plugin.to_kwargs()
                 ###############################################################################################################
                 if self.use_regional_compilation:
-                    compile_regions(engine.module, compile_kwargs)
+                    compile_regions(engine.module, *compile_kwargs)
                 else:
                     engine.compile(
                         backend=compile_kwargs.pop("backend"),
