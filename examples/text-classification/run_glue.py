1b98a158bd4ea357cda8fa3abbe5bc17a5bdb6bf
tianyuan211 <paulyuen@umich.edu>
2025-06-17 16:13:48 +0800

    Update run_glue.py

--- examples/text-classification/run_glue.py
+++ examples/text-classification/run_glue.py
@@ -442,9 +442,6 @@ def main():
         padding = False
 
     if model_args.add_pad_token:
-#       if not model.config.pad_token_id and not tokenizer.pad_token:
-#           tokenizer.pad_token = tokenizer.eos_token
-#           model.config.pad_token_id = tokenizer.eos_token_id
         pad_id = getattr(model.config, "pad_token_id", None)
         if pad_id is None or pad_id < 0:
             tokenizer.add_special_tokens({"pad_token": tokenizer.eos_token})
