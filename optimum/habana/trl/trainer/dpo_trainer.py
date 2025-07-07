8e011aff6e656433e21669c6a450e69cc94c0854
Piotr Bielak <pbielak@users.noreply.github.com>
2025-07-07 15:51:58 +0200

    Silence `Trainer.tokenizer` warnings (#2116)

--- optimum/habana/trl/trainer/dpo_trainer.py
+++ optimum/habana/trl/trainer/dpo_trainer.py
@@ -477,7 +477,7 @@ class GaudiDPOTrainer(DPOTrainer, GaudiTrainer):
             data_collator=data_collator,
             train_dataset=train_dataset,
             eval_dataset=eval_dataset,
-            tokenizer=tokenizer,
+            processing_class=tokenizer,
             model_init=model_init,
             compute_metrics=compute_metrics,
             callbacks=callbacks,
