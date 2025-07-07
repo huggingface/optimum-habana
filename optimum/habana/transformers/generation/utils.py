adbd2877c1e013efa367a8e8698190385115f11d
Adam Stachowicz <astachowicz@habana.ai>
2025-07-07 14:29:34 +0300

    Merge #2044 to main then rebase

--- optimum/habana/transformers/generation/utils.py
+++ optimum/habana/transformers/generation/utils.py
@@ -17,6 +17,7 @@
 import copy
 import inspect
 import math
+import time
 import warnings
 from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Union
 
@@ -2079,7 +2080,7 @@ class GaudiGenerationMixin(GenerationMixin):
                 inc = iter(incrementor(bucket_size, cur_len))
             if bucket_size > 0:
                 assert "position_ids" not in model_kwargs, "Untested path"
-
+        greedy_first = True
         token_idx = model_kwargs.get("token_idx", None)
         top_k_ids = None
         if token_idx is not None:
@@ -2475,6 +2476,13 @@ class GaudiGenerationMixin(GenerationMixin):
                 )
                 this_peer_finished = unfinished_sequences.max() == 0
 
+            if greedy_first:
+                import habana_frameworks.torch.hpu as torch_hpu
+
+                torch_hpu.synchronize()
+                print(f"First Token time(greedy):{time.perf_counter() * 1000}")
+                greedy_first = False
+
             if (
                 not model_kwargs.get("pad_done", False)
                 and not model_kwargs.get("reuse_cache", False)
@@ -2670,6 +2678,7 @@ class GaudiGenerationMixin(GenerationMixin):
         )
         hb_profer.start()
 
+        sample_first = True
         if not bucket_internal:
             if bucket_size >= 0:
                 inc = iter(incrementor(bucket_size, cur_len))
@@ -2873,6 +2882,13 @@ class GaudiGenerationMixin(GenerationMixin):
             # This is needed to properly delete outputs.logits which may be very large for first iteration
             # Otherwise a reference to outputs is kept which keeps the logits alive in the next iteration
             del outputs
+
+            if sample_first:
+                import habana_frameworks.torch.hpu as torch_hpu
+
+                torch_hpu.synchronize()
+                print(f"First Token time(sample):{time.perf_counter() * 1000}")
+                sample_first = False
 
         if (
             model_kwargs.get("use_hpu_graphs", False)
