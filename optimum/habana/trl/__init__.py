84bc77cf33214565921c27dd432240db68d56cf7
Sun Choi <schoi@habana.ai>
2025-07-28 03:43:57 -0700

    Enable trl GRPO trainer (#2088)
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>
    Signed-off-by: Daniel Socek <daniel.socek@intel.com>
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>
    Signed-off-by: Vivek Kumar <vivkumar@habana.ai>
    Signed-off-by: Urszula <urszula.golowicz@intel.com>
    Co-authored-by: regisss <15324346+regisss@users.noreply.github.com>
    Co-authored-by: Jimin Ha <jha@habana.ai>
    Co-authored-by: Shiv Kaul <skaul@habana.ai>
    Co-authored-by: Yeonsil Yoon <yyoon@habana.ai>
    Co-authored-by: Harish Subramony <81822986+hsubramony@users.noreply.github.com>
    Co-authored-by: Vidya Galli <vidya.s.galli@intel.com>
    Co-authored-by: Iman Gohari <s.m.iman.gohari@intel.com>
    Co-authored-by: Bhargav <beede@habana.ai>
    Co-authored-by: Chetan Kumar Verma <39086835+ckvermaAI@users.noreply.github.com>
    Co-authored-by: Sheng Yang <yang.sheng@intel.com>
    Co-authored-by: Yaser Afshar <yaser.afshar@intel.com>
    Co-authored-by: Wang, Yi <yi.a.wang@intel.com>
    Co-authored-by: Akihiro Takahashi <akihiro.takahashi@intel.com>
    Co-authored-by: Edward Mascarenhas <edward.mascarenhas@intel.com>
    Co-authored-by: Libin Tang <litang@habana.ai>
    Co-authored-by: Sayantan Sarkar <sasarkar@habana.ai>
    Co-authored-by: Daniel Socek <daniel.socek@intel.com>
    Co-authored-by: Silvia Colabrese <silvia.colabrese@intel.com>
    Co-authored-by: Mieszko Dziadowiec <mieszko.dziadowiec@intel.com>
    Co-authored-by: Dmitry <dmitry.smertin@intel.com>
    Co-authored-by: Urszula Golowicz <urszula.golowicz@intel.com>
    Co-authored-by: Nikolay Protasov <nikolay.protasov@intel.com>
    Co-authored-by: U. Artie Eoff <ullysses.a.eoff@intel.com>
    Co-authored-by: Luca Calabria <luca.calabria@intel.com>
    Co-authored-by: Harshvardhan Chauhan <hchauhan@habana.ai>
    Co-authored-by: Shifani Rajabose <shifani.rajabose@intel.com>
    Co-authored-by: Dmitry <dsmertin@habana.ai>
    Co-authored-by: Mounika Mandava <mounika.mandava@intel.com>
    Co-authored-by: ZhengHongming888 <hongming.zheng@intel.com>
    Co-authored-by: Alexey Fadeev <alexey.fadeev@intel.com>
    Co-authored-by: Vivek Kumar <vivek5.kumar@intel.com>
    Co-authored-by: Vivek Kumar <vivkumar@habana.ai>
    Co-authored-by: Ilyas Moutawwakil <57442720+IlyasMoutawwakil@users.noreply.github.com>
    Co-authored-by: Rafal Bogdanowicz <rafal.bogdanowicz@intel.com>
    Co-authored-by: Rafal <rbogdanowicz@habana.ai>
    Co-authored-by: Jan Kamiński <jkaminski@habana.ai>
    Co-authored-by: Karol Brejna <karol.brejna@intel.com>
    Co-authored-by: Piotr Bielak <piotr.bielak@intel.com>
    Co-authored-by: Piotr Bielak <pbielak@users.noreply.github.com>
    Co-authored-by: karol-brejna-i <karolbrejna@apache.org>
    Co-authored-by: IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>

--- optimum/habana/trl/__init__.py
+++ optimum/habana/trl/__init__.py
@@ -1,10 +1,21 @@
+import importlib.metadata
+
+from packaging import version
+
 from .models.modeling_base import adapt_PreTrainedModelWrapper_to_gaudi
 from .models.modeling_sd_base import GaudiDefaultDDPOStableDiffusionPipeline
 from .trainer.ddpo_trainer import GaudiDDPOTrainer
 from .trainer.dpo_config import GaudiDPOConfig
 from .trainer.dpo_trainer import GaudiDPOTrainer
-from .trainer.ppo_config import GaudiPPOConfig
-from .trainer.ppo_trainer import GaudiPPOTrainer
+
+
+trl_version = importlib.metadata.version("trl")
+if version.parse(trl_version) < version.parse("0.17.0"):
+    from .trainer.ppo_config import GaudiPPOConfig
+    from .trainer.ppo_trainer import GaudiPPOTrainer
+else:
+    from .trainer.grpo_config import GaudiGRPOConfig
+    from .trainer.grpo_trainer import GaudiGRPOTrainer
 from .trainer.reward_trainer import GaudiRewardTrainer, RewardDataCollatorWithPadding
 from .trainer.sft_config import GaudiSFTConfig
 from .trainer.sft_trainer import GaudiSFTTrainer
