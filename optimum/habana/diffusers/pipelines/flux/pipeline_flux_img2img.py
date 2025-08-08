b3ed6d1fa8efe905693f15b8bd45f5263329cdda
Urszula Golowicz <urszula.golowicz@intel.com>
2025-08-08 16:53:56 +0200

    Fixes for bad use of potential None value (#2198)
    Signed-off-by: Urszula <urszula.golowicz@intel.com>

--- optimum/habana/diffusers/pipelines/flux/pipeline_flux_img2img.py
+++ optimum/habana/diffusers/pipelines/flux/pipeline_flux_img2img.py
@@ -562,7 +562,7 @@ class GaudiFluxImg2ImgPipeline(GaudiDiffusionPipeline, FluxImg2ImgPipeline):
                 # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
                 timestep = timestep.expand(latents_batch.shape[0]).to(latents_batch.dtype)
 
-                if i >= quant_mixed_step:
+                if quant_mode == "quantize-mixed" and i >= quant_mixed_step:
                     # Mixed quantization
                     noise_pred = transformer_bf16(
                         hidden_states=latents_batch,
