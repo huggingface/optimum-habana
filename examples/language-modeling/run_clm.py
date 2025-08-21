94e2fbda56adc8e5010d8088430dd9061e479a18
Piotr Bielak <pbielak@users.noreply.github.com>
2025-08-21 18:21:21 +0200

    [Transformers 4.55] Do not use `.keys()` on dicts (#2218)
    Co-authored-by: regisss <15324346+regisss@users.noreply.github.com>

--- examples/language-modeling/run_clm.py
+++ examples/language-modeling/run_clm.py
@@ -587,7 +587,7 @@ def main():
     # Main data processing function that will concatenate all texts from our dataset and generate chunks of block_size.
     def group_texts(examples):
         # Concatenate all texts.
-        concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+        concatenated_examples = {k: list(chain(*examples[k])) for k in examples}
         total_length = len(concatenated_examples[list(examples.keys())[0]])
         # We drop the small remainder, and if the total_length < block_size  we exclude this batch and return an empty dict.
         # We could add padding if the model supported it instead of this drop, you can customize this part to your needs.
