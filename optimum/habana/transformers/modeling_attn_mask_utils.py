a1234dc7865d45065e518e96bf53f803c5067e78
Iman Gohari <s.m.iman.gohari@intel.com>
2025-08-28 20:27:12 +0000

    fea(attn_mask): reverted to unsplitted attn mask for swa and regular

--- optimum/habana/transformers/modeling_attn_mask_utils.py
+++ optimum/habana/transformers/modeling_attn_mask_utils.py
@@ -29,77 +29,54 @@ class GaudiAttentionMaskConverter(AttentionMaskConverter):
     """
 
     @staticmethod
-    def _create_causal_mask(
+    def _make_causal_mask(
         input_ids_shape: torch.Size,
         dtype: torch.dtype,
         device: torch.device,
         past_key_values_length: int = 0,
+        sliding_window: Optional[int] = None,
+        token_idx: Optional[int] = 0,
     ):
         """
         Make causal mask used for bi-directional self-attention.
         """
-        bsz, tgt_len = input_ids_shape
-        mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
-        mask_cond = torch.arange(mask.size(-1), device=device)
-        mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
-
-        mask = mask.to(dtype)
-
-        if past_key_values_length > 0:
-            mask = torch.cat(
-                [torch.zeros(tgt_len, past_key_values_length - tgt_len, dtype=dtype, device=device), mask], dim=-1
-            )
-
-        return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
-
-    @staticmethod
-    def _create_sliding_window_causal_mask(
-        input_ids_shape: torch.Size,
-        dtype: torch.dtype,
-        device: torch.device,
-        past_key_values_length: int = 0,
-        sliding_window: int = None,
-        token_idx: Optional[int] = 0,
-    ):
-        """
-        Make sliding window causal mask used for bi-directional self-attention.
-        """
-        # if sliding_window is None:
-        #     raise ValueError("Could not find a `sliding_window` argument, or it is not set")
-
         token_idx = token_idx if token_idx is not None else past_key_values_length
         bsz, tgt_len = input_ids_shape
 
         mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
         mask_cond = torch.arange(mask.size(-1), device=device)
-        # lower triangle and diagonal filled with 0, upper triangles are -inf
         mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
 
         mask = mask.to(dtype)
 
         if past_key_values_length > 0:
             mask = torch.cat(
-                [torch.zeros(tgt_len, past_key_values_length - tgt_len, dtype=dtype, device=device), mask], dim=-1
+                [torch.zeros(tgt_len, past_key_values_length - tgt_len, dtype=dtype, device=device), mask],
+                dim=-1,
             )
 
-        # add lower triangular
-        diagonal = token_idx - sliding_window - 1
+        # add lower triangular sliding window mask if necessary
+        if sliding_window is not None:
+            diagonal = token_idx - sliding_window - 1
 
-        # Replace tril with below
-        row_indices = torch.arange(mask.size(0), device=mask.device).view(-1, 1)  # Reshape to column vector
-        col_indices = torch.arange(mask.size(1), device=mask.device)
-        context_mask = (col_indices <= row_indices + diagonal).bool().expand_as(mask)  # Expand to match mask shape
+            # Replace tril with below
+            row_indices = torch.arange(mask.size(0), device=mask.device).view(-1, 1)  # Reshape to column vector
+            col_indices = torch.arange(mask.size(1), device=mask.device)
+            context_mask = (col_indices <= row_indices + diagonal).bool().expand_as(mask)  # Expand to match mask shape
+            # lower triangle of context_mask (in_len + out_len - sliding_window) is True
 
-        # Recent changes in PyTorch prevent mutations on tensors converted with aten::_to_copy
-        # See https://github.com/pytorch/pytorch/issues/127571
-        if is_torchdynamo_compiling():
-            mask = mask.clone()
+            # Recent changes in PyTorch prevent mutations on tensors converted with aten::_to_copy
+            # See https://github.com/pytorch/pytorch/issues/127571
+            if is_torchdynamo_compiling():
+                mask = mask.clone()
 
-        mask.masked_fill_(context_mask, torch.finfo(dtype).min)  # when context_mask is True, fill with -inf
-        if past_key_values_length > 0:
-            return mask[None, None, :, :].expand(bsz, 1, tgt_len, past_key_values_length)
-        else:
-            return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len)
+            mask.masked_fill_(context_mask, torch.finfo(dtype).min)
+
+            if past_key_values_length > 0:
+                return mask[None, None, :, :].expand(bsz, 1, tgt_len, past_key_values_length)
+            else:
+                return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len)
+        return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
 
     def to_4d(
         self,
@@ -124,27 +101,22 @@ class GaudiAttentionMaskConverter(AttentionMaskConverter):
                     "This attention mask converter is causal. Make sure to pass `key_value_length` to correctly create a causal mask."
                 )
 
-            past_key_values_length = key_value_length - query_length
-            if self.sliding_window is not None:
-                # when sliding_window is not None, find the token_idx by chechking the last idx of 1 in attention_mask_2d
-                if input_shape[-1] == 1:
-                    cumsum = attention_mask_2d.cumsum(dim=1)
-                    token_idx = cumsum.argmax(dim=1, keepdim=True)[0]
-                else:
-                    token_idx = None
-
-                causal_4d_mask = self._create_sliding_window_causal_mask(
-                    input_shape,
-                    dtype,
-                    device=device,
-                    past_key_values_length=past_key_values_length,  # 0 when prefill, token_idx when decode
-                    sliding_window=self.sliding_window,
-                    token_idx=token_idx,
-                )
+            # When sliding_window is not None, find the token_idx by chechking the last idx of 1 in attention_mask_2d
+            if input_shape[-1] == 1:
+                cumsum = attention_mask_2d.cumsum(dim=1)
+                token_idx = cumsum.argmax(dim=1, keepdim=True)[0]
             else:
-                causal_4d_mask = self._create_causal_mask(
-                    input_shape, dtype, device=device, past_key_values_length=past_key_values_length
-                )
+                token_idx = None
+
+            past_key_values_length = key_value_length - query_length
+            causal_4d_mask = self._make_causal_mask(
+                input_shape,
+                dtype,
+                device=device,
+                past_key_values_length=past_key_values_length,
+                sliding_window=self.sliding_window,
+                token_idx=token_idx,
+            )
 
             # just create a bool tensor with shape [bsz, 1, tgt_seq_len, src_seq_len]
             # OOM problem can be prevent by using bool tensor
