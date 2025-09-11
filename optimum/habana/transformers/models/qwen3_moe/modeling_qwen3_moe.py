cc80f0e6349213d262ace56238543026448eb808
Karol Brejna <karolbrejna@apache.org>
2025-09-11 16:01:11 +0200

    Remove dead code (for )

--- optimum/habana/transformers/models/qwen3_moe/modeling_qwen3_moe.py
+++ optimum/habana/transformers/models/qwen3_moe/modeling_qwen3_moe.py
@@ -956,7 +956,8 @@ class GaudiQwen3MoeModel(Qwen3MoeModel):
         # embed positions
         hidden_states = inputs_embeds
 
-        next_decoder_cache = () if not use_new_cache else None
+        # HPU uses legacy cache path (use_new_cache = False)
+        next_decoder_cache = ()
 
         if lazy_mode:
             htcore.mark_step()
