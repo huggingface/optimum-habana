40297d6e5c2369c0edb682bc832d98c85ac37165
bmengke <mengkejiergeli.ba@intel.com>
2025-09-29 20:05:35 +0800

    Add Qwen2.5-VL (#2235)
    Co-authored-by: regisss <15324346+regisss@users.noreply.github.com>

---
+++ optimum/habana/transformers/models/qwen2_5_vl/__init__.py
@@ -0,0 +1,9 @@
+from .modeling_qwen2_5_vl import (
+    GaudiQwen2_5_VisionTransformerPretrainedModel,
+    GaudiQwen2_5_VLAttention,
+    GaudiQwen2_5_VLDecoderLayer,
+    GaudiQwen2_5_VLForConditionalGeneration,
+    GaudiQwen2_5_VLModel,
+    GaudiQwen2_5_VLTextModel,
+    GaudiQwen2_5_VLVisionBlock,
+)
