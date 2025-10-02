Fix next_decoder_cache formula based on use_new_cache after tranformers 4.55 upgrade (#2284)

optimum/habana/transformers/models/gemma/modeling_gemma.py
@@ -616,14 +616,12 @@ class GaudiGemmaModel(GemmaModel):
         if inputs_embeds is None:
             inputs_embeds = self.embed_tokens(input_ids)
 
-        use_new_cache = False  # Ignoring new Cache path for HPU
         past_seen_tokens = 0
 
         if past_key_values is not None and use_cache:  # kept for BC (cache positions)
             if reuse_cache:
                 past_seen_tokens = past_key_values[0][0][2]
             else:
-                # HPU uses legacy cache path (use_new_cache = False)
                 past_seen_tokens = past_key_values[0][0].shape[2]
 
         cache_position = None
@@ -648,7 +646,7 @@ class GaudiGemmaModel(GemmaModel):
         normalizer = torch.tensor(self.config.hidden_size**0.5, dtype=hidden_states.dtype, device=inputs_embeds.device)
         hidden_states = hidden_states * normalizer
 
-        next_decoder_cache = () if not use_new_cache else None
+        next_decoder_cache = ()
 
         if lazy_mode:
             htcore.mark_step()

optimum/habana/transformers/models/mistral/modeling_mistral.py
@@ -24,7 +24,7 @@ from typing import Optional, Union
 
 import habana_frameworks.torch.core as htcore
 import torch
-from transformers.cache_utils import Cache, DynamicCache
+from transformers.cache_utils import Cache
 from transformers.modeling_outputs import BaseModelOutputWithPast, CausalLMOutputWithPast
 from transformers.models.mistral.configuration_mistral import MistralConfig
 from transformers.models.mistral.modeling_mistral import (
@@ -467,20 +467,9 @@ class GaudiMistralModel(MistralModel):
             raise ValueError("You have to specify either decoder_input_ids or decoder_inputs_embeds")
 
         past_key_values_length = 0
-        use_new_cache = False
-        return_legacy_cache = False
 
         if inputs_embeds is None:
             inputs_embeds = self.embed_tokens(input_ids)
-
-        if use_cache and not isinstance(past_key_values, Cache) and use_new_cache:
-            past_key_values = DynamicCache.from_legacy_cache(past_key_values)
-            return_legacy_cache = True
-            logger.warning_once(
-                "We detected that you are passing `past_key_values` as a tuple and this is deprecated and will be removed in v4.43. "
-                "Please use an appropriate `Cache` class (https://huggingface.co/docs/transformers/v4.41.3/en/internal/generation_utils#transformers.Cache)"
-            )
-            past_key_values_length = past_key_values.get_seq_length() if past_key_values is not None else 0
 
         if cache_position is None:
             cache_position = torch.arange(
@@ -541,11 +530,7 @@ class GaudiMistralModel(MistralModel):
 
         next_cache = None
         if use_cache:
-            next_cache = (
-                next_decoder_cache
-                if not use_new_cache
-                else (next_decoder_cache.to_legacy_cache() if return_legacy_cache else next_decoder_cache)
-            )
+            next_cache = next_decoder_cache
 
         return BaseModelOutputWithPast(
             last_hidden_state=hidden_states,