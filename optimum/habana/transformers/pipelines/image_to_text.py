50236d266215d780f502237d092ceec3cb932596
Urszula Golowicz <urszula.golowicz@intel.com>
2025-10-22 09:27:34 +0200

    Fix for visual-question-answering example (#2318)
    Signed-off-by: Urszula <urszula.golowicz@intel.com>

--- optimum/habana/transformers/pipelines/image_to_text.py
+++
@@ -1,9 +0,0 @@
-from transformers.pipelines.image_to_text import ImageToTextPipeline
-
-from ..generation import GaudiGenerationConfig
-
-
-class GaudiImageToTextPipeline(ImageToTextPipeline):
-    _default_generation_config = GaudiGenerationConfig(
-        max_new_tokens=256,
-    )
